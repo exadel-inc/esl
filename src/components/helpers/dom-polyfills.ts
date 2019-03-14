ba1fe2ece2af0f6843ea651bbcd167cb3742ad8c
yadamska <yuli.adamskaya@gmail.com>
2019-03-14 16:26:14 +0300

    [EDMTP-66]: change common helpers

--- src/components/helpers/dom-polyfills.ts
+++ src/components/helpers/dom-polyfills.ts
@@ -1,7 +1,12 @@
 /**
  * Element.closest polyfill
  * */
-(function (e: any) {
+interface Element {
+	msMatchesSelector(selectors: string): boolean;
+	mozMatchesSelector(selectors: string): boolean;
+}
+
+(function (e:Element ) {
 	e.matches = e.matches || e.msMatchesSelector || e.mozMatchesSelector || e.webkitMatchesSelector;
 	e.closest = e.closest || function (css: any) {
 		let node = this;
