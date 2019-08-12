98d3b4b137617ebbc1a763b9e5d9fe256dab2116
Alexander Avseev <aauseyeu@exadel.com>
2019-08-12 13:27:53 +0300

    EDMTP-148 Fix es6 file names

--- build/es6-task.js
+++ build/es6-task.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 
 const named = require('vinyl-named');
 const ts = require('gulp-typescript');
+const rename = require("gulp-rename");
 
 const TS_CONFIG = path.join(__dirname, '../tsconfig.json');
 
@@ -14,5 +15,10 @@ module.exports.buildES6 = function tsBuildES6(config) {
   });
   return gulp.src(config.src)
     .pipe(named(config.nameFunction))
-    .pipe(tsProject());
+    .pipe(tsProject())
+    .pipe(rename((path) => {
+      if (path.extname === ".js") {
+        path.extname = ".es6";
+      }
+    }));
 };
