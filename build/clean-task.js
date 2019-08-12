7713f5ed27c9488f78ddab001cdf4bb1857b7aba
Alexander Avseev <aauseyeu@exadel.com>
2019-08-12 15:15:37 +0300

    EDMTP-148 Cleanup

--- build/clean-task.js
+++ build/clean-task.js
@@ -2,5 +2,6 @@ const gulp = require('gulp');
 const clean = require('gulp-clean');
 
 module.exports = function cleanPath(config) {
-	return gulp.src(config.src, {read: false, allowEmpty: true}).pipe(clean({force: true}));
+	return gulp.src(config.src, {read: false, allowEmpty: true})
+		.pipe(clean({force: true}));
 };
