c62dfdcd3390d5e1180e67936f691250b44e1c5f
ygrebenchuk <grebenchuku@gmail.com>
2019-10-01 18:19:31 +0300

    EDMTP-86: refactoring smart-popup-trigger

--- src/components/smart-trigger/smart-popup-trigger.ts
+++ src/components/smart-trigger/smart-popup-trigger.ts
@@ -2,7 +2,7 @@ import { isTouch } from '../../helpers/device-utils';
 import { ISmartTrigger } from './smart-triger-interface';
 import { SmartPopup } from '../smart-popup/smart-popup';
 
-const hoverHideDelay = isTouch() ? 0 : 5000;
+const hoverHideDelay = isTouch() ? 0 : 1000;
 const hoverShowEvent = isTouch() ? 'click' : 'mouseenter';
 const hoverHideEvent = isTouch() ? 'click' : 'mouseleave';
 let modeType: any;
@@ -16,9 +16,7 @@ export interface ISmartPopupActionParams {
 class SmartPopupTrigger extends HTMLElement implements ISmartTrigger {
   protected eventType = {
     showEvent: '',
-    hideEvent: '',
-    hideDelay: 0,
-    showDelay: 0,
+    hideEvent: ''
   };
 
   static readonly is: string = 'smart-popup-trigger';
@@ -30,7 +28,6 @@ class SmartPopupTrigger extends HTMLElement implements ISmartTrigger {
   }
 
   protected attributeChangedCallback(attr: string, prevValue: string, value: string) {
-    // this.removeEvents();
     switch (attr) {
       case 'data-target-id':
         if (prevValue !== value) {
@@ -56,30 +53,25 @@ class SmartPopupTrigger extends HTMLElement implements ISmartTrigger {
       default:
         this.eventType.showEvent = this.eventType.hideEvent = value;
     }
-
-    if (this.eventType.hideEvent === hoverHideEvent && isNaN(hoverHideDelay)) {
-      this.eventType.hideDelay = hoverHideDelay;
-    }
   }
 
   setMode(value: string) {
-    this.removeEvents();
     const options = this.popup;
     switch (value) {
       case 'show':
-        this.addEventListener(this.eventType.showEvent, modeType = () => options.show());
+        this.showPopup(options);
         events.push({ event: this.eventType.showEvent, handler: options.show });
         break;
       case 'hide':
-        this.addEventListener(this.eventType.hideEvent, modeType = () => options.hide());
+        this.hidePopup(options);
         events.push({ event: this.eventType.hideEvent, handler: options.hide });
         break;
       default:
         if (this.eventType.showEvent === this.eventType.hideEvent) {
-          this.addEventListener(this.eventType.showEvent, modeType = () => options.toggle());
+          this.addEventListener(this.eventType.showEvent, this.togglePopup(options));
         } else {
-          this.addEventListener(this.eventType.showEvent, modeType = () => options.show());
-          this.addEventListener(this.eventType.hideEvent, modeType = () => options.hide());
+          this.showPopup(options);
+          this.hidePopup(options);
         }
     }
     events.forEach((event: any) => {
@@ -87,14 +79,26 @@ class SmartPopupTrigger extends HTMLElement implements ISmartTrigger {
     })
   }
 
+  protected showPopup(options: SmartPopup) {
+    modeType = () => options.show();
+    this.removeEventListener(this.eventType.showEvent, modeType);
+    this.addEventListener(this.eventType.showEvent, modeType)
+  }
 
-  removeEvents() {
-    events.forEach((event: any) => {
-      this.removeEventListener(event.event, event.handler);
-    })
+  protected hidePopup(options: SmartPopup) {
+    modeType = () => options.hide();
+    this.removeEventListener(this.eventType.hideEvent, modeType);
+    this.addEventListener(this.eventType.hideEvent, () => {
+      setTimeout(() => {
+        modeType();
+      }, hoverHideDelay)
+    });
+  }
+
+  protected togglePopup(options: SmartPopup) {
+    return modeType = () => options.toggle()
   }
 }
-
 
 customElements.define(SmartPopupTrigger.is, SmartPopupTrigger);
 export {
