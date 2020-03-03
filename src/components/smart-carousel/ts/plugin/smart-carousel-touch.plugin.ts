e9e41232caa45c2b977d3dbb1b86dcae367caa23
Alexey Stsefanovich <astsefanovich@exadel.com>
2020-03-03 15:09:31 +0300

    WIP: [Smart Carousel]: Touch plugin updated. Utils updated. Cleanup.

--- src/components/smart-carousel/ts/plugin/smart-carousel-touch.plugin.ts
+++ src/components/smart-carousel/ts/plugin/smart-carousel-touch.plugin.ts
@@ -1,42 +1,74 @@
 import SmartCarouselPlugin from './smart-carousel-plugin';
-import {TouchEventDetails, TouchObserver} from '@helpers/touch-observer';
+import {DeviceDetector} from '@helpers/device-utils';
+import {normalizeTouchPoint, Point} from '@helpers/dom-utils';
 
 /**
- * WIP Slide Carousel Touch plugin
- * TODO: discuss: should it be core / plugin / or plugin with the core hooks ?
+ * Slide Carousel Touch plugin
  */
 class SmartCarouselTouchPlugin extends SmartCarouselPlugin {
 	public static is = 'smart-carousel-touch-plugin';
 
-	private manager = new TouchObserver(this._onTouchMove.bind(this));
+	private isTouchStarted = false;
+	private startPoint: Point = null;
 
 	public bind() {
-		this.manager.observe(this.carousel);
+		const events = DeviceDetector.TOUCH_EVENTS;
+
+		this.carousel.addEventListener(events.START, this.onTouchStart);
+		this.carousel.addEventListener(events.MOVE, this.onTouchMove);
+		this.carousel.addEventListener(events.END, this.onTouchEnd);
 	}
 
 	public unbind() {
-		this.manager.unobserve(this.carousel);
+		const events = DeviceDetector.TOUCH_EVENTS;
+
+		this.carousel.removeEventListener(events.START, this.onTouchStart);
+		this.carousel.removeEventListener(events.MOVE, this.onTouchMove);
+		this.carousel.removeEventListener(events.END, this.onTouchEnd);
 	}
 
-	private _onTouchMove(e: TouchEventDetails) {
-		console.log(e);
+	onTouchStart = (event: TouchEvent | PointerEvent) => {
+		if ((event instanceof TouchEvent && event.touches.length !== 1) ||
+			(event instanceof PointerEvent && event.pointerType !== 'touch')) {
+			this.isTouchStarted = false;
+			return;
+		}
+		this.isTouchStarted = true;
+		this.startPoint = normalizeTouchPoint(event);
+	};
 
-		if (e.name !== 'swipe') return;
-		// TODO: we need to track direction and delegate slide move to the views
-		// Some markers will be needed for the move state
-		if (Math.abs(e.offset.x) < Math.abs(e.offset.y)) return; // Direction
-		if (Math.abs(e.offset.x) < 100) return; // Tolerance
+	onTouchMove = (event: TouchEvent | PointerEvent) => {
+		if (!this.isTouchStarted) return;
+
+		// const point = normalizeTouchPoint(event);
+		// const offset = {
+		// 	x: point.x - this.startPoint.x,
+		// 	y: point.y - this.startPoint.y
+		// };
+	};
+
+	onTouchEnd = (event: TouchEvent | PointerEvent) => {
+		if (!this.isTouchStarted) return;
+		const point = normalizeTouchPoint(event);
+		const offset = {
+			x: point.x - this.startPoint.x,
+			y: point.y - this.startPoint.y
+		};
+		this.isTouchStarted = false;
+
+		// TODO: temporary, update according final implementation
+		if (Math.abs(offset.x) < Math.abs(offset.y)) return; // Direction
+		if (Math.abs(offset.x) < 100) return; // Tolerance
 
 		// Swipe gesture example
-		// TODO: should set direct slide based on move offset state
-		if (e.offset.x < 0) {
+		if (offset.x < 0) {
 			this.carousel.next();
 		} else {
 			this.carousel.prev();
 		}
-		e.event.preventDefault();
-		e.event.stopPropagation();
-	}
+		event.preventDefault();
+		event.stopPropagation();
+	};
 }
 
 export default SmartCarouselTouchPlugin;
