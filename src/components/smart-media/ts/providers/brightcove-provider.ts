0dddd66ca913007fc9aef6cb0dd3912efc1c9fcf
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-03-19 13:56:11 +0300

    [TECH]: Brightcove Provider cleanup. Version reset.

--- src/components/smart-media/ts/providers/brightcove-provider.ts
+++ src/components/smart-media/ts/providers/brightcove-provider.ts
@@ -13,22 +13,47 @@ import SmartMediaProviderRegistry from '../smart-media-registry';
 
 const API_SCRIPT_ID = 'BC_API_SOURCE';
 
+export interface BCPlayerAccount {
+	playerId: string;
+	accountId: string;
+}
+
 export class BrightcoveProvider extends BaseProvider<HTMLVideoElement |  HTMLDivElement> {
 
 	static get providerName() {
 		return 'brightcove';
 	}
 
-	/**
-	 * @returns brightcove player script endpoint
-	 */
-	protected buildAPISrc = () => `//players.brightcove.net/${this._accountId}/${this._playerId}_default/index.min.js`;
-
 	protected _api: VideoJsPlayer;
-	protected _playerId: string;
-	protected _accountId: string;
+	protected _account: BCPlayerAccount;
 
-	protected buildVideo(sm: SmartMedia) {
+	/**
+	 * @returns {BCPlayerAccount} settings, get from element by default
+	 */
+	protected static getAccount(el: SmartMedia): BCPlayerAccount {
+		return {
+			playerId: el.getAttribute('player-id'),
+			accountId: el.getAttribute('account-id')
+		};
+	}
+
+	/**
+	 * Loads player API according defined settings
+	 * */
+	protected static loadAPI(account: BCPlayerAccount): Promise<Event> {
+		const apiSrc =
+			`//players.brightcove.net/${account.accountId}/${account.playerId}_default/index.min.js`;
+		const apiScript = document.getElementById(API_SCRIPT_ID);
+		if (apiScript && apiScript.getAttribute('src') !== apiSrc) {
+			apiScript.parentNode.removeChild(apiScript);
+		}
+		return loadScript(API_SCRIPT_ID, apiSrc);
+	}
+
+	/**
+	 * Build video brightcove element
+	 */
+	protected static buildVideo(sm: SmartMedia, account: BCPlayerAccount) {
 		const el = document.createElement('video');
 		el.id = 'smedia-brightcove-' + generateUId();
 		el.className = 'smedia-inner smedia-brightcove video-js vjs-default-skin video-js-brightcove';
@@ -41,47 +66,38 @@ export class BrightcoveProvider extends BaseProvider<HTMLVideoElement |  HTMLDiv
 		el.controls = sm.controls;
 		el.setAttribute('aria-label', el.title);
 		el.setAttribute('data-embed', 'default');
-		el.setAttribute('data-player', this._playerId);
-		el.setAttribute('data-account', this._accountId);
+		el.setAttribute('data-player', account.playerId);
+		el.setAttribute('data-account', account.accountId);
 		el.setAttribute('data-video-id', `ref:${sm.mediaId}`);
 		return el;
 	}
 
-	protected initializePlayer() {
-		const apiSrc = this.buildAPISrc();
-		const apiScript = document.getElementById(API_SCRIPT_ID);
-		if (apiScript && apiScript.getAttribute('src') !== apiSrc) {
-			apiScript.parentNode.removeChild(apiScript);
+	protected onAPILoaded() {
+		if (typeof window.bc !== 'function' || typeof window.videojs !== 'function') {
+			throw new Error('Brightcove API is not in the global scope');
 		}
-		this._ready = loadScript(API_SCRIPT_ID, apiSrc);
+		this._api = window.videojs(this._el);
+		this.onAPIReady();
 
-		this._ready = this._ready.then(() => {
-            if (typeof window.bc !== 'function' || typeof window.videojs !== 'function') {
-                throw new Error('Brightcove API is not in the global scope');
-            }
-            this._api = window.videojs(this._el);
-            this.onAPIReady();
+		this._api.on('play', () => this.component._onPlay());
+		this._api.on('pause', () => this.component._onPaused());
+		this._api.on('ended', () => this.component._onEnded());
 
-            this._api.on('play', () => this.component._onPlay());
-            this._api.on('pause', () => this.component._onPaused());
-            this._api.on('ended', () => this.component._onEnded());
-
-            this.component._onReady();
-        });
+		this.component._onReady();
+	}
+	protected onAPIReady() {
+		window.bc(this._el);
 	}
 
 	public bind() {
-        this._playerId = this.component.getAttribute('player-id');
-        this._accountId = this.component.getAttribute('account-id');
-
-		this._el = this.buildVideo(this.component);
+		const Provider = (this.constructor as typeof BrightcoveProvider);
+		this._account = Provider.getAccount(this.component);
+		this._el = Provider.buildVideo(this.component, this._account);
 		this.component.appendChild(this._el);
 
-		this.initializePlayer();
-	}
-
-	protected onAPIReady() {
-		window.bc(this._el);
+		this._ready = Provider.loadAPI(this._account)
+			.then(() => this.onAPILoaded())
+			.catch((e) => this.component._onError(e));
 	}
 
 	public unbind() {
@@ -100,7 +116,7 @@ export class BrightcoveProvider extends BaseProvider<HTMLVideoElement |  HTMLDiv
 		}
 	}
 
-	get state() {
+	public get state() {
 		if (this._api) {
 			if (this._api.ended()) return PlayerStates.ENDED;
 			if (this._api.paused()) return PlayerStates.PAUSED;
@@ -110,16 +126,16 @@ export class BrightcoveProvider extends BaseProvider<HTMLVideoElement |  HTMLDiv
 		return PlayerStates.UNINITIALIZED;
 	}
 
-	get defaultAspectRatio(): number {
+	public get defaultAspectRatio(): number {
 		if (!this._api) return 0;
 		return this._api.videoWidth() / this._api.videoHeight();
 	}
 
-	get currentTime() {
+	public get currentTime() {
 		return this._api ? this._api.currentTime() : 0;
 	}
 
-	get duration() {
+	public get duration() {
 		return this._api ? this._api.duration() : 0;
 	}
 
@@ -146,7 +162,7 @@ SmartMediaProviderRegistry.register(BrightcoveProvider, BrightcoveProvider.provi
 // typings
 declare global {
 	interface Window {
-		bc?: (el: HTMLElement, ...args : any[]) => any;
-		videojs?: (el: HTMLElement, ...args : any[]) => VideoJsPlayer;
+		bc?: (el: HTMLElement, ...args: any[]) => any;
+		videojs?: (el: HTMLElement, ...args: any[]) => VideoJsPlayer;
 	}
 }
