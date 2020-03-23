1521fd6ce30a1c419293bb51a450a89eca0d10ce
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-03-23 17:54:17 +0300

    [TECH]: Fix loadScript utility. Version updated.

--- src/helpers/common-utils.ts
+++ src/helpers/common-utils.ts
@@ -1,22 +1,43 @@
 /**
+ * Creates new async script tag by id and src
+ */
+const createAsyncScript = (id: string, src: string) => {
+	const script = document.createElement('script');
+	script.id = id;
+	script.async = true;
+	script.src = src;
+	return script;
+};
+
+/**
  * Common function that loads script async
- * @param {string} id - unique script id that used as a marker to prevent feature load
+ * @param {string} id - unique script id that is used as a marker to prevent future load
  * @param {string} src - script src (url) to load
  */
 export function loadScript(id: string, src: string): Promise<Event> {
 	return new Promise((resolve, reject) => {
-		if (document.getElementById(id)) {
-			resolve();
-			return;
+		const script: HTMLScriptElement =
+			(document.getElementById(id) || createAsyncScript(id, src)) as HTMLScriptElement;
+		const state = script.getAttribute('state');
+
+		switch (state) {
+			case 'success': resolve(); break;
+			case 'error': reject(); break;
+			default:
+				script.addEventListener('load', () => {
+					script.setAttribute('state', 'success');
+					resolve();
+				});
+				script.addEventListener('error', () => {
+					script.setAttribute('state', 'error');
+					reject();
+				});
 		}
-		const tag = document.createElement('script');
-		tag.id = id;
-		tag.async = true;
-		tag.onload = resolve;
-		tag.onerror = reject;
-		tag.src = src;
-		const firstScriptTag = document.getElementsByTagName('script')[0];
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		if (!script.parentNode) {
+			const firstScriptTag = document.querySelector('script') ||
+				document.querySelector('head title');
+			firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
+		}
 	});
 }
 
