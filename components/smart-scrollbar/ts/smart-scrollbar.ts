18938821cfcb9209dc014124ded26a23526ea1fe
yadamska <yadamska@exadel.com>
2020-06-16 15:56:28 +0300

    [SmartScrollbar] Update component

--- components/smart-scrollbar/ts/smart-scrollbar.ts
+++ components/smart-scrollbar/ts/smart-scrollbar.ts
@@ -1,3 +1,8 @@
+/**
+ * Smart Scrollbar
+ * @version 1.0.0
+ * @author Yuliya Adamskaya
+ */
 import {CustomElement} from '../../smart-utils/abstract/custom-element';
 import {attr} from '../../smart-utils/decorators/attr';
 import {findTarget} from '../../smart-utils/dom/traversing';
@@ -6,25 +11,25 @@ import {rafDecorator} from '../../smart-utils/async/raf';
 export class SmartScrollbar extends CustomElement {
     public static is = 'smart-scrollbar';
 
-    protected $scrollableContent: HTMLDivElement;
-    protected $scrollbarThumb: HTMLDivElement;
-    protected $scrollbarTrack: HTMLDivElement;
+    protected $scrollbarThumb: HTMLElement;
+    protected $scrollbarTrack: HTMLElement;
+    protected $scrollableContent: HTMLElement;
 
-    protected _draggingStarted: boolean;
     protected _initialMousePosition: number;
     protected _initialPosition: number;
 
-    @attr({conditional: true}) protected inactive: boolean;
+    @attr({conditional: true}) protected dragging: boolean;
+    @attr({conditional: true, readonly: true}) public inactive: boolean;
 
-    @attr({defaultValue: 'vertical'}) protected direction: string;
-    @attr({defaultValue: '::parent'}) protected target: string;
-    @attr({defaultValue: 'scrollbar-thumb'}) protected thumbClass: string;
-    @attr({defaultValue: 'scrollbar-track'}) protected trackClass: string;
+    @attr({defaultValue: '::parent'}) public target: string;
+    @attr({defaultValue: 'vertical'}) public direction: string;
+    @attr({defaultValue: 'scrollbar-thumb'}) public thumbClass: string;
+    @attr({defaultValue: 'scrollbar-track'}) public trackClass: string;
 
     protected connectedCallback() {
         super.connectedCallback();
 
-        this.updateScrollableTarget();
+        this.findTarget();
 
         this.render();
         this.refresh();
@@ -34,6 +39,26 @@ export class SmartScrollbar extends CustomElement {
 
     protected disconnectedCallback() {
         this.unbindEvents();
+    }
+
+    private attributeChangedCallback(attrName: string, oldVal: string, newVal: string) {
+        if (!this.connected && oldVal === newVal) return;
+        if (attrName === 'target') {
+            this.findTarget();
+        }
+    }
+
+    protected findTarget() {
+        if (!this.target) return;
+        const content = findTarget(this.target, this) as HTMLDivElement;
+        this.targetElement = content ? content : null;
+    }
+    public set targetElement(content: HTMLElement) {
+        if (this.$scrollableContent) {
+            this.$scrollableContent.removeEventListener('scroll', this.onScroll);
+        }
+        this.$scrollableContent = content;
+        this.$scrollableContent.addEventListener('scroll', this.onScroll, {passive: true});
     }
 
     protected render() {
@@ -49,31 +74,19 @@ export class SmartScrollbar extends CustomElement {
     protected bindEvents() {
         this.addEventListener('click', this.onClick);
         this.$scrollbarThumb.addEventListener('mousedown', this.onMouseDown);
+
         window.addEventListener('resize', this.onResize, {passive: true});
     }
 
     protected unbindEvents() {
         this.removeEventListener('click', this.onClick);
         this.$scrollbarThumb.removeEventListener('mousedown', this.onMouseDown);
+
         window.removeEventListener('resize', this.onResize);
+
         this.$scrollableContent.removeEventListener('scroll', this.onScroll);
     }
 
-    protected set scrollableContent (content: HTMLDivElement) {
-        if (this.$scrollableContent) {
-            this.$scrollableContent.removeEventListener('scroll', this.onScroll);
-        }
-        this.$scrollableContent = content;
-        this.$scrollableContent.addEventListener('scroll', this.onScroll, {passive: true});
-    }
-    public refresh() {
-        this.update();
-        this.updateMarkers();
-    }
-
-    public get active() {
-        return this.thumbSize === 1;
-    }
     public get thumbSize() {
         // behave as native scroll
         if (!this.$scrollableContent) return 1;
@@ -82,7 +95,8 @@ export class SmartScrollbar extends CustomElement {
 
     public get position() {
         if (!this.$scrollableContent) return 0;
-        return this.$scrollableContent.scrollTop / (this.$scrollableContent.scrollHeight - this.$scrollableContent.offsetHeight);
+        const scrollableHeight = this.$scrollableContent.scrollHeight - this.$scrollableContent.offsetHeight;
+        return scrollableHeight ? (this.$scrollableContent.scrollTop / scrollableHeight) : 0;
     }
     public set position(position) {
         const normalizedPosition = Math.min(1, Math.max(0, position));
@@ -99,23 +113,21 @@ export class SmartScrollbar extends CustomElement {
         this.$scrollbarThumb.style.top = `${thumbTop}px`;
         this.$scrollbarThumb.style.height = `${thumbSize}px`;
     }
-
-    protected updateScrollableTarget() {
-        if (!this.target) return;
-        const content = findTarget(this.target, this) as HTMLDivElement;
-        this.scrollableContent = content ? content : null;
-    }
-
-    protected updateMarkers() {
-        if (this.active) {
-            this.setAttribute('active', '')
+    public updateMarkers() {
+        if (this.thumbSize === 1) {
+            this.setAttribute('inactive', '');
         } else {
-            this.removeAttribute('active');
+            this.removeAttribute('inactive');
         }
     }
+    public refresh() {
+        this.update();
+        this.updateMarkers();
+    }
 
+    // Event listeners
     protected onMouseDown = (event: MouseEvent) => {
-        this._draggingStarted = true;
+        this.dragging = true;
         this._initialPosition = this.position;
         this._initialMousePosition = event.clientY;
         event.preventDefault();
@@ -125,35 +137,35 @@ export class SmartScrollbar extends CustomElement {
     };
 
     protected onMouseMove = rafDecorator((event: MouseEvent) => {
-        if (!this._draggingStarted) return;
+        if (!this.dragging) return;
         const positionChange = event.clientY - this._initialMousePosition;
-        const absChange = positionChange / (this.$scrollbarTrack.offsetHeight - this.$scrollbarThumb.offsetHeight);
+        const scrollableAreaHeight = this.$scrollbarTrack.offsetHeight - this.$scrollbarThumb.offsetHeight;
+        const absChange = scrollableAreaHeight ? (positionChange / scrollableAreaHeight) : 0;
         this.position = this._initialPosition + absChange;
         event.preventDefault();
     });
 
     protected onMouseUp = (event: MouseEvent) => {
-        if (this._draggingStarted) {
+        if (this.dragging) {
             event.stopPropagation();
             event.preventDefault();
         }
-        this._draggingStarted = false;
+        this.dragging = false;
         window.removeEventListener('mousemove', this.onMouseMove);
         window.removeEventListener('mouseup', this.onMouseUp);
     };
 
     protected onClick = (event: MouseEvent) => {
         if (event.target !== this.$scrollbarTrack) return;
+
         const thumbHeight = this.$scrollbarThumb.offsetHeight;
-        const trackOffset = this.$scrollbarTrack.getBoundingClientRect().top + document.body.scrollTop;
+        const trackOffset = this.$scrollbarTrack.getBoundingClientRect().top + window.pageYOffset;
         const positionChange = event.pageY - trackOffset - thumbHeight / 2;
         this.position = positionChange / (this.$scrollbarTrack.offsetHeight - thumbHeight);
     };
 
     protected onScroll = rafDecorator(() => {
-        if (!this._draggingStarted) {
-            this.update();
-        }
+        if (!this.dragging) this.update();
     });
 
     protected onResize = rafDecorator(() => this.refresh());
