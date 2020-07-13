93324cc8f4ef272d9c48abe1a5728982d3ae0e26
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-07-13 22:18:44 +0300

    [TECH]: Build tasks updated.

--- build/clean-task.js
+++ build/clean-task.js
@@ -1,7 +1,21 @@
 const gulp = require('gulp');
+const paths = require('../paths');
 const clean = require('gulp-clean');
 
-module.exports = function cleanPath(config) {
+function cleanPath(config) {
 	return gulp.src(config.src, {read: false, allowEmpty: true})
 		.pipe(clean({force: true}));
-};
+}
+
+module.exports.default = cleanPath;
+
+module.exports.clean = function cleanLib() {
+	return cleanPath({
+		src: [paths.bundle.target].map((path) => `${path}/`)
+	});
+}
+module.exports.cleanLocal = function cleanLocal() {
+	return cleanPath({
+		src: `${paths.test.target}/`
+	});
+}
