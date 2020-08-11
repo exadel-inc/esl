01f987b1a0446c2f267af5b69e3e462a6de041bb
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-08-12 00:55:44 +0300

    [CORE]: DOMTokenList.toggle polyfill added.

---
+++ polyfills/list/dom.tokenList.toggle.ts
@@ -0,0 +1,18 @@
+/**
+ * DOMTokenList.toggle polyfill for IE11 to support force option
+ */
+(function (DOMTokenListProto) {
+	const nativeToggle = DOMTokenListProto.toggle;
+	const testTokenList = window.document.createElement('div').classList;
+
+	if (!DOMTokenListProto.toggle) return;
+	if (testTokenList.toggle('a', false) === false) return;
+
+	DOMTokenListProto.toggle = function toggle(val:string, force?: boolean) {
+		if (arguments.length > 1) {
+			this[force ? 'add' : 'remove'](val);
+			return !!force;
+		}
+		return nativeToggle.call(this, val);
+	};
+})(window.DOMTokenList.prototype);
