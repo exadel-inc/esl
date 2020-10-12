128bb56a10a7988ab834b39a93c1cb4cf7a5c394
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-10-12 17:58:50 +0300

    [.editorconfig]: Normalize esl-popup + base-popup + carousel plugins

--- components/esl-popup/ts/esl-popup.ts
+++ components/esl-popup/ts/esl-popup.ts
@@ -5,22 +5,22 @@ import {ScrollStrategy, ScrollUtility} from '../../esl-utils/dom/scroll';
 
 @ExportNs('Popup')
 export class ESLPopup extends ESLBasePopup {
-	public static is = 'esl-popup';
-	public static eventNs = 'esl:popup';
+  public static is = 'esl-popup';
+  public static eventNs = 'esl:popup';
 
-	@attr({ defaultValue: null }) public disableScroll: ScrollStrategy;
+  @attr({defaultValue: null}) public disableScroll: ScrollStrategy;
 
-	protected onShow(params: PopupActionParams) {
-		super.onShow(params);
+  protected onShow(params: PopupActionParams) {
+    super.onShow(params);
 
-		this.disableScroll !== null && ScrollUtility.requestLock(this, this.disableScroll);
-	}
+    this.disableScroll !== null && ScrollUtility.requestLock(this, this.disableScroll);
+  }
 
-	protected onHide(params: PopupActionParams) {
-		super.onHide(params);
+  protected onHide(params: PopupActionParams) {
+    super.onHide(params);
 
-		this.disableScroll !== null && ScrollUtility.requestUnlock(this, this.disableScroll);
-	}
+    this.disableScroll !== null && ScrollUtility.requestUnlock(this, this.disableScroll);
+  }
 }
 
 export default ESLPopup;
