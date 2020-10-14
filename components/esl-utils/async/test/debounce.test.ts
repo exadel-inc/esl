57c77e562c09be8b92fb86634e9c9d25a29c21c2
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-10-15 02:46:08 +0300

    [Test]: debounce basic test.

---
+++ components/esl-utils/async/test/debounce.test.ts
@@ -0,0 +1,26 @@
+import {debounce} from '../debounce';
+
+describe('async/debounce tests', () => {
+  test('basic test', (done) => {
+    let log = 0;
+    const increment = () => log++;
+
+    const debounced = debounce(increment, 20);
+
+    expect(typeof debounced).toBe('function');
+    expect(debounced.then).toBeDefined();
+
+    expect(log).toBe(0);
+    debounced();
+    debounced();
+    setTimeout(() => debounced());
+    expect(log).toBe(0);
+
+    setTimeout(() => {
+      expect(log).toBe(1);
+      done();
+    }, 50);
+  }, 100);
+
+  // TODO: more tests
+});
