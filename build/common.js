9d3a2c10e5efa72b91dd754dbeda7e79ec91a9b3
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-10-30 02:42:13 +0300

    tech: Catlog :) And build updates.

--- build/common.js
+++ build/common.js
@@ -1,8 +1,19 @@
-module.exports.printBuildStart = function () {
-  console.log('=== Running ESL Build ===');
-};
+function print(...logArgs) {
+  return (cb) => {
+    console.log(...logArgs);
+    (typeof cb === 'function') && cb();
+  };
+}
+module.exports.print = print;
 
-module.exports.print = (...logArgs) => (cb) => {
-  console.log(...logArgs);
-  cb();
+const ANSI_CATS = [
+  '\t\t\t ((      /|_/|\n\t\t\t  \\\\.._.\'  , ,\\\n\t\t\t  /\\ | \'.__ v /\n\t\t\t (_ .   /   "        \n\t\t\t  ) _)._  _ /\n\t\t\t \'.\\ \\|( / ( mrf\n\t\t\t   \'\' \'\'\\\\ \\\\',
+  '\t\t\t _._     _,-\'""`-._\n\t\t\t(,-.`._,\'(       |\\`-/|\n\t\t\t    `-.-\' \\ )-`( , o o)\n\t\t\t          `-    \\`_`"\'-',
+  '\t\t\t  /\\_/\\  (\n\t\t\t ( ^.^ ) _)\n\t\t\t   \\"/  (\n\t\t\t ( | | )\n\t\t\t(__d b__)',
+  '      /^--^\\     /^--^\\     /^--^\\\n\\____/     \\____/     \\____/\n     /      \\   /      \\   /      \\\n    |        | |        | |        |\n     \\__  __/   \\__  __/   \\__  __/\n|^|^|^|^\\ \\^|^|^|^/ /^|^|^|^|^\\ \\^|^|^|^|^|\n| | | | |\\ \\| | |/ /| | | | | | \\ \\ | | | |\n| | | | / / | | |\\ \\| | | | | |/ /| | | | |\n| | | | \\/| | | | \\/| | | | | |\\/ | | | | |\n###########################################'
+];
+
+module.exports.catLog = (text) => {
+  const cat = ANSI_CATS[Math.floor(Math.random() * ANSI_CATS.length)];
+  return print(cat + '\n\t\t\t' + text);
 };
