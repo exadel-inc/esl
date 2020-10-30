947c4e097bb2858b6fe9c9064aed49c39e953184
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-10-30 04:01:54 +0300

    fix: Sonar code small fix.

--- modules/esl-utils/misc/format.ts
+++ modules/esl-utils/misc/format.ts
@@ -16,7 +16,7 @@ export const toCamelCase = (str: string) => {
  * Unwrap string from parenthesis
  */
 export const unwrapParenthesis = (str: string) => {
-  return str.replace(/^\s*\((.*)\)\s*$/, '$1').trim();
+  return str.trim().replace(/^\((.*)\)$/, '$1').trim();
 };
 
 /**
