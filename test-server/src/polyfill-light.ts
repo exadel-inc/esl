ecb738e8173f1834ef2a8eb8f7ed863baf3b6a1d
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-11-10 00:33:50 +0300

    fix: polyfill content fix

--- test-server/src/polyfill-light.ts
+++ test-server/src/polyfill-light.ts
@@ -2,14 +2,8 @@
  * Target: Edge, Safari 9
  */
 import 'intersection-observer/intersection-observer';
-import {ResizeObserver} from '@juggle/resize-observer';
 
-if ('ResizeObserver' in window) {
-  (window as any).ResizeObserver = ResizeObserver;
-}
+import {ResizeObserver} from '@juggle/resize-observer';
+window.ResizeObserver = window.ResizeObserver || ResizeObserver;
 
 import '@webcomponents/custom-elements/custom-elements.min';
-
-// @ts-ignore
-import smoothscroll from 'smoothscroll-polyfill';
-smoothscroll.polyfill();
