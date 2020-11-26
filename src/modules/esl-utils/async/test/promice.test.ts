028c7771e3e8b9ec105cb46ebb04ef4638eb0f8f
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-11-26 04:07:13 +0300

    test: async promise coverage improved

--- src/modules/esl-utils/async/test/promice.test.ts
+++ src/modules/esl-utils/async/test/promice.test.ts
@@ -1,44 +1,54 @@
 import {PromiseUtils} from '../promise';
 
 describe('PromiseUtils', () => {
-  describe('Common methods', () => {
+  describe('common', () => {
     test('resolve', () => PromiseUtils.resolve(true).then((v) => expect(v).toBe(true)));
     test('reject', () => PromiseUtils.reject(true).catch((v) => expect(v).toBe(true)));
+  });
 
-    test('deferred.resolve', () => {
+  describe('deferred', () => {
+    test('resolve', () => {
       const def$$ = PromiseUtils.deferred();
       def$$.resolve(1);
       return def$$.promise.then((n) => expect(n).toBe(1));
-    })
-
-    test('deferred.reject', () => {
+    });
+    test('reject', () => {
       const def$$ = PromiseUtils.deferred();
       def$$.reject(1);
       return def$$.promise.catch((n) => expect(n).toBe(1));
-    })
+    });
   });
 
-  describe('Converters', () => {
+  describe('fromTimeout', () => {
     test('fromTimeout', () =>
         PromiseUtils.fromTimeout(30, true)
-          .then((v) => expect(v).toBe(true)),
-      60
+          .then((v) => expect(v).toBe(true))
     );
+  });
 
-    test('fromEvent', () => {
+  describe('fromEvent', () => {
+    test('simple', () => {
       const el = document.createElement('div');
       setTimeout(() => el.dispatchEvent(new CustomEvent('test')), 20);
       return PromiseUtils.fromEvent(el, 'test').then((e) => expect(e.type).toBe('test'));
     });
-    test('fromEvent: timeout', () => {
+    test('timeout', () => {
       const el = document.createElement('div');
       setTimeout(() => el.dispatchEvent(new CustomEvent('test')), 100);
       return PromiseUtils.fromEvent(el, 'test', 10).catch(() => expect(true).toBe(true));
     });
+  });
 
-    test('fromMarker', () => {
+  describe('fromMarker', () => {
+    test('attribute', () => {
       const el = document.createElement('div');
       (el as any).test = true;
+      return PromiseUtils.fromMarker(el, 'test').then((e) => expect(true).toBe(true));
+    });
+
+    test('event', () => {
+      const el = document.createElement('div');
+      setTimeout(() => el.dispatchEvent(new CustomEvent('test')), 10);
       return PromiseUtils.fromMarker(el, 'test').then((e) => expect(true).toBe(true));
     });
   });
@@ -53,6 +63,18 @@ describe('PromiseUtils', () => {
       return PromiseUtils.tryUntil(() => i > 1, 10, 10)
         .then(() => expect(true).toBe(true));
     });
+
+    test('error', () => {
+      let i = false;
+      function fn() {
+        if (i) return true;
+        i = true;
+        throw new Error('Error test');
+      }
+      return PromiseUtils.tryUntil(fn, 2, 10)
+        .then(() => expect(true).toBe(true));
+    });
+
     test('failed', () => {
       return PromiseUtils.tryUntil(() => false, 5, 10)
         .catch(() => expect(true).toBe(true));
