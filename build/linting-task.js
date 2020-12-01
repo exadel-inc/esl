94e2ade59cf97fb7ad0ca07d47bed19a3a97b637
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-12-01 13:55:09 +0300

    chore: remove useless build param

--- build/linting-task.js
+++ build/linting-task.js
@@ -5,18 +5,18 @@ const root = path.resolve(__dirname, './..');
 const eslint = require('gulp-eslint');
 const stylelint = require('gulp-stylelint');
 
-module.exports.lintTypeScript = (src, base) => {
+module.exports.lintTypeScript = (src) => {
   return function lintTypeScript() {
-    return gulp.src(src, {base})
+    return gulp.src(src)
       .pipe(eslint(path.join(root, '.eslintrc.json')))
       .pipe(eslint.format('unix', console.warn))
       .pipe(eslint.failAfterError());
   };
 };
 
-module.exports.lintStyle = (src, base) => {
+module.exports.lintStyle = (src) => {
   return function lintStyles() {
-    return gulp.src(src, {base})
+    return gulp.src(src)
       .pipe(stylelint({
         reporters: [
           {formatter: 'string', console: true},
