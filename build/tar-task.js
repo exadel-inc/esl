4618e381794e02d7c15f8153cde3710bd42301b5
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-01-13 03:24:44 +0300

    refactor: remove unused gz tarball

--- build/tar-task.js
+++ build/tar-task.js
@@ -2,7 +2,6 @@ const path = require('path');
 const gulp = require('gulp');
 
 const gulpTar = require('gulp-tar');
-const gulpGzip = require('gulp-gzip');
 const gulpPrint = require('gulp-print').default;
 const gulpIgnore = require('gulp-exclude-gitignore');
 
@@ -16,8 +15,6 @@ module.exports.tarBuild = (dest) => {
       .pipe(gulpIgnore(ignoreFile))
       .pipe(gulpPrint((filepath) => `NPM PACKAGE LIST: ${filepath}`))
       .pipe(gulpTar(tarPackageName + '.tar', {prefix: tarPackageName}))
-      .pipe(gulp.dest(dest))
-      .pipe(gulpGzip())
       .pipe(gulp.dest(dest))
       .on('end', () => console.log(`'${tarPackageName}' PACKAGE CREATED`));
   }
