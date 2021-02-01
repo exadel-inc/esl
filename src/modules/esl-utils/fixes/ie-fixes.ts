32cb2b27907e01b5768ac8c9e7e8a26c24b51d62
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-02-01 16:56:43 +0300

    refactor!: rename createIframe IE fixes utility to createZIndexIframe

--- src/modules/esl-utils/fixes/ie-fixes.ts
+++ src/modules/esl-utils/fixes/ie-fixes.ts
@@ -1,5 +1,5 @@
 /** Fix IE browser to allow to display alert under iframe */
-export function createIframe(): HTMLElement {
+export function createZIndexIframe(): HTMLElement {
   const iframe = document.createElement('iframe');
   iframe.className = 'ie-zindex-fix';
   iframe.src = 'about:blank';
