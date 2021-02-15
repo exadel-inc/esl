9a4b09f9194ed8d9430264626ae5cf9af0f3d76c
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-02-15 14:16:03 +0300

    refactor: [].includes polyfill simplified

--- src/polyfills/list/es6.array.includes.ts
+++ src/polyfills/list/es6.array.includes.ts
@@ -2,26 +2,13 @@
  * Group: ES6 shim
  * Target Browsers: IE11, Edge < 14
  * Array.prototype.includes polyfill
- * Require: Object.is
  */
 if (!Array.prototype.includes) {
   Array.prototype.includes = function <T> (item: T, fromIndex?: number) {
-    if (this == null) throw new TypeError('"this" is null or not defined');
-
-    const list = Object(this);
-    // eslint-disable-next-line no-bitwise
-    const length = list.length >>> 0;
-
-    if (length === 0) return false;
-    // Normalize fromIndex
-    let i: number = fromIndex ?
-      Math.max(0, fromIndex >= 0 ? fromIndex : length - Math.abs(fromIndex)) :
-      0;
-    while (i < length) {
-      // NaN should be correctly handled by Object.is
-      if (Object.is(list[i], item)) return true;
-      i++;
+    if (item !== item) {
+      // NaN case
+      return Array.prototype.slice.call(this, fromIndex).some((i: T) => i !== i);
     }
-    return false;
+    return Array.prototype.indexOf.call(this, item, fromIndex) !== -1;
   };
 }
