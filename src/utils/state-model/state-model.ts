4966f7d7732c61ccb684853e67461475fb2992e7
apalanevich <spalanevich@mail.ru>
2021-03-07 18:12:25 +0300

    feat(state-model): add setting/getting attributes from root

--- src/utils/state-model/state-model.ts
+++ src/utils/state-model/state-model.ts
@@ -1,3 +1,30 @@
+import {TraversingQuery} from "@exadel/esl";
+
 export class UIPStateModel {
-  html: string;
+  public html: string;
+
+  protected get root(): Element {
+    return new DOMParser().parseFromString(this.html, 'text/html').body;
+  }
+
+  protected getTargets(target: string): Element[] {
+    return TraversingQuery.all(target, this.root);
+  }
+
+  public getAttribute(target: string, name: string): (string | null)[] {
+    return this.getTargets(target).map(el => el.getAttribute(name))
+  }
+
+  public setAttribute(target: string, name: string, value: string | boolean): void {
+    const query = this.getTargets(target);
+
+    if (typeof value === 'string') {
+      query.forEach(el => el.setAttribute(name, value));
+    }
+    else {
+      query.forEach(el => {
+        value ? el.setAttribute(name, '') : el.removeAttribute(name);
+      })
+    }
+  }
 }
