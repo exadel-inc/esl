4f1b2dca3a84928c235fc557bb66c8f5f4c53c6d
Anna Barmina <abarmina@exadel.com>
2021-06-07 11:44:00 +0200

    fix: adding config to eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,21 @@
-module.exports = (config) => {
-    config.addPassthroughCopy({
-        'pages/static/bundles': '/bundles',
-        'pages/static/assets': '/assets'
-    });
+module.exports = config => {
+  config.addPassthroughCopy({
+    'pages/static/bundles': '/bundles',
+    'pages/static/assets': '/assets',
+  });
 
-    return {
-        dir: {
-            input: 'pages/views-11ty',
-            output: 'pages/dist'
-        },
-        templateFormats: [
-            'html'
-        ],
-    };
+  const environment = process.env.ELEVENTY_ENV;
+
+  return {
+    dir: {
+      input: 'pages/views-11ty',
+      output: 'pages/dist',
+      includes: 'includes',
+      layouts: 'layouts',
+    },
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+    templateFormats: ['md', 'njk'],
+  };
 };
