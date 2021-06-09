0e002cd22807d8e82ea7f8ca4b81eaca39bc9b6a
Anna Barmina <abarmina@exadel.com>
2021-06-10 00:44:51 +0200

    feat: a command-line argument instead of the environment variable

--- pages/views-11ty/data/env.js
+++ pages/views-11ty/data/env.js
@@ -1,4 +1,4 @@
 module.exports = {
-  environment: process.env.ELEVENTY_ENV,
+  environment: process.argv.find(arg => arg.startsWith("--env="))?.split("=")[1],
   version: process.env.npm_package_version
 };
