5f800cf6aceb45ac7373fd44ff32bff94f5f14e7
Anna Barmina <abarmina@exadel.com>
2021-06-20 21:36:51 +0200

    feat: Browsersynс watches changes in styles, js and automatically opens localhost

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,24 @@ module.exports = config => {
     'pages/static/assets': 'assets',
     'pages/static/tools': '.',
   });
+
   config.addFilter("sortByName", (values) => {
     if (!values || !Array.isArray(values)) {
       console.error(`Unexpected values in "sortByName" filter: ${values}`);
       return values;
     }
     return [...values].sort((a, b) => a.data.name.localeCompare(b.data.name))
+  });
+
+  config.setBrowserSyncConfig({
+    files: [
+      'pages/dist/bundles/*.js',
+      'pages/dist/bundles/*.css',
+      'pages/dist/bundles/*.map',
+    ],
+    // TODO(abarmina): update with addGlobalData function when v1.0.0 is available
+    // we have the same function in pages/views/_data/env.js
+    open: process.argv.find(arg => arg.startsWith('--env='))?.split('=')[1] === 'development',
   });
 
   return {
