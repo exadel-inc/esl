7942a5ac2a62a64632c1f9b53a6f2478fafb03b4
AlexanderBazukevich <54510364+AlexanderBazukevich@users.noreply.github.com>
2021-07-27 12:29:49 +0300

    feat: move UIP options to attributes, rework uip-editor themes control
    PR: #122

--- src/core/root.ts
+++ src/core/root.ts
@@ -1,13 +1,20 @@
-import {ObserverCallback} from '@exadel/esl';
-import {ESLBaseElement} from '@exadel/esl/modules/esl-base-element/core';
+import {EventUtils, ObserverCallback} from '@exadel/esl';
+import {attr, ESLBaseElement} from '@exadel/esl/modules/esl-base-element/core';
 import {UIPStateModel} from './state-model';
 
 export class UIPRoot extends ESLBaseElement {
   public static is = 'uip-root';
   private _model = new UIPStateModel();
 
+  @attr() public mode: string;
+  @attr() public theme: string;
+
   public get model(): UIPStateModel {
     return this._model;
+  }
+
+  static get observedAttributes() {
+    return ['theme', 'mode'];
   }
 
   public addStateListener(listener: ObserverCallback) {
@@ -16,5 +23,19 @@ export class UIPRoot extends ESLBaseElement {
 
   public removeStateListener(listener: ObserverCallback) {
     this._model.removeListener(listener);
+  }
+
+  protected attributeChangedCallback(attrName: string, oldVal: string, newVal: string) {
+    if (oldVal !== newVal) {
+      this.classList.remove(`${oldVal}-${attrName}`);
+      this.classList.add(`${newVal}-${attrName}`);
+      EventUtils.dispatch(this, 'uip:configchange', {
+        bubbles: false,
+        detail: {
+          attribute: attrName,
+          value: newVal
+        }
+      });
+    }
   }
 }
--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -11,19 +11,28 @@ import {jsonAttr} from '@exadel/esl/modules/esl-base-element/core';
 import {UIPPlugin} from '../core/plugin';
 
 interface EditorConfig {
-  theme: string;
-  mode: string;
-  printMarginColumn: number;
-  wrap: number;
+  theme?: string;
+  mode?: string;
+  printMarginColumn?: number;
+  wrap?: number | boolean;
+}
+
+interface Theme {
+  [index: string]: string;
 }
 
 export class UIPEditor extends UIPPlugin {
   public static is = 'uip-editor';
-  public static defaultOptions = {
+  public static defaultOptions: EditorConfig = {
     theme: 'ace/theme/chrome',
     mode: 'ace/mode/html',
     printMarginColumn: -1,
     wrap: true,
+  };
+
+  static themesMapping: Theme = {
+    'uip-light': 'ace/theme/chrome',
+    'uip-dark': 'ace/theme/tomorrow_night'
   };
 
   @jsonAttr()
@@ -38,6 +47,20 @@ export class UIPEditor extends UIPPlugin {
   protected connectedCallback() {
     super.connectedCallback();
     this.initEditor();
+    this.bindEvents();
+  }
+
+  protected disconnectedCallback() {
+    this.unbindEvents();
+    super.disconnectedCallback();
+  }
+
+  protected bindEvents() {
+    this.root?.addEventListener('uip:configchange', this._onRootConfigChange);
+  }
+
+  protected unbindEvents() {
+    this.root?.removeEventListener('uip:configchange', this._onRootConfigChange);
   }
 
   protected initEditor() {
@@ -75,5 +98,18 @@ export class UIPEditor extends UIPPlugin {
   public setEditorConfig(editorConfig: EditorConfig): void {
     this.editorConfig = editorConfig;
     this.initEditorOptions();
+  }
+
+  @bind
+  protected _onRootConfigChange(e: CustomEvent) {
+    if (e.detail.attribute !== 'theme') return false;
+    const value = e.detail.value;
+    const defaultTheme = UIPEditor.defaultOptions.theme;
+
+    const theme = !Object.hasOwnProperty.call(UIPEditor.themesMapping, value)
+      ? defaultTheme
+      : UIPEditor.themesMapping[value];
+
+    this.setEditorConfig({theme});
   }
 }
