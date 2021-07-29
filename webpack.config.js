96fabbb8fe6739010589c303d8f809a53a628e44
nharshunova@exadel.com <nharshunova@exadel.com>
2021-07-29 13:47:23 +0300

    fix: add es5 output

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,79 @@
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import {resolve, dirname} from 'path';
+import {fileURLToPath} from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default {
+//TODO: optimize build
+const lessLoaderConf = {
+  test: /\.less$/i,
+  type: 'asset/resource',
+  use: [
+    'less-loader',
+    /*'postcss-loader'*/
+  ]
+};
+
+const tsToEs6Loader = {
+  test: /\.ts$/,
+  loader: 'ts-loader',
+  options: {
+    compilerOptions: {
+      target: 'ES6',
+      declaration: true
+    },
+  }
+};
+
+const tsToEs5Loader = {
+  test: /\.(ts|js)$/,
+  exclude: /node_modules\/(?!@exadel\/esl)/,
+  loader: 'ts-loader',
+  options: {
+    compilerOptions: {
+      target: 'ES5',
+      declaration: true
+    },
+  }
+};
+
+const output = {
+  library: {
+    root: 'UIP',
+    amd: 'uip',
+    commonjs: 'uip',
+  },
+  libraryTarget: 'umd',
+  path: resolve(__dirname, 'lib'),
+  filename: '[name].js',
+  assetModuleFilename: '[name].css'
+};
+
+const baseConf = {
   mode: 'development',
   devtool: 'source-map',
   entry: {
     'playground': './src/playground.ts',
-    'playground.run': './src/registration.ts'
+    'playground.run': './src/registration.ts',
   },
   resolve: {
     roots: [],
     extensions: ['.ts', '.js']
-  },
-  module: {
-    noParse: [
-      /\/brace\//
-    ],
-    rules: [{
-      test: /\.tsx?$/,
-      loader: 'ts-loader',
-      options: {
-        compilerOptions: {
-          target: 'ES6',
-          declaration: true
-        }
-      }
-    }, {
-      test: /\.less$/i,
-      type: 'asset/resource',
-      use: [
-        'less-loader',
-        /*'postcss-loader'*/
-      ]
-    }]
-  },
-  output: {
-    library: {
-      root: 'UIP',
-      amd: 'uip',
-      commonjs: 'uip',
-    },
-    libraryTarget: 'umd',
-    path: resolve(__dirname, 'lib'),
-    filename: '[name].js',
-    assetModuleFilename: '[name].css'
   }
 };
+
+export default [
+  Object.assign({}, baseConf, {
+    module: {
+      noParse: [/\/ace-builds\//, /\/js-beautify\//],
+      rules: [tsToEs5Loader, lessLoaderConf]
+    },
+    output: Object.assign({}, output, {filename: '[name].es5.js'})
+  }),
+  Object.assign({}, baseConf, {
+    module: {
+      noParse: [/\/ace-builds\//, /\/js-beautify\//],
+      rules: [tsToEs6Loader, lessLoaderConf]
+    },
+    output: Object.assign({}, output)
+  })
+]
