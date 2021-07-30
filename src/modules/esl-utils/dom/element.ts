d931b379c19cec5185e5bb686f44a21169cf14f3
Dmitry Shovchko <d.shovchko@gmail.com>
2021-07-30 15:17:05 +0300

    refactor(esl-utils): get rid of getComputedStyles from element dom helper

--- src/modules/esl-utils/dom/element.ts
+++ src/modules/esl-utils/dom/element.ts
@@ -1,13 +1,3 @@
-import {getWindow} from './window';
-
-/**
- * Get an object containing the values of all CSS properties of an element.
- * @param element - element for which to get the computed style
- * */
-export function getComputedStyle(element: Element): CSSStyleDeclaration {
-  return getWindow(element).getComputedStyle(element);
-}
-
 /**
  * Get the Element that is the root element of the document.
  * @param element - element for which to get the document element
