d583f243fdb68ef02c7b711ade7c7ad360a69d10
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-08-13 23:14:15 +0300

    feat(esl-toggleable): add jsx tag shape

--- src/modules/esl-toggleable/core.ts
+++ src/modules/esl-toggleable/core.ts
@@ -1,2 +1,4 @@
+export type {ESLToggleableShape} from './core/esl-toggleable.shape';
+export type {ESLToggleableDispatcherShape} from './core/esl-toggleable-dispatcher.shape';
 export * from './core/esl-toggleable';
 export * from './core/esl-toggleable-dispatcher';
