cc20b04bd26545f296a03ca94f2d7201abc47b99
yadamska <yadamska@exadel.com>
2021-08-17 17:05:36 +0300

    refactor(esl-panel): update attributeChangedCallback method, add memoize

--- src/modules/esl-panel/core/esl-panel-group.ts
+++ src/modules/esl-panel/core/esl-panel-group.ts
@@ -2,6 +2,7 @@ import {ExportNs} from '../../esl-utils/environment/export-ns';
 import {attr, ESLBaseElement} from '../../esl-base-element/core';
 import {afterNextRender} from '../../esl-utils/async/raf';
 import {bind} from '../../esl-utils/decorators/bind';
+import {memoize} from '../../esl-utils/decorators/memoize';
 import {CSSClassUtils} from '../../esl-utils/dom/class';
 import {ESLMediaRuleList} from '../../esl-media-query/core';
 import {TraversingQuery} from '../../esl-traversing-query/core';
@@ -43,6 +44,24 @@ export class ESLPanelGroup extends ESLBaseElement {
     return ['mode'];
   }
 
+  /** ESLMediaRuleList instance of the mode mapping */
+  @memoize()
+  public get modeRules() {
+    return this.modeRules = ESLMediaRuleList.parse(this.mode);
+  }
+  public set modeRules(rules: ESLMediaRuleList<string>) {
+    if (this._modeRules) {
+      this._modeRules.removeListener(this._onModeChange);
+    }
+    this._modeRules = rules;
+    this._modeRules.addListener(this._onModeChange);
+  }
+
+  /** @returns current mode */
+  public get currentMode(): string {
+    return this.modeRules.activeValue || '';
+  }
+
   protected connectedCallback() {
     super.connectedCallback();
     this.bindEvents();
@@ -61,7 +80,9 @@ export class ESLPanelGroup extends ESLBaseElement {
   protected attributeChangedCallback(attrName: string, oldVal: string, newVal: string) {
     if (!this.connected || oldVal === newVal) return;
     if (attrName === 'mode') {
-      this.modeRules = ESLMediaRuleList.parse(newVal);
+      this.modeRules.removeListener(this._onModeChange);
+      memoize.clear(this, 'modeRules');
+      this.modeRules.addListener(this._onModeChange);
       this.updateMode();
     }
   }
@@ -82,6 +103,24 @@ export class ESLPanelGroup extends ESLBaseElement {
     this.removeEventListener('transitionend', this._onTransitionEnd);
   }
 
+  /** Update element state according to current mode */
+  protected updateMode() {
+    this.setAttribute('view', this.currentMode);
+    const $target = TraversingQuery.first(this.modeClsTarget, this);
+    if (!$target) return;
+    ESLPanelGroup.supportedModes.forEach((mode) => {
+      $target.classList.toggle(`esl-${mode}-view`, this.currentMode === mode);
+    });
+
+    // TODO: refactor
+    ESLPanel.registered.then(() => {
+      this.$panels.forEach((panel) => {
+        const shouldOpen = this.currentMode === 'open' || panel.initiallyOpened;
+        panel.toggle(shouldOpen, {initiator: 'group', activator: this});
+      });
+    });
+  }
+
   /** @returns Panels that are processed by the current panel group */
   public get $panels(): ESLPanel[] {
     const els = Array.from(this.querySelectorAll(ESLPanel.is));
@@ -93,46 +132,23 @@ export class ESLPanelGroup extends ESLBaseElement {
     return this.$panels.filter((el: ESLPanel) => el.open);
   }
 
+  /** @returns Whether the collapse/expand animation should be handheld by the group */
+  public get shouldCollapse() {
+    const noCollapseModes = this.noCollapse.split(',').map((mode) => mode.trim());
+    return !noCollapseModes.includes('all') && !noCollapseModes.includes(this.currentMode);
+  }
+
+  /** @returns Action params config that is used by controlled {@link ESLPanel}s */
+  public get panelConfig(): PanelActionParams {
+    return {
+      noCollapse: !this.shouldCollapse || (this.currentMode === 'tabs')
+    };
+  }
+
   /** Condition-guard to check if the passed target is a Panel that should be controlled by the Group */
   public includesPanel(target: any): target is ESLPanel {
     if (!(target instanceof ESLPanel)) return false;
     return target.$group === this;
-  }
-
-  /** Process {@link ESLPanel} pre-show event */
-  @bind
-  protected _onBeforeShow(e: CustomEvent) {
-    const panel = e.target;
-    if (!this.includesPanel(panel)) return;
-    this.$activePanels.forEach((el) => (el !== panel) && el.hide());
-  }
-
-  /** Process {@link ESLPanel} show event */
-  @bind
-  protected _onShow(e: CustomEvent) {
-    const panel = e.target;
-    if (!this.includesPanel(panel)) return;
-    if (this.currentMode !== 'tabs') return;
-
-    this.beforeAnimate();
-    if (this.shouldCollapse) {
-      this.onAnimate(this._previousHeight, panel.initialHeight);
-    } else {
-      afterNextRender(() => this.afterAnimate());
-    }
-  }
-
-  /** Process {@link ESLPanel} pre-hide event */
-  @bind
-  protected _onBeforeHide(e: CustomEvent) {
-    // TODO: refactor
-    if (this.currentMode === 'open') {
-      e.preventDefault();
-      return;
-    }
-    const panel = e.target;
-    if (!this.includesPanel(panel)) return;
-    this._previousHeight = this.offsetHeight;
   }
 
   /** Animate the height of the component */
@@ -171,6 +187,42 @@ export class ESLPanelGroup extends ESLBaseElement {
     this._fallbackTimer = window.setTimeout(() => this.afterAnimate(), time);
   }
 
+  /** Process {@link ESLPanel} pre-show event */
+  @bind
+  protected _onBeforeShow(e: CustomEvent) {
+    const panel = e.target;
+    if (!this.includesPanel(panel)) return;
+    this.$activePanels.forEach((el) => (el !== panel) && el.hide());
+  }
+
+  /** Process {@link ESLPanel} show event */
+  @bind
+  protected _onShow(e: CustomEvent) {
+    const panel = e.target;
+    if (!this.includesPanel(panel)) return;
+    if (this.currentMode !== 'tabs') return;
+
+    this.beforeAnimate();
+    if (this.shouldCollapse) {
+      this.onAnimate(this._previousHeight, panel.initialHeight);
+    } else {
+      afterNextRender(() => this.afterAnimate());
+    }
+  }
+
+  /** Process {@link ESLPanel} pre-hide event */
+  @bind
+  protected _onBeforeHide(e: CustomEvent) {
+    // TODO: refactor
+    if (this.currentMode === 'open') {
+      e.preventDefault();
+      return;
+    }
+    const panel = e.target;
+    if (!this.includesPanel(panel)) return;
+    this._previousHeight = this.offsetHeight;
+  }
+
   /** Catching CSS transition end event to start post-animate processing */
   @bind
   protected _onTransitionEnd(e?: TransitionEvent) {
@@ -179,61 +231,10 @@ export class ESLPanelGroup extends ESLBaseElement {
     }
   }
 
-  /** @returns Whether the collapse/expand animation should be handheld by the group */
-  public get shouldCollapse() {
-    const noCollapseModes = this.noCollapse.split(',').map((mode) => mode.trim());
-    return !noCollapseModes.includes('all') && !noCollapseModes.includes(this.currentMode);
-  }
-
-  /** @returns Action params config that is used by controlled {@link ESLPanel}s */
-  public get panelConfig(): PanelActionParams {
-    return {
-      noCollapse: !this.shouldCollapse || (this.currentMode === 'tabs')
-    };
-  }
-
-  /** ESLMediaRuleList instance of the mode mapping */
-  public get modeRules() {
-    if (!this._modeRules) {
-      this.modeRules = ESLMediaRuleList.parse(this.mode);
-    }
-    return this._modeRules;
-  }
-  public set modeRules(rules: ESLMediaRuleList<string>) {
-    if (this._modeRules) {
-      this._modeRules.removeListener(this._onModeChange);
-    }
-    this._modeRules = rules;
-    this._modeRules.addListener(this._onModeChange);
-  }
-
-  /** @returns current mode */
-  public get currentMode(): string {
-    return this.modeRules.activeValue || '';
-  }
-
   /** Handles mode change */
   @bind
   protected _onModeChange() {
     this.updateMode();
-  }
-
-  /** Update element state according to current mode */
-  protected updateMode() {
-    this.setAttribute('view', this.currentMode);
-    const $target = TraversingQuery.first(this.modeClsTarget, this);
-    if (!$target) return;
-    ESLPanelGroup.supportedModes.forEach((mode) => {
-      $target.classList.toggle(`esl-${mode}-view`, this.currentMode === mode);
-    });
-
-    // TODO: refactor
-    ESLPanel.registered.then(() => {
-      this.$panels.forEach((panel) => {
-        const shouldOpen = this.currentMode === 'open' || panel.initiallyOpened;
-        panel.toggle(shouldOpen, {initiator: 'group', activator: this});
-      });
-    });
   }
 }
 
@@ -241,6 +242,7 @@ declare global {
   export interface ESLLibrary {
     PanelGroup: typeof ESLPanelGroup;
   }
+
   export interface HTMLElementTagNameMap {
     'esl-panel-group': ESLPanelGroup;
   }
