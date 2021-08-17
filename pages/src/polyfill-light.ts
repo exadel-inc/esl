2ca8d0b340512c914b9cd6245eed99d3fabe6048
PetrikAnna-Mariia <apetryk@exadel.com>
2021-08-17 18:46:20 +0300

    feat: add resize observer polyfill

--- pages/src/polyfill-light.ts
+++ pages/src/polyfill-light.ts
@@ -3,7 +3,9 @@
  */
 import 'intersection-observer/intersection-observer';
 
-import {ResizeObserver} from '@juggle/resize-observer';
-window.ResizeObserver = window.ResizeObserver || ResizeObserver;
+// import {ResizeObserver} from '@juggle/resize-observer';
+import { ResizeObserverPolyfill} from '../../src/polyfills/polyfillResizeObserver'
+
+window.ResizeObserver = window.ResizeObserver || ResizeObserverPolyfill;
 
 import '@webcomponents/custom-elements/custom-elements.min';
---
+++ src/polyfills/polyfillResizeObserver.ts
@@ -0,0 +1,25 @@
+import {bind} from '../modules/all';
+
+export class ResizeObserverPolyfill{
+  private els = new Set<HTMLElement>();
+
+  constructor(private callback: (els:HTMLElement[])=>void){
+  }
+  @bind
+  private onChange(){
+    this.callback([...this.els]);
+  }
+  observe(el:HTMLElement){
+    this.els.add(el);
+    window.addEventListener('resize', this.onChange);
+  }
+  unobserve(el:HTMLElement){
+    this.els.delete(el);
+    if(this.els.size === 0){
+      window.removeEventListener('resize', this.onChange);
+    }
+  }
+  disconnect(){
+    window.removeEventListener('resize', this.onChange);
+  }
+}
