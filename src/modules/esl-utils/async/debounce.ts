3a527747791c94dc41eabf9de026bc569030b232
PetrikAnna-Mariia <apetryk@exadel.com>
2021-08-18 20:09:39 +0300

    feat: debounced method API changes
    BREAKING CHANGE:
    decorated `debouncedMethod = debounce(fn)` call no longer returns Promise use 'debouncedMethod.promise' instead

--- src/modules/esl-utils/async/debounce.ts
+++ src/modules/esl-utils/async/debounce.ts
@@ -23,7 +23,7 @@ export function debounce<F extends AnyToAnyFnSignature>(fn: F, wait = 10): Debou
   let timeout: number | null = null;
   let deferred: Deferred<ReturnType<F>> | null = null;
 
-  function debouncedSubject(...args: any[]) {
+  function debouncedSubject(...args: any[]){
     deferred = deferred || createDeferred();
     (typeof timeout === 'number') && clearTimeout(timeout);
     timeout = window.setTimeout(() => {
@@ -32,7 +32,6 @@ export function debounce<F extends AnyToAnyFnSignature>(fn: F, wait = 10): Debou
       deferred!.resolve(fn.apply(this, args));
       deferred = null;
     }, wait);
-    return deferred.promise;
   }
   function cancel() {
     (typeof timeout === 'number') && clearTimeout(timeout);
