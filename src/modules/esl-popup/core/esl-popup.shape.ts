180f411ceef38d3b558de4c5b4c902dc1cb8a1eb
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-09-15 03:23:44 +0300

    feat: add TSX shapes for `ESLPopup`

---
+++ src/modules/esl-popup/core/esl-popup.shape.ts
@@ -0,0 +1,26 @@
+import type {ESLToggleableTagShape} from '../../esl-toggleable/core/esl-toggleable.shape';
+import type {ESLPopup} from './esl-popup';
+import type {PositionType} from './esl-popup-position';
+
+/**
+ * Tag declaration interface of {@link ESLPopup} element
+ * Used for TSX declaration
+ */
+export interface ESLPopupShape extends ESLToggleableTagShape<ESLPopup> {
+  /** Popup behavior if it does not fit in the window ('fit' by default) */
+  behavior?: string;
+  /**
+   * Popup position relative to trigger.
+   * Currently supported: 'top', 'bottom', 'left', 'right' position types ('top' by default)
+   */
+  position?: PositionType;
+}
+
+declare global {
+  namespace JSX {
+    export interface IntrinsicElements {
+      /** {@link ESLPopup} custom tag */
+      'esl-popup': ESLPopupShape;
+    }
+  }
+}
