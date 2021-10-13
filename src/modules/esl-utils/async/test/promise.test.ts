03eff4c08064024025a16186c57eab6d9d403028
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-10-14 02:41:22 +0300

    feat(esl-utils): add `repeatSequence` utility to call async function in chain n-th count of times

--- src/modules/esl-utils/async/test/promise.test.ts
+++ src/modules/esl-utils/async/test/promise.test.ts
@@ -1,4 +1,4 @@
-import {createDeferred, PromiseUtils, promisifyEvent, promisifyMarker, promisifyTimeout, rejectPromise, resolvePromise, tryUntil} from '../promise';
+import {createDeferred, PromiseUtils, promisifyEvent, promisifyMarker, promisifyTimeout, rejectPromise, repeatSequence, resolvePromise, tryUntil} from '../promise';
 
 describe('promise utils', () => {
   beforeAll(() =>  jest.useFakeTimers());
@@ -105,6 +105,36 @@ describe('promise utils', () => {
       const promise$ = tryUntil(() => false, 3, 100);
       jest.advanceTimersByTime(500);
       return expect(promise$).rejects.toBeTruthy();
+    });
+  });
+
+  describe('repeatSequence', () => {
+    test('basic scenario', async () => {
+      let times = 0;
+      const f = async () => ++times;
+
+      const result$ = repeatSequence(f, 4);
+      // Initial call side-effects
+      expect(times).toBe(0);
+      // Result validation
+      const res = await result$;
+      expect(res).toBe(4);
+      expect(times).toBe(4);
+    });
+    test('failed chain', () => {
+      let times = 0;
+      const err = new Error('test');
+      const f = async () => {
+        if (++times > 2 ) throw err;
+      }
+
+      return repeatSequence(f, 4).then(
+        () => expect(1).toBe(0),
+        (e) => {
+          expect(e).toBe(err);
+          expect(times).toBe(3);
+        }
+      );
     });
   });
 
