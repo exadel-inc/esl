dff86e9e6fd2f9eeca58373853ade9477856b795
Kien <kiennguyenchi11@gmail.com>
2021-10-15 14:16:23 -0400

    add transform.js

--- pages/views/_controllers/transform.js
+++ pages/views/_controllers/transform.js
@@ -1,5 +1,5 @@
 const htmlmin = require("html-minifier");
-
+const { isDev } = require('../_data/env');
 module.exports = function(eleventyConfig) {
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if( this.outputPath && this.outputPath.endsWith(".html") ) {
