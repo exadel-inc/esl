ab1c550292f8a19bdb4deb1458e2f0b9b7a58ca3
yadamska <yadamska@exadel.com>
2021-10-20 14:42:58 +0300

    feat: add TSX shapes for ESLSelect and ESLSelectList

---
+++ src/modules/esl-forms/esl-select-list/core/esl-select-list.shape.ts
@@ -0,0 +1,26 @@
+import type {ESLBaseElementShape} from '../../../esl-base-element/core/esl-base-element.shape';
+import type {ESLSelectList} from './esl-select-list';
+
+/**
+ * Tag declaration interface of {@link ESLSelectList} element
+ * Used for TSX declaration
+ */
+export interface ESLSelectListTagShape extends ESLBaseElementShape<ESLSelectList> {
+  /** Define label to select all options text ('Select All' by default) */
+  'select-all-label'?: string;
+
+  /** Define state marker */
+  disabled?: boolean;
+
+  /** Define marker for selecting items to be pinned to the top of the list */
+  'pin-selected'?: boolean;
+}
+
+declare global {
+  namespace JSX {
+    export interface IntrinsicElements {
+      /** {@link ESLSelectList} custom tag */
+      'esl-select-list': ESLSelectListTagShape;
+    }
+  }
+}
