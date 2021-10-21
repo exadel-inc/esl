c486c7b3b5425da09f81c07af2275db7d7dfa826
Kien <kiennguyenchi11@gmail.com>
2021-10-21 17:23:21 -0400

    edit .eleventy to add transform.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const htmlmin = require('html-minifier');
 const { isDev } = require('./pages/views/_data/env');
 const { markdown } = require('./pages/views/_data/markdown');
 const { MDRenderer } = require('./pages/views/_data/md-render');
+const { transform } = require('./pages/views/_controllers/transform') 
 
 module.exports = (config) => {
   config.addWatchTarget('src/**/*.md');
@@ -15,6 +16,8 @@ module.exports = (config) => {
   config.setLibrary('md', markdown);
 
   config.addNunjucksAsyncShortcode('mdRender', MDRenderer.render);
+
+  config.addTransform('transform', transform)
 
   config.addFilter('sortByName', (values) => {
     if (!values || !Array.isArray(values)) {
