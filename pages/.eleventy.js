ebee5fc221029efe9453a27d16084687b806e190
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-10-24 05:33:32 +0300

    chore(pages): move 11ty config and build basepath to `pages` dir

--- pages/.eleventy.js
+++ pages/.eleventy.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const chalk = require('chalk');
-const {isDev} = require('./pages/11ty/env.config');
+const {isDev} = require('./11ty/env.config');
 
 module.exports = (config) => {
   // Init all 11ty config modules
-  const cfgFiles = fs.readdirSync('./pages/11ty');
+  const cfgFiles = fs.readdirSync('./11ty');
   for (const file of cfgFiles) {
     if (file.startsWith('_')) return;
     try {
       console.info(chalk.blue(`Initializing module: ${file}`));
-      require('./pages/11ty/' + file)(config);
+      require('./11ty/' + file)(config);
       console.info(chalk.green(`Module ${file} initialized.`));
     } catch (e) {
       console.error(chalk.red(`Module ${file} initialization failed`));
@@ -17,32 +17,32 @@ module.exports = (config) => {
     }
   }
 
-  config.addWatchTarget('src/**/*.md');
+  config.addWatchTarget('../src/**/*.md');
 
   config.addPassthroughCopy({
-    'pages/static/assets': 'assets',
-    'pages/static/tools': '.',
+    'static/assets': 'assets',
+    'static/tools': '.'
   });
 
   config.setBrowserSyncConfig({
     files: [
-      'pages/dist/bundles/*.js',
-      'pages/dist/bundles/*.css',
-      'pages/dist/bundles/*.map',
+      'dist/bundles/*.js',
+      'dist/bundles/*.css',
+      'dist/bundles/*.map'
     ],
-    open: isDev,
+    open: isDev
   });
 
   return {
     dir: {
-      input: 'pages/views',
-      output: 'pages/dist',
-      layouts: '_layouts',
+      input: 'views',
+      output: 'dist',
+      layouts: '_layouts'
     },
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     passthroughFileCopy: true,
     templateFormats: ['md', 'njk'],
-    pathPrefix: '/esl/',
+    pathPrefix: '/esl/'
   };
 };
