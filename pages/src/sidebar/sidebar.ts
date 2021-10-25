32cfe9ab1814ecddc21f3af22f0a4d97ea111201
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-10-25 16:26:04 +0300

    refactor(pages): refactoring for sidebar (wip)

--- pages/src/sidebar/sidebar.ts
+++ pages/src/sidebar/sidebar.ts
@@ -1,8 +1,45 @@
-import { ESLPanel } from "../../../src/modules/all";
+import {ready} from '../../../src/modules/esl-utils/decorators/ready';
+import {ESLToggleable} from '../../../src/modules/esl-toggleable/core/esl-toggleable';
+import type {ToggleableActionParams} from '../../../src/modules/esl-toggleable/core/esl-toggleable';
+import {prop} from '../../../src/modules/esl-utils/decorators/prop';
 
-document!.getElementById("list-trigger")!.addEventListener("click", function () {
-  const panels = Array.from(document.querySelectorAll('.sb-dropdown-content')) as ESLPanel[];
-  panels.filter((panel) => {
-    panel.open && panel.toggle();
-  })
-});
+
+export class ESLSidebar extends ESLToggleable {
+  static is = 'esl-sidebar';
+
+  @prop() public submenus: string = '.sb-dropdown-content';
+
+  public get $submenus(): ESLToggleable[] {
+    return Array.from(this.querySelectorAll(this.submenus));
+  }
+
+  @ready
+  protected connectedCallback() {
+    super.connectedCallback();
+  }
+
+  protected onShow(params: ToggleableActionParams) {
+    super.onShow(params);
+    this.expandActive();
+  }
+  protected onHide(params: ToggleableActionParams) {
+    super.onHide(params);
+    this.collapseAll();
+  }
+
+  public collapseAll() {
+    this.$submenus.forEach((menu) => menu.hide());
+  }
+  public expandActive() {
+    this.$submenus
+      .filter((menu) => !!menu.querySelector('.nav-item-selected'))
+      .forEach((menu) => menu.show());
+  }
+
+  protected updateA11y() {
+    const targetEl = this.$a11yTarget;
+    if (!targetEl) return;
+    targetEl.setAttribute('aria-expanded', String(this.open));
+  }
+}
+ESLSidebar.register();
