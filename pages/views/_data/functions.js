8357a209b612fd3835476592a722f7f925c8716c
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-10-29 04:56:33 +0300

    refactor(gh-pages): fix semantic and related section positioning

--- pages/views/_data/functions.js
+++ pages/views/_data/functions.js
@@ -5,5 +5,11 @@ module.exports = {
 
   isActivePath(url, collection) {
     return collection && url.includes(collection);
+  },
+
+  findItemsByName(names, collection) {
+    return names
+      .map((name) => collection.find((item) => item.fileSlug === name))
+      .filter((item) => !!item);
   }
 };
