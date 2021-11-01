a2813f5bdff4621afc66217d03c6ba4834102326
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-11-01 03:10:56 +0300

    refactor(gh-pages): replace `highlight.js` with `prism.js`

--- pages/11ty/markdown.lib.js
+++ pages/11ty/markdown.lib.js
@@ -1,16 +1,24 @@
-const hljs = require('highlight.js');
-const markdownLib = require('markdown-it');
+const md = require('markdown-it');
+const Prism = require('prismjs');
 
-const markdown = markdownLib({
+require('prismjs/components/prism-bash');
+require('prismjs/components/prism-css');
+require('prismjs/components/prism-less');
+require('prismjs/components/prism-javascript');
+require('prismjs/components/prism-typescript');
+
+const highlight = (str, lang) => {
+  try {
+    lang = lang || 'text';
+    if (!Prism.languages[lang]) return `<!-- Error: Unsupported language '${lang}' -->`;
+    return Prism.highlight(str, Prism.languages[lang], lang);
+  } catch { }
+  return '';
+};
+
+const markdown = md({
   html: true,
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return hljs.highlight(str, { language: lang }).value;
-      } catch { }
-    }
-    return '';
-  }
+  highlight
 });
 
 module.exports = (config) => {
