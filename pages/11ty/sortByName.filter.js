56170e60aa77f571fbbc6d00f9c77d5baed0302b
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-11-02 03:11:15 +0300

    refactor(gh-pages): extend sort filters

--- pages/11ty/sortByName.filter.js
+++
@@ -1,9 +0,0 @@
-module.exports = (config) => {
-  config.addFilter('sortByName', (values) => {
-    if (!values || !Array.isArray(values)) {
-      console.error(`Unexpected values in 'sortByName' filter: ${values}`);
-      return values;
-    }
-    return [...values].sort((a, b) => a.data.name.localeCompare(b.data.name));
-  });
-};
