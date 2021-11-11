e3693507b65194ec952ada37d5255bc6f9d48b31
PetrikAnna-Mariia <apetryk@exadel.com>
2021-11-11 13:49:53 +0200

    feat(esl-animate): make a new module "esl-animate" to animate elements on viewport intersecting

---
+++ src/modules/esl-animate/README.md
@@ -0,0 +1,7 @@
+[ESL](https://exadel-inc.github.io/esl/) Animate
+
+Version: _1.0.0-beta_.
+
+Authors: _Alexey Stsefanovich (ala'n), Anna-Mariia Petryk_.
+
+**_Important Notice: the component is under beta version, it is tested and ready to use but be aware of its potential critical API changes._**
---
+++ src/modules/esl-animate/core/esl-animate.ts
@@ -0,0 +1,84 @@
+import {memoize} from '../../esl-utils/decorators/memoize';
+import {bind} from '../../esl-utils/decorators/bind';
+
+
+const options: IntersectionObserverInit  = {
+  threshold: [0.5]
+};
+
+interface ESLAnimateConfig {
+  group?: boolean;
+  repeat?: boolean;
+  delete?: boolean;
+}
+
+export class ESLAnimate {
+
+  private _markedElements: Element[] = [];
+  private _io = new IntersectionObserver(this.onIntersect, options);
+  private _configMap = new WeakMap<Element, ESLAnimateConfig>();
+  private _defaultConfig: ESLAnimateConfig = {repeat: false, group: false, delete: false};
+
+  @memoize()
+  static get instance() {
+    return new ESLAnimate();
+  }
+
+  configFor(el: Element): ESLAnimateConfig | undefined {
+    return this._configMap.get(el);
+  }
+
+  /**
+  * Intersection observable callback
+  */
+  @bind
+  private onIntersect(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      const target = entry.target;
+      const config = this.configFor(target);
+      if (entry.isIntersecting) {
+        this._markedElements.push(target);
+        this.postponedAnimate();
+        if (!config?.repeat) {
+          observer.unobserve(target);
+          config?.delete = true;
+        }
+      } else {
+        if (config?.repeat) {
+          target.setAttribute('esl-animate', 'true');
+        }
+      }
+    });
+  }
+
+  /**
+  * Method to show up HTMLElement
+  */
+  private handleAnimation() {
+    let counter = 0;
+    this._markedElements.forEach(el => {
+      const config = this.configFor(el);
+      if (config?.group) {
+        setTimeout(() => el.setAttribute('esl-animate', 'false'), 100 * counter);
+        counter++;
+      } else {
+        el.setAttribute('esl-animate', 'false');
+      }
+      config?.delete && this._configMap.delete(el);
+    });
+    this._markedElements = [];
+  }
+
+  private postponedAnimate() {
+    setTimeout(() => this.handleAnimation(), 100);
+  }
+
+  public subscribe(el: HTMLElement, config?: ESLAnimateConfig) {
+    this._configMap.set(el, Object.assign(this._defaultConfig, config));
+    this._io.observe(el);
+  }
+
+  public unsubscribe(el: HTMLElement) {
+    this._io.unobserve(el);
+  }
+}
