80b8aa04068f9d047c459769ce32a71a5ddaf0d9
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2021-11-19 05:07:46 +0300

    feat(esl-utils): fix `isPrimitive` check and `isObject` return type, add `isArrayLike`

--- src/modules/esl-utils/misc/object.ts
+++ src/modules/esl-utils/misc/object.ts
@@ -1,9 +1,30 @@
-export const isObject = (obj: any): obj is Record<string, any> => obj && typeof obj === 'object';
+/** Checks that passed value is object, but not a callable-object (function) */
+export const isObject = (obj: any): obj is Record<string, any> => !!obj && typeof obj === 'object';
+/** Check that passed value is an object or function */
 export const isObjectLike = (obj: any) => isObject(obj) || typeof obj === 'function';
-export const isPrimitive = (obj: any): obj is string | number | boolean =>
-  typeof obj === 'string' || typeof obj === 'number' || typeof obj === 'boolean';
-
+/** Checks if the passed value is primitive */
+export const isPrimitive = (obj: any): obj is string | number | boolean | symbol | undefined | null =>
+  obj === null ||
+  typeof obj === 'undefined' ||
+  typeof obj === 'string' ||
+  typeof obj === 'number' ||
+  typeof obj === 'boolean' ||
+  typeof obj === 'symbol';
+/** Check that passed object is prototype of some class */
 export const isPrototype = (obj: any) => Object.hasOwnProperty.call(obj, 'constructor');
+
+/** Array-like type definition */
+export type ArrayLike<T = any> = {
+  [key: number]: T;
+  length: number;
+};
+/** Checks that passed object is array-like */
+export const isArrayLike = (value: any): value is ArrayLike => {
+  if (!value || typeof value !== 'object') return false;
+  if (Array.isArray(value)) return true;
+  if (typeof value.length !== 'number' || value.length < 0) return false;
+  return !value.length || Object.hasOwnProperty.call(value, value.length - 1);
+};
 
 export type CopyPredicate = (key: string, value: any) => boolean;
 
