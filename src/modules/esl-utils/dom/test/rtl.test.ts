b5b0d3f1acc13f2fd99217b8737a0720f21753f0
PetrikAnna-Mariia <apetryk@exadel.com>
2021-12-01 11:03:49 +0200

    test: add tests for scrollType

--- src/modules/esl-utils/dom/test/rtl.test.ts
+++ src/modules/esl-utils/dom/test/rtl.test.ts
@@ -28,13 +28,20 @@ describe('RTLUtils', () => {
 
       jest.spyOn(el, 'scrollWidth', 'get').mockImplementation(() => 1000);
       jest.spyOn(el, 'clientWidth', 'get').mockImplementation(() => 100);
+      jest.spyOn(el, 'scrollLeft', 'get').mockImplementation(() => 200);
+      jest.spyOn(RTLUtils, 'scrollType', 'get').mockImplementation(() => 'negative');
 
-      expect(RTLUtils.normalizeScrollLeft(el)).toBe(0);
+      expect(RTLUtils.normalizeScrollLeft(el)).toBe(200);
       expect(RTLUtils.normalizeScrollLeft(el, 50)).toBe(50);
       expect(RTLUtils.normalizeScrollLeft(el, 50, false)).toBe(50);
-      expect(RTLUtils.normalizeScrollLeft(el, null, false)).toBe(0);
+      expect(RTLUtils.normalizeScrollLeft(el, null, false)).toBe(200);
+      expect(RTLUtils.normalizeScrollLeft(el, 50, true)).toBe(950);
+      expect(RTLUtils.normalizeScrollLeft(el, null, true)).toBe(1100);
+
+      jest.spyOn(RTLUtils, 'scrollType', 'get').mockImplementation(() => 'reverse');
+
       expect(RTLUtils.normalizeScrollLeft(el, 50, true)).toBe(850);
-      expect(RTLUtils.normalizeScrollLeft(el, null, true)).toBe(900);
+      expect(RTLUtils.normalizeScrollLeft(el, null, true)).toBe(700);
     });
   });
 
