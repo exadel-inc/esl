f1ddb52cb77bca0792fe920699c8ee357938dff4
Anna Barmina <abarmina@exadel.com>
2021-12-16 01:00:41 +0100

    refactor(gh-pages): fix lint errors for Search Box component

--- pages/src/navigation/header/header-search.ts
+++ pages/src/navigation/header/header-search.ts
@@ -3,32 +3,38 @@ import {
   afterNextRender,
   CSSClassUtils,
   ESLToggleable,
-  ToggleableActionParams,
   TraversingQuery,
   FormatUtils
 } from '../../../../src/modules/all';
-import { attr } from '../../../../src/modules/esl-base-element/core';
+import { attr, boolAttr } from '../../../../src/modules/esl-base-element/core';
+
+import type {ToggleableActionParams} from '../../../../src/modules/all';
 
 export class ESLDemoSearchBox extends ESLToggleable {
   static is = 'esl-d-search-box';
 
   @attr() public postCls: string;
   @attr() public postClsDelay: string;
-  @attr({defaultValue: '::find(input)'}) public autofocus: string;
+  @attr({defaultValue: '::find(input)'}) public firstFocusable: string;
+
+  @boolAttr() public autofocus: boolean;
 
   @prop() public closeOnOutsideAction = true;
 
   public onShow(params: ToggleableActionParams): void {
     CSSClassUtils.add(this, this.postCls);
-    afterNextRender(() => {
-      super.onShow(params);
-      const $focusEl = TraversingQuery.first(this.autofocus, this) as HTMLElement;
-      $focusEl && $focusEl.focus();
-    });
+    afterNextRender(() => super.onShow(params));
+    if (this.autofocus) {
+      const $focusEl = TraversingQuery.first(this.firstFocusable, this) as HTMLElement;
+      $focusEl && window.setTimeout(() => $focusEl.focus(), FormatUtils.parseNumber(this.postClsDelay));
+    };
   }
 
   public onHide(params: ToggleableActionParams): void {
     super.onHide(params);
-    window.setTimeout(() => CSSClassUtils.remove(this, this.postCls), FormatUtils.parseNumber(this.postClsDelay));
+    window.setTimeout(() => {
+      CSSClassUtils.remove(this, this.postCls);
+      this.activator?.focus({preventScroll: true});
+    }, FormatUtils.parseNumber(this.postClsDelay));
   }
 }
