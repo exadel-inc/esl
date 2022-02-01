9fe21b19daa87c95a2153ef932051ab8d27b1f81
Dmitry Shovchko <d.shovchko@gmail.com>
2022-02-01 16:42:40 +0200

    fix(esl-footnotes): fix esl-note-group attributes propagation

--- src/modules/esl-footnotes/core/esl-note-group.ts
+++ src/modules/esl-footnotes/core/esl-note-group.ts
@@ -7,18 +7,11 @@ export class ESLNoteGroup extends ESLBaseElement {
   static is = 'esl-note-group';
   static readonly noteTag: string = 'esl-note';
 
-  static get observedAttributes(): string[] {
-    return ['container', 'ignore-footnotes'];
-  }
-
-  /** Tag name of the note element */
-  protected get _childTagName(): string {
-    return (this.constructor as typeof ESLNoteGroup).noteTag;
-  }
+  static readonly attributes: string[] = ['container', 'ignore-footnotes'];
 
   /** Array of child notes */
   public get notes(): HTMLElement[] {
-    return Array.from(this.querySelectorAll(this._childTagName));
+    return Array.from(this.querySelectorAll((this.constructor as typeof ESLNoteGroup).noteTag));
   }
 
   @ready
@@ -27,27 +20,15 @@ export class ESLNoteGroup extends ESLBaseElement {
     this.propagateAttributes();
   }
 
-  protected attributeChangedCallback(attrName: string, oldVal: string, newVal: string): void {
-    if (!this.connected || oldVal === newVal) return;
-    this.getControlledNotes(attrName)
-      .forEach((el) => newVal !== null ? el.setAttribute(attrName, newVal) : el.removeAttribute(attrName));
-  }
-
-  /** Gets an array of controlled child notes */
-  protected getControlledNotes(attrName?: string): HTMLElement[] {
-    const attrSel = attrName ? `[${attrName}]` : '';
-    const groupSel = (this.constructor as typeof ESLNoteGroup).is + attrSel;
-    return this.notes.filter((note) => note.closest(groupSel) === this);
-  }
-
   /** Propagates attributes values from the list to all child notes */
   protected propagateAttributes(): void {
-    const attrs = (this.constructor as typeof ESLNoteGroup).observedAttributes;
-    attrs.forEach((attrName) => {
-      const value = this.getAttribute(attrName);
-      if (value === null) return;
-      this.getControlledNotes(attrName)
-        .forEach((el) => el.setAttribute(attrName, value));
+    const attrs = (this.constructor as typeof ESLNoteGroup).attributes;
+    this.notes.forEach((el) => {
+      attrs.forEach((attrName) => {
+        const value = this.getAttribute(attrName);
+        if (value === null) return;
+        el.setAttribute(attrName, value);
+      });
     });
   }
 }
