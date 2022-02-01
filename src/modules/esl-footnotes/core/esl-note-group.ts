7f3d569c3fc13353b7e4007de3142b7d537e810f
Dmitry Shovchko <d.shovchko@gmail.com>
2022-02-01 13:41:32 +0200

    fix(esl-footnotes): fix esl-note-group attributes propagation

--- src/modules/esl-footnotes/core/esl-note-group.ts
+++ src/modules/esl-footnotes/core/esl-note-group.ts
@@ -2,14 +2,6 @@ import {ExportNs} from '../../esl-utils/environment/export-ns';
 import {ESLBaseElement} from '../../esl-base-element/core';
 import {ready} from '../../esl-utils/decorators/ready';
 
-const setAttribute = ($el: HTMLElement, attrName: string, attrValue: string | null): void => {
-  if (attrValue !== null) {
-    $el.setAttribute(attrName, attrValue);
-  } else {
-    $el.removeAttribute(attrName);
-  }
-};
-
 @ExportNs('NoteGroup')
 export class ESLNoteGroup extends ESLBaseElement {
   static is = 'esl-note-group';
@@ -37,17 +29,25 @@ export class ESLNoteGroup extends ESLBaseElement {
 
   protected attributeChangedCallback(attrName: string, oldVal: string, newVal: string): void {
     if (!this.connected || oldVal === newVal) return;
-    this.notes.forEach((el) => setAttribute(el, attrName, newVal));
+    this.getControlledNotes(attrName)
+      .forEach((el) => newVal !== null ? el.setAttribute(attrName, newVal) : el.removeAttribute(attrName));
+  }
+
+  /** Gets an array of controlled child notes */
+  protected getControlledNotes(attrName?: string): HTMLElement[] {
+    const attrSel = attrName ? `[${attrName}]` : '';
+    const groupSel = (this.constructor as typeof ESLNoteGroup).is + attrSel;
+    return this.notes.filter((note) => note.closest(groupSel) === this);
   }
 
   /** Propagates attributes values from the list to all child notes */
   protected propagateAttributes(): void {
     const attrs = (this.constructor as typeof ESLNoteGroup).observedAttributes;
-    this.notes.forEach((el): void => {
-      attrs.forEach((attrName) => {
-        const value = this.getAttribute(attrName);
-        setAttribute(el, attrName, value);
-      });
+    attrs.forEach((attrName) => {
+      const value = this.getAttribute(attrName);
+      if (value === null) return;
+      this.getControlledNotes(attrName)
+        .forEach((el) => el.setAttribute(attrName, value));
     });
   }
 }
