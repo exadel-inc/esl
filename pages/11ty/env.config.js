47ac88c3eb9423ae4eb42ef1f5267e0a78838d26
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2022-02-04 21:12:04 +0300

    chore(gh-pages): extend env global dataa with the current date

--- pages/11ty/env.config.js
+++ pages/11ty/env.config.js
@@ -1,8 +1,9 @@
 const env = process.argv.find(arg => arg.startsWith('--env='))?.split('=')[1];
 const version = process.env.npm_package_version;
 const isDev = env === 'development';
+const date = new Date();
 
-const context = {isDev, version, env};
+const context = {isDev, version, env, date};
 
 module.exports = (config) => {
   config.addGlobalData('env', context);
