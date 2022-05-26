78adb8c4761bb3103825a1639908330a2865aee8
pvalchetskaya <pvalchetskaya@exadel.com>
2022-05-26 12:09:08 +0300

    feat: update header

--- pages/src/playground.ts
+++ pages/src/playground.ts
@@ -1,8 +1,12 @@
 import {ESLScrollbar} from '@exadel/esl/modules/esl-scrollbar/core';
 import {ESLSelect} from '@exadel/esl/modules/esl-forms/esl-select/core';
+import {ESLTrigger} from '@exadel/esl/modules/esl-trigger/core';
+import {ESLToggleable} from '@exadel/esl/modules/esl-toggleable/core';
 
 import {init} from '../../src/registration';
 
 ESLSelect.register();
 ESLScrollbar.register();
+ESLTrigger.register();
+ESLToggleable.register();
 init();
