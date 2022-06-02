4fdd7a88439ab08b6fe43f3daea4ccff93bbbafe
Aliaksandr Bazukevich <aliaksandr.bazukevich@hp.com>
2022-06-02 14:13:56 +0300

    chore: updated file paths to fix build

--- src/plugins/registration.ts
+++ src/plugins/registration.ts
@@ -1,11 +1,15 @@
 import {UIPEditor} from './editor/editor';
+import {UIPHeader} from './header/header';
 import {UIPOptions} from './header/options/options';
+import {UIPSnippets} from './header/snippets/snippets';
 import {UIPSettings} from './settings/settings';
 
-export {UIPEditor, UIPOptions, UIPSettings};
+export {UIPEditor, UIPOptions, UIPSettings, UIPSnippets, UIPHeader};
 
 export const registerPlugins = () => {
   UIPEditor.register();
+  UIPHeader.register();
   UIPOptions.register();
+  UIPSnippets.register();
   UIPSettings.register();
 };
