1f3564761c3034b4fb7c0ddcb856c071dc3a915e
Feoktist Shovchko <fshovchko@exadel.com>
2022-06-21 16:25:58 +0300

    feat(): ability to block container scroll with JS

--- src/modules/esl-scrollbar/core/esl-scrollbar.ts
+++ src/modules/esl-scrollbar/core/esl-scrollbar.ts
@@ -7,6 +7,7 @@ import {isMouseEvent, isTouchEvent, getTouchPoint, getOffsetPoint} from '../../e
 import {isRelativeNode} from '../../esl-utils/dom/traversing';
 import {TraversingQuery} from '../../esl-traversing-query/core';
 import {RTLUtils} from '../../esl-utils/dom/rtl';
+import {ScrollUtils} from '../../all';
 
 /**
  * ESLScrollbar is a reusable web component that replaces the browser's default scrollbar with
@@ -191,6 +192,7 @@ export class ESLScrollbar extends ESLBaseElement {
   }
 
   public set position(position: number) {
+    if (this._$target && ScrollUtils.isScrollLocked(this._$target)) return;
     this.scrollTargetTo(this.scrollableSize * this.normalizePosition(position));
     this.update();
   }
--- src/modules/esl-utils/dom/scroll/utils.ts
+++ src/modules/esl-utils/dom/scroll/utils.ts
@@ -2,6 +2,7 @@ export type ScrollStrategy = 'none' | 'native' | 'pseudo';
 
 const $html = document.documentElement;
 const initiatorSet = new Set();
+const targetMap = new Map();
 
 // TODO: functional
 
@@ -50,5 +51,45 @@ export abstract class ScrollUtils {
   public static requestUnlock(initiator: any, strategy?: ScrollStrategy): void {
     initiator && initiatorSet.delete(initiator);
     (initiatorSet.size === 0) && ScrollUtils.unlock();
+  }
+
+  private static _onScrollElement(target: Element): any {
+    const scrollTop = target.scrollTop;
+    const scrollLeft = target.scrollLeft;
+    return (e: KeyboardEvent) => {
+      if (e.target === target) {
+        target.scrollTop = scrollTop;
+        target.scrollLeft = scrollLeft;
+      }
+    };
+  }
+
+  private static _onScrollWindow(): any {
+    const scrollTop = window.scrollY || window.pageYOffset;
+    const scrollLeft = window.scrollX || window.pageXOffset;
+    return () => window.scrollTo(scrollLeft, scrollTop);
+  }
+
+  public static lockScrollTriggers(target: Element): boolean {
+    if (this.isScrollLocked(target)) return true;
+    target.setAttribute('esl-js-scroll-lock', '');
+    const isTargetWindow = target.scrollHeight === target.clientHeight;
+    const eventTarget = isTargetWindow ? window : target;
+    const handler = isTargetWindow ? this._onScrollWindow() : this._onScrollElement(target);
+    targetMap.set(target, handler);
+    eventTarget.addEventListener('scroll', handler);
+    return this.isScrollLocked(target);
+  }
+
+  public static unlockScrollTriggers(target: Element): boolean {
+    if (!this.isScrollLocked(target)) return true;
+    target.removeAttribute('esl-js-scroll-lock');
+    target.removeEventListener('scroll', targetMap.get(target));
+    targetMap.delete(target);
+    return this.isScrollLocked(target);
+  }
+
+  public static isScrollLocked(target: Element): boolean {
+    return target.hasAttribute('esl-js-scroll-lock') && targetMap.has(target);
   }
 }
