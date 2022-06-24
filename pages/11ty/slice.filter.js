db3b658f98929729b18c3093131786c59137697e
nsmirnova <nsmirnova@exadel.com>
2022-06-24 14:40:42 +0300

    refactor(gh-pages): update blogs component on landing page

--- pages/11ty/slice.filter.js
+++ pages/11ty/slice.filter.js
@@ -1,6 +1,6 @@
 /** filter to limit collection */
-const sliceFilter = (collection, count) => collection ? collection.slice(0, count) : [];
+const limit = (collection, count) => collection ? collection.slice(0, count) : [];
 
 module.exports = (config) => {
-  config.addFilter('limit', sliceFilter);
+  config.addFilter('limit', limit);
 };
