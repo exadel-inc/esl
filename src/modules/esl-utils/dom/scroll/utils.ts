63a5dd098797858ac4d64d26f5157810014328de
nsmirnova <nsmirnova@exadel.com>
2022-06-24 16:02:35 +0300

    refactor(esl-utils): `ScrollUtils` rewritten in a functional way, `hasHorizontalScroll` added

--- src/modules/esl-utils/dom/scroll/utils.ts
+++ src/modules/esl-utils/dom/scroll/utils.ts
@@ -3,52 +3,67 @@ export type ScrollStrategy = 'none' | 'native' | 'pseudo';
 const $html = document.documentElement;
 const initiatorSet = new Set();
 
-// TODO: functional
 
+/** Check vertical scroll based on content height */
+export function hasVerticalScroll(target = $html): boolean {
+  return target.scrollHeight > target.clientHeight;
+}
+
+/** Check horizontal scroll based on content height */
+export function hasHorizontalScroll(target = $html): boolean {
+  return target.scrollWidth > target.clientWidth;
+}
+
+/**
+ * Disable scroll on the page.
+ * @param strategy - to make scroll visually disabled
+ * */
+function lockScroll(strategy?: ScrollStrategy): void {
+  const hasScroll = ScrollUtils.hasVerticalScroll();
+  if (strategy && strategy !== 'none' && hasScroll) {
+    $html.classList.add(`esl-${strategy}-scroll`);
+  }
+  $html.classList.add('esl-disable-scroll');
+}
+
+/**
+ * Enable scroll on the page.
+ * */
+function unlockScroll(): void {
+  $html.classList.remove('esl-disable-scroll', 'esl-pseudo-scroll', 'esl-native-scroll');
+}
+
+/**
+ * Disable scroll on the page.
+ * @param initiator - object to associate request with
+ * @param strategy - to make scroll visually disabled
+ *
+ * TODO: currently requests with different strategy is not taken into account
+ * */
+function requestLockScroll(initiator: any, strategy?: ScrollStrategy): void {
+  initiator && initiatorSet.add(initiator);
+  (initiatorSet.size > 0) && lockScroll(strategy);
+}
+
+/**
+ * Enable scroll on the page in case it was requested with given initiator.
+ * @param initiator - object to associate request with
+ * @param strategy - to make scroll visually disabled
+ */
+function requestUnlockScroll(initiator: any, strategy?: ScrollStrategy): void {
+  initiator && initiatorSet.delete(initiator);
+  (initiatorSet.size === 0) && unlockScroll();
+}
+
+/** @deprecated use functional utils instead */
 export abstract class ScrollUtils {
-  /** Check vertical scroll based on content height */
-  static hasVerticalScroll(target = $html): boolean {
-    return target.scrollHeight > target.clientHeight;
-  }
 
-  /**
-   * Disable scroll on the page.
-   * @param strategy - to make scroll visually disabled
-   * */
-  public static lock(strategy?: ScrollStrategy): void {
-    const hasScroll = ScrollUtils.hasVerticalScroll();
-    if (strategy && strategy !== 'none' && hasScroll) {
-      $html.classList.add(`esl-${strategy}-scroll`);
-    }
-    $html.classList.add('esl-disable-scroll');
-  }
+  public static readonly hasVerticalScroll = hasVerticalScroll;
+  public static readonly hasHorizontalScroll = hasHorizontalScroll;
 
-  /**
-   * Enable scroll on the page.
-   * */
-  public static unlock(): void {
-    $html.classList.remove('esl-disable-scroll', 'esl-pseudo-scroll', 'esl-native-scroll');
-  }
+  public static readonly lock = lockScroll;
+  public static readonly unlock = unlockScroll;
+  public static readonly requestLock = requestLockScroll;
+  public static readonly requestUnlock = requestUnlockScroll;
 
-  /**
-   * Disable scroll on the page.
-   * @param initiator - object to associate request with
-   * @param strategy - to make scroll visually disabled
-   *
-   * TODO: currently requests with different strategy is not taken into account
-   * */
-  public static requestLock(initiator: any, strategy?: ScrollStrategy): void {
-    initiator && initiatorSet.add(initiator);
-    (initiatorSet.size > 0) && ScrollUtils.lock(strategy);
-  }
-
-  /**
-   * Enable scroll on the page in case it was requested with given initiator.
-   * @param initiator - object to associate request with
-   * @param strategy - to make scroll visually disabled
-   */
-  public static requestUnlock(initiator: any, strategy?: ScrollStrategy): void {
-    initiator && initiatorSet.delete(initiator);
-    (initiatorSet.size === 0) && ScrollUtils.unlock();
-  }
 }
