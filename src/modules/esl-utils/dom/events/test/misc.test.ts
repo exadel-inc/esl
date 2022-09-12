1b0f82c61644e36140dcd2c79ca999a1890572bf
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2022-09-12 22:36:41 +0200

    test(esl-utils): `splitEvents` method covered with tests

--- src/modules/esl-utils/dom/events/test/misc.test.ts
+++ src/modules/esl-utils/dom/events/test/misc.test.ts
@@ -5,7 +5,7 @@ import {
   isPassiveByDefault,
   getTouchPoint,
   getOffsetPoint,
-  getCompositeTarget
+  getCompositeTarget, splitEvents
 } from '../misc';
 
 describe('dom/events: misc', () => {
@@ -117,6 +117,33 @@ describe('dom/events: misc', () => {
   });
 
   describe('splitEvents', () => {
-    // TODO
+    test(
+      'Empty string parsed to an empty array without exceptions',
+      () => expect(splitEvents('')).toEqual([])
+    );
+    test(
+      'Single event string parsed to single event',
+      () => expect(splitEvents('abc')).toEqual(['abc'])
+    );
+    test(
+      'Single event string with extra spaces parsed to single event',
+      () => expect(splitEvents('  abc ')).toEqual(['abc'])
+    );
+    test(
+      'Multiple events string parsed to separate events',
+      () => expect(splitEvents('abc de f')).toEqual(['abc', 'de', 'f'])
+    );
+    test(
+      'Multiple events string with extra spaces parsed to separate events',
+      () => expect(splitEvents(' a b')).toEqual(['a', 'b'])
+    );
+    test(
+      'The same events count ones when parsed (two unique terms)',
+      () => expect(splitEvents(' a a b')).toEqual(['a', 'b'])
+    );
+    test(
+      'The same events count ones when parsed (one unique term)',
+      () => expect(splitEvents('a a a a a')).toEqual(['a'])
+    );
   });
 });
