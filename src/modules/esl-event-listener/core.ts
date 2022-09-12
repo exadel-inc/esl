d443c54289b1d59e3c636d1da3bdebeaac983064
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2022-09-13 01:00:41 +0200

    refactor(esl-event-listener): update public API of esl-event-listener module

--- src/modules/esl-event-listener/core.ts
+++ src/modules/esl-event-listener/core.ts
@@ -1,3 +1,12 @@
 export * from './core/api';
-export * from './core/listener';
-export * from './core/descriptor';
+export type {
+  ESLListenerEventMap,
+  ESLListenerDescriptor,
+  ESLListenerTarget,
+  ESLListenerDefinition
+} from './core/descriptor';
+export type {
+  ESLEventListener,
+  ESLListenerHandler,
+  ESLListenerCriteria
+} from './core/listener';
