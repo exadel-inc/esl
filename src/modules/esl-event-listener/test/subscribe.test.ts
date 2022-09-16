f6244c1390d2af1806c9c7eb0b6bb13cd6768be7
Anna Barmina <abarmina@exadel.com>
2022-09-16 19:40:34 +0200

    test(esl-event-listener): add tests for once attribute

--- src/modules/esl-event-listener/test/subscribe.test.ts
+++ src/modules/esl-event-listener/test/subscribe.test.ts
@@ -301,7 +301,29 @@ describe('EventUtils:subscribe tests', () => {
     });
   });
 
-  // TODO: tests for `once` marker
+  describe('EventUtils.subscribe invokes with `once` attribute', () => {
+    const host = document.createElement('a');
+    const fn = jest.fn();
+
+    test('subscription to a single event is called at most once after being adding', () => {
+      EventUtils.subscribe(host, {event: 'click', once: true}, fn);
+      expect(EventUtils.listeners(host).length).toBe(1);
+      host.click();
+      expect(EventUtils.listeners(host).length).toBe(0);
+    });
+
+    test('subscriptions to the multiple events are called at most once after being adding', () => {
+      EventUtils.subscribe(host, {event: 'click keydown', once: true}, fn);
+      expect(EventUtils.listeners(host).length).toBe(2);
+
+      host.click();
+      expect(EventUtils.listeners(host, 'click').length).toBe(0);
+      expect(EventUtils.listeners(host, 'keydown').length).toBe(1);
+
+      host.dispatchEvent(new KeyboardEvent('keydown', {'key': 'a'}));
+      expect(EventUtils.listeners(host, 'keydown').length).toBe(0);
+    });
+  });
 
   // TODO: tests for find method
 
