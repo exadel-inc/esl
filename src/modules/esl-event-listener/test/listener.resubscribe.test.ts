68fae3d42190d907c9184af909bef1951c41c2a0
Anna Barmina <abarmina@exadel.com>
2022-09-21 16:22:17 +0200

    test(esl-listeners-test): typo fix

--- src/modules/esl-event-listener/test/listener.resubscribe.test.ts
+++ src/modules/esl-event-listener/test/listener.resubscribe.test.ts
@@ -8,14 +8,14 @@ describe('EventUtils.subscribe resubscribing event', () => {
   const target = jest.fn(() => targ1);
 
   const listeners1 = EventUtils.subscribe($host, {event: 'click', target}, handle);
-  test('EventUtils.subscribe subscription correctly first time', () => {
+  test('EventUtils.subscribe subscription correct first time', () => {
     expect(listeners1.length).toBe(1);
     expect(EventUtils.listeners($host).length).toBe(1);
   });
 
   target.mockImplementation(() => targ2);
   const listeners2 = EventUtils.subscribe($host, {event: 'click', target}, handle);
-  test('EventUtils.subscribe subscription correctly second time', () => {
+  test('EventUtils.subscribe subscription correct second time', () => {
     expect(listeners2.length).toBe(1);
     expect(EventUtils.listeners($host).length).toBe(1);
   });
@@ -30,5 +30,5 @@ describe('EventUtils.subscribe resubscribing event', () => {
     expect(handle).toBeCalled();
   });
 
-  test('EventUtils des not recreate instance', () => expect(listeners1[0]).toBe(listeners2[0]));
+  test('EventUtils does not recreate instance', () => expect(listeners1[0]).toBe(listeners2[0]));
 });
