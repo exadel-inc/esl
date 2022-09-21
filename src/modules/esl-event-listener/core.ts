e4bd9b277708ac69e43aa8f4db55c970b85c89d9
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2022-09-21 22:00:17 +0200

    chore: bad commit to check CLI

--- src/modules/esl-event-listener/core.ts
+++ src/modules/esl-event-listener/core.ts
@@ -4,7 +4,6 @@ export type {
   ESLListenerCriteria,
   ESLListenerEventMap,
   ESLListenerDescriptor,
-  ESLListenerDescriptorFn,
   ESLListenerTarget,
   ESLListenerDefinition
 } from './core/descriptor';
