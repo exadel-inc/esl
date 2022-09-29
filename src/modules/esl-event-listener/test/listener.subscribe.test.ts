fd4a1c3bed83b8dec3d4d557000ee2041134e602
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2022-09-29 16:11:40 +0200

    style(esl-event-listener): cosmetic fix for test names
    Co-authored-by: Anastasiya Lesun <72765981+NastaLeo@users.noreply.github.com>

--- src/modules/esl-event-listener/test/listener.subscribe.test.ts
+++ src/modules/esl-event-listener/test/listener.subscribe.test.ts
@@ -30,7 +30,7 @@ describe('ESLEventUtils:subscribe tests', () => {
     ESLEventUtils.unsubscribe($host);
   });
 
-  test('ESLEventListener subscribe successfully by global selector',  () => {
+  test('ESLEventListener subscribes successfully by global selector',  () => {
     const host = {};
     const handle = jest.fn();
     ESLEventUtils.subscribe(host, {event: 'click', target: 'body'}, handle);
@@ -38,7 +38,7 @@ describe('ESLEventUtils:subscribe tests', () => {
     ESLEventUtils.unsubscribe(host);
   });
 
-  test('ESLEventListener subscribe successfully by target instance',  () => {
+  test('ESLEventListener subscribes successfully by target instance',  () => {
     const host = {};
     const el = document.createElement('div');
     const handle = jest.fn();
