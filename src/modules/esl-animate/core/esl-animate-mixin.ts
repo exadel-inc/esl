96714781274f4a425c54bb3101e03031d2e3604a
Feoktist Shovchko <fshovchko@exadel.com>
2022-10-08 13:56:34 +0300

    chore(esl-animate): remove observed attributes

--- src/modules/esl-animate/core/esl-animate-mixin.ts
+++ src/modules/esl-animate/core/esl-animate-mixin.ts
@@ -1,72 +1,50 @@
-import {attr, boolAttr, ESLMixinElement} from '../../esl-mixin-element/core';
+import {jsonAttr, ESLMixinElement} from '../../esl-mixin-element/core';
 import {ExportNs} from '../../esl-utils/environment/export-ns';
 import {ready} from '../../esl-utils/decorators/ready';
 import {memoize} from '../../esl-utils/decorators/memoize';
-import {ESLTraversingQuery} from '../../esl-traversing-query/core';
 import {parseNumber} from '../../esl-utils/misc/format';
 
 import {ESLAnimateService} from './esl-animate-service';
 
-/**
- * ESLAnimateMixin - custom mixin element for quick {@link ESLAnimateService} attaching
- *
- * Have two types of usage:
- * - trough the target-s definition, then esl-animate (without it's own content) became invisible plugin-component
- * `<div esl-animate target="::next"></div><div>Content</div>`
- * - trough the content wrapping
- * `<div esl-animate>Content</div>`
- */
-@ExportNs('AnimateMixin')
-export class ESLAnimateMixin extends ESLMixinElement {
-  public static is = 'esl-animate';
-  public static observedAttributes = ['group', 'repeat', 'target'];
-
+interface AnimateOptions {
   /**
    * Class(es) to add on viewport intersection
    * @see ESLAnimateConfig.cls
    */
-  @attr({defaultValue: 'in'}) public cls: string;
-
-  /**
-   * Enable group animation for targets
-   * @see ESLAnimateConfig.group
-   */
-  @boolAttr() public group: boolean;
-
-  /**
-   * Delay to start animation from previous item in group
-   * @see ESLAnimateConfig.groupDelay
-   */
-  @attr({defaultValue: '100'}) public groupDelay: string;
-
+  cls: string;
   /**
    * Re-animate item after its getting hidden
    * @see ESLAnimateConfig.repeat
    */
-  @boolAttr() public repeat: boolean;
-
+  repeat: boolean;
   /**
    * Intersection ratio to consider element as visible.
    * Only 0.2 (20%), 0.4 (40%), 0.6 (60%), 0.8 (80%) values are allowed due to share of IntersectionObserver instance
    * with a fixed set of thresholds defined.
    */
-  @attr() public ratio: string;
+  ratio: string;
+}
 
-  /**
-   * Define target(s) to observe and animate
-   * Uses {@link TraversingQuery} with multiple targets support
-   * Default: ` ` - current element, `<esl-animate>` behave as a wrapper
-   */
-  @attr() public target: string;
+/**
+ * ESLAnimateMixin - custom mixin element for quick {@link ESLAnimateService} attaching
+ *
+ * Use example:
+ * `<div esl-animate>Content</div>`
+ * Supports aditional parameters^
+ * `<div esl-animate={repeat: true, ratio: 0.8, cls: 'in'}>Content</div>`
+ */
+@ExportNs('AnimateMixin')
+export class ESLAnimateMixin extends ESLMixinElement {
+  public static is = 'esl-animate';
 
-  /** Elements-targets found by target query */
-  @memoize()
-  public get $targets(): HTMLElement[] {
-    return ESLTraversingQuery.all(this.target, this.$host) as HTMLElement[];
-  }
+  @jsonAttr({name: ESLAnimateMixin.is})
+  public options: AnimateOptions;
 
-  public attributeChangedCallback(): void {
-    this.reanimate();
+  public static defaultConfig: Partial<AnimateOptions> = {cls: 'in'};
+
+  protected mergeDefaultParams(): AnimateOptions {
+    const type = this.constructor as typeof ESLAnimateMixin;
+    return Object.assign({}, type.defaultConfig, this.options);
   }
 
   @ready
@@ -78,20 +56,14 @@ export class ESLAnimateMixin extends ESLMixinElement {
   @ready
   public disconnectedCallback(): void {
     super.disconnectedCallback();
-    ESLAnimateService.unobserve(this.$targets);
+    ESLAnimateService.unobserve(this.$host);
   }
 
   /** Reinitialize {@link ESLAnimateService} for targets */
   public reanimate(): void {
-    ESLAnimateService.unobserve(this.$targets);
-    memoize.clear(this.$host, '$targets');
-    ESLAnimateService.observe(this.$targets, {
-      force: true,
-      cls: this.cls,
-      ratio: parseNumber(this.ratio, 0.4),
-      repeat: this.repeat,
-      group: this.group,
-      groupDelay: parseNumber(this.groupDelay, 0)
-    });
+    const {cls, ratio, repeat} = this.mergeDefaultParams();
+    ESLAnimateService.unobserve(this.$host);
+    memoize.clear(this, '$targets');
+    ESLAnimateService.observe(this.$host, {force: true, cls, ratio: parseNumber(ratio, 0.4), repeat});
   }
 }
