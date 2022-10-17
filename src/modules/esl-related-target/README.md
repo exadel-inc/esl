d606129683a1a10cc9f4e656a75da7103b7b5b66
alesun <alesun@exadel.com>
2022-10-17 12:26:20 +0200

    style(esl-related-target): apply suggestion from code review

--- src/modules/esl-related-target/README.md
+++ src/modules/esl-related-target/README.md
@@ -6,7 +6,7 @@ Authors: *Anastasiya Lesun, Alexey Stsefanovich (ala'n)*.
 
 <a name="intro"></a>
 
-**ESLRelatedTarget** is a custom mixin that can be used with `ESLToggleable` instance to link with another ESLToggleable element and manage its state.
+**ESLRelatedTarget** is a custom mixin that can be used with `ESLToggleable` instance to link with another ESLToggleable element and sync their states.
 
-Mixin attaches to the element via `esl-related-target` attribute that defines selector to find corresponding element.
-Activation (set open state) or deactivation of related target handles through `esl:show` and `esl:hide` custom events listening.
+Mixin attaches to the element via the `esl-related-target` attribute. The attribute's value defines a selector to find the corresponding element.
+Activation (set open state) or deactivation of the related target happens when the corresponding `esl:show` and `esl:hide` DOM events are dispatched on the current mixin element.
--- src/modules/esl-related-target/core/esl-related-target.ts
+++ src/modules/esl-related-target/core/esl-related-target.ts
@@ -1,3 +1,4 @@
+import {ExportNs} from '../../esl-utils/environment/export-ns';
 import {ESLMixinElement} from '../../esl-mixin-element/core';
 import {ESLToggleable} from '../../esl-toggleable/core/esl-toggleable';
 import {ESLTraversingQuery} from '../../esl-traversing-query/core';
@@ -6,12 +7,21 @@ import {copyDefinedKeys} from '../../esl-utils/misc/object/copy';
 
 import type {ESLToggleableActionParams} from '../../esl-toggleable/core/esl-toggleable';
 
+/**
+ * ESLRelatedTarget mixin element
+ * @author Anastasia Lesun, Alexey Stsefanovich (ala'n)
+ *
+ * ESLRelatedTarget is a custom mixin element that can be used with {@link ESLToggleable}s to sync their states
+ */
+@ExportNs('RelatedTarget')
 export class ESLRelatedTarget extends ESLMixinElement {
   static is = 'esl-related-target';
 
+  /** Selector to find target element */
   @attr({name: ESLRelatedTarget.is}) public selector: string;
 
-  protected get $relatedTarget(): ESLToggleable | null {
+  /** @returns related toggleable instance */
+  public get $relatedTarget(): ESLToggleable | null {
     const {selector} = this;
     if (!selector) return null;
     const target = ESLTraversingQuery.first(selector);
@@ -19,6 +29,7 @@ export class ESLRelatedTarget extends ESLMixinElement {
     return null;
   }
 
+  /** Merges params that are used by toggleable for actions */
   protected mergeLocalParams(params: ESLToggleableActionParams): ESLToggleableActionParams {
     return Object.assign(copyDefinedKeys(params), {
       initiator: 'relatedToggleable',
@@ -26,12 +37,14 @@ export class ESLRelatedTarget extends ESLMixinElement {
     });
   }
 
+  /** Processes {@link ESLToggleable} show event */
   @listen('esl:show')
   protected onShow(e: CustomEvent): void {
     const {params} = e.detail;
     this.$relatedTarget?.show({params: this.mergeLocalParams(params)});
   }
 
+  /** Processes {@link ESLToggleable} hide event */
   @listen('esl:hide')
   protected onHide(e: CustomEvent): void {
     const {params} = e.detail;
