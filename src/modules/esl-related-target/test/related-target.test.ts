d30dafbb48a6eeaaa8f912a43aee2f38c8fb6251
alesun <alesun@exadel.com>
2022-11-02 12:54:14 +0100

    style(esl-related-target): split tests

--- src/modules/esl-related-target/test/related-target.test.ts
+++ src/modules/esl-related-target/test/related-target.test.ts
@@ -16,10 +16,17 @@ describe('ESLRelatedTarget: show/hide depending on mixin element state', () => {
   afterAll(() => ($el.parentElement === document.body) && document.body.removeChild($el));
   afterAll(() => ($relatedEl.parentElement === document.body) && document.body.removeChild($relatedEl));
 
-  describe('Synchronization of mixin and target states: check SHOW_REQUEST event', () => {
-    test('SHOW_REQUEST on the current mixin element leads to show target element', () => {
-      $el.hide();
+  describe('Synchronization of mixin and target states: SHOW_REQUEST on the current mixin element leads to show target element', () => {
+    beforeEach(() =>  $el.hide());
+    test('Direct call of show function', () => {
+      expect($el.open).toBe(false);
+      expect($relatedEl.open).toBe(false);
+      $el.show();
       jest.advanceTimersByTime(1);
+      expect($el.open).toBe(true);
+      expect($relatedEl.open).toBe(true);
+    });
+    test('SHOW_REQUEST_EVENT dispatching', () => {
       expect($el.open).toBe(false);
       expect($relatedEl.open).toBe(false);
       $el.dispatchEvent(new CustomEvent($el.SHOW_REQUEST_EVENT));
@@ -29,10 +36,18 @@ describe('ESLRelatedTarget: show/hide depending on mixin element state', () => {
     });
   });
 
-  describe('Synchronization of mixin and target states: check HIDE_REQUEST event', () => {
-    test('HIDE_REQUEST on the current mixin element leads to show target element', () => {
-      $el.show();
+  describe('Synchronization of mixin and target states: HIDE_REQUEST on the current mixin element leads to show target element', () => {
+    beforeEach(() =>  $el.show());
+    test('Direct call of hide function', () => {
+      expect($el.open).toBe(true);
+      expect($relatedEl.open).toBe(true);
+      $el.hide();
       jest.advanceTimersByTime(1);
+      expect($el.open).toBe(false);
+      expect($relatedEl.open).toBe(false);
+
+    });
+    test('HIDE_REQUEST_EVENT dispatching', () => {
       expect($el.open).toBe(true);
       expect($relatedEl.open).toBe(true);
       $el.dispatchEvent(new CustomEvent($el.HIDE_REQUEST_EVENT));
