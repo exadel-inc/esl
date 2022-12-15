70ec8a68bc3eeaeac4096e8d2f5362a93df8ddc8
Dmitry Shovchko <d.shovchko@gmail.com>
2022-12-15 12:17:37 +0200

    chore(esl-share): mark esl-share actions as side effects

--- pages/src/localdev.ts
+++ pages/src/localdev.ts
@@ -43,10 +43,10 @@ import '../../src/modules/esl-media/providers/html5/video-provider';
 import '../../src/modules/esl-media/providers/youtube-provider';
 import '../../src/modules/esl-media/providers/brightcove-provider';
 
-import {ESLShareCopyAction} from '../../src/modules/esl-share/actions/copy-action';
-import {ESLShareMailAction} from '../../src/modules/esl-share/actions/mail-action';
-import {ESLShareMediaAction} from '../../src/modules/esl-share/actions/media-action';
-import {ESLSharePrintAction} from '../../src/modules/esl-share/actions/print-action';
+import '../../src/modules/esl-share/actions/copy-action';
+import '../../src/modules/esl-share/actions/mail-action';
+import '../../src/modules/esl-share/actions/media-action';
+import '../../src/modules/esl-share/actions/print-action';
 import {ESLShareFetchConfigProvider} from '../../src/modules/esl-share/config-providers/fetch-provider';
 
 import {
@@ -114,10 +114,6 @@ ESLCarouselPlugins.Link.register();
 ESLCarouselPlugins.Touch.register();
 ESLCarouselPlugins.Autoplay.register();
 
-ESLShareCopyAction.register();
-ESLShareMailAction.register();
-ESLShareMediaAction.register();
-ESLSharePrintAction.register();
 ESLShareConfig.use(ESLShareFetchConfigProvider, {
   url: '/assets/share/config.json'
 });
