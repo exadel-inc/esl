f775cbff711a3702daeabf9dde634eba9566374a
Feoktist Shovchko <fshovchko@exadel.com>
2022-12-18 11:50:36 +0200

    chore(docs): code refactoring

--- pages/11ty/eslDoc/helpers/functions.js
+++ pages/11ty/eslDoc/helpers/functions.js
@@ -25,42 +25,29 @@ function addFunctionOverloads(declaration, output) {
 }
 
 function getFunctionDeclaration(declaration) {
-  const declarationObj = {
-    type: 'Function',
-    name: getDeclarationName(declaration),
-    comment: getJSDocFullText(declaration),
-    typeParameters: getGenericTypes(declaration),
-    modifiers: getModifiers(declaration),
-    parameters: getArgumentsList(declaration),
-    returnType: getTypeSignature(declaration),
-  }
-  return Object.assign(declarationObj, {signature: getFunctionSignature(declarationObj)});
-}
-
-function getFunctionSignature (declaration) {
-  const {name, isAsync, typeParameters, returnType} = declaration;
-  const parameters = getArgumentsSignature(declaration.parameters);
-  return `${isAsync ? 'async' : ''}${name}${typeParameters ? `<${typeParameters.signature}>` : ''}(${parameters || ''})${returnType ? `: ${returnType}` : ''}`;
+  const type = 'Function';
+  const name = getDeclarationName(declaration);
+  const comment = getJSDocFullText(declaration);
+  const typeParameters = getGenericTypes(declaration);
+  const modifiers = getModifiers(declaration);
+  const parameters = getArgumentsList(declaration);
+  const returnType = getTypeSignature(declaration);
+  const argsSignature = getArgumentsSignature(parameters);
+  const signature = `${modifiers.isAsync ? 'async' : ''}${name}${typeParameters ? `<${typeParameters.signature}>` : ''}(${argsSignature})${returnType ? `: ${returnType}` : ''}`;
+  return {type, name, comment, typeParameters, modifiers, parameters, returnType, signature};
 }
 
 function getFunctionStatement(declaration) {
   const statement = declaration.declarationList.declarations[0];
-  const declarationObj = {
-    type: 'Function statement',
-    name: getDeclarationName(statement),
-    comment: getJSDocFullText(declaration),
-    typeParameters: getGenericTypes(statement.initializer),
-    parameters: getArgumentsList(statement.initializer),
-    returnType: getTypeSignature(statement.initializer)
-  };
-  const signature = getFunctionStatementSignature(declarationObj);
-  return Object.assign(declarationObj, {signature});
-}
-
-function getFunctionStatementSignature (declaration) {
-  const {name, typeParameters, returnType} = declaration;
-  const args = getArgumentsSignature(declaration.parameters);
-  return `const ${name} = ${typeParameters ? `<${typeParameters.signature}>` : ''}(${args || ''}) => ${returnType || 'void'}`;
+  const type = 'Function statement';
+  const name = getDeclarationName(statement);
+  const comment = getJSDocFullText(declaration);
+  const typeParameters = getGenericTypes(statement.initializer);
+  const parameters = getArgumentsList(statement.initializer);
+  const returnType = getTypeSignature(statement.initializer);
+  const argsSignature = getArgumentsSignature(parameters);
+  const signature = `const ${name} = ${typeParameters ? `<${typeParameters.signature}>` : ''}(${argsSignature}) => ${returnType || 'void'}`;
+  return {type, name, comment, typeParameters, parameters, returnType, signature};
 }
 
 module.exports = {
--- pages/11ty/eslDoc/helpers/type.js
+++ pages/11ty/eslDoc/helpers/type.js
@@ -10,55 +10,39 @@ const {
 } = require('./common');
 
 function getAlias(declaration) {
-  if (ts.isUnionTypeNode(declaration.type)) return getUnionType(declaration);
   if (ts.isTypeLiteralNode(declaration.type)) return getTypeLiteral(declaration);
   if (ts.isFunctionTypeNode(declaration.type)) return getFunctionType(declaration);
+  return getDefaultType(declaration);
 }
 
-function getUnionType(declaration) {
-  const declarationObj = {
-    type: 'Type alias',
-    name: getDeclarationName(declaration),
-    signature: `${this.name} = ${declaration.type.getText()}`,
-    comment: getJSDocFullText(declaration),
-  };
-  return Object.assign(declarationObj, {signature: `${declarationObj.name} = ${declaration.type.getText()}`});
+function getDefaultType(declaration) {
+  const type = 'Type alias';
+  const name = getDeclarationName(declaration);
+  const comment = getJSDocFullText(declaration);
+  const signature = `${name} = ${declaration.type.getText()}`;
+  return {type, name, comment, signature};  
 }
 
 function getTypeLiteral(declaration) {
-  const declarationObj = {
-    type: 'Type alias',
-    name: getDeclarationName(declaration),
-    comment: getJSDocFullText(declaration),
-    typeParameters: getGenericTypes(declaration),
-    parameters: getMemberList(declaration.type)
-  };
-  const signature = getAliasSignature(declarationObj);
-  return Object.assign(declarationObj, {signature});
-}
-
-function getAliasSignature(declaration) {
-  const {name, typeParameters} = declaration;
-  return `${name} ${typeParameters ? `<${typeParameters.signature}>` : ''}`;
+  const type = 'Type alias';
+  const name = getDeclarationName(declaration);
+  const comment = getJSDocFullText(declaration);
+  const typeParameters = getGenericTypes(declaration);
+  const parameters = getMemberList(declaration.type);
+  const signature = `${name} ${typeParameters ? `<${typeParameters.signature}>` : ''}`;
+  return {type, name, comment, typeParameters, parameters, signature};
 }
 
 function getFunctionType(declaration) {
-  const declarationObj = {
-    type: 'Function type',
-    name: getDeclarationName(declaration),
-    comment: getJSDocFullText(declaration),
-    typeParameters: getGenericTypes(declaration),
-    parameters: getArgumentsList(declaration.type),
-    returnType: getTypeSignature(declaration.type)
-  };
-  const signature = getFunctionTypeSignature(declarationObj);
-  return Object.assign(declarationObj, {signature});
-}
-
-function getFunctionTypeSignature (declaration) {
-  const {name, typeParameters, returnType} = declaration;
-  const parameters = getArgumentsSignature(declaration.parameters);
-  return `type ${name}${typeParameters ? `<${typeParameters.signature}>` : ''}(${parameters || ''})${returnType ? `: ${returnType}` : ''}`;
+  const type = 'Function type';
+  const name = getDeclarationName(declaration);
+  const comment = getJSDocFullText(declaration);
+  const typeParameters = getGenericTypes(declaration);
+  const parameters = getArgumentsList(declaration.type);
+  const returnType = getTypeSignature(declaration.type);
+  const argsSignature = getArgumentsSignature(parameters);
+  const signature = `type ${name}${typeParameters ? `<${typeParameters.signature}>` : ''}(${argsSignature})${returnType ? `: ${returnType}` : ''}`;
+  return {type, name, comment, typeParameters, parameters, returnType, signature};
 }
 
 module.exports = {getAlias};
