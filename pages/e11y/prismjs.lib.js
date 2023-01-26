829a691b426b350c62284a951df884e451e9ab21
Yuliya Adamska <yadamska@exadel.com>
2023-01-26 14:46:02 +0300

    chore: add highlighted languages for readme.md files

--- pages/e11y/prismjs.lib.js
+++ pages/e11y/prismjs.lib.js
@@ -1,5 +1,12 @@
 const Prism = require('prismjs');
 
+// Register highlighted languages
+require('prismjs/components/prism-bash');
+require('prismjs/components/prism-css');
+require('prismjs/components/prism-less');
+require('prismjs/components/prism-javascript');
+require('prismjs/components/prism-typescript');
+
 const highlight = (str, lang) => {
   try {
     lang = lang || 'text';
