bc3b2deefc6a811165fe9af6e153dfa4a23f85ce
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-02-15 02:49:27 +0100

    refactor(esl-utils): `ResizeObserver` mock enhanced

--- src/modules/esl-utils/test/resizeObserver.mock.ts
+++ src/modules/esl-utils/test/resizeObserver.mock.ts
@@ -1,5 +1,17 @@
-window.ResizeObserver = window.ResizeObserver || jest.fn().mockImplementation(() => ({
+export interface ResizeObserverMock {
+  callback: ResizeObserverCallback;
+  disconnect: jest.MockedFn<any>;
+  observe: jest.MockedFn<any>;
+  unobserve: jest.MockedFn<any>;
+}
+
+export const ResizeObserverMockImpl = jest.fn().mockImplementation((callback: ResizeObserverCallback) => ({
+  callback,
   disconnect: jest.fn(),
   observe: jest.fn(),
-  unobserve: jest.fn(),
+  unobserve: jest.fn()
 }));
+
+export const getLastResizeObserverMock = (): ResizeObserverMock => ResizeObserverMockImpl.mock.results.at(-1)?.value;
+
+window.ResizeObserver = window.ResizeObserver || ResizeObserverMockImpl;
