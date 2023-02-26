8e4286cf6e74288e0eebeea3f6745e1694083e91
Feoktist Shovchko <fshovchko@exadel.com>
2023-02-26 11:20:03 +0200

    chore(esl-utils): code refactoring

--- src/modules/esl-utils/dom/attr.ts
+++ src/modules/esl-utils/dom/attr.ts
@@ -1,38 +1,35 @@
-import {isArrayLike} from '../misc/object';
-
-import type {ArrayLike} from '../misc/object';
-
 export type AttributeTarget = {$host: Element};
 export type AttributeDecorator = (target: Element | AttributeTarget, propName: string) => void;
 
-/** @returns if attribute presented */
-export function hasAttr($el: Element | AttributeTarget | undefined | null, name: string): boolean {
+type ArgumentTarget = (Element | AttributeTarget)[] | Element | AttributeTarget | undefined | null;
+
+/** @returns if attribute presented
+ */
+export function hasAttr($el: ArgumentTarget, name: string): boolean {
   if (!$el) return false;
+  if (Array.isArray($el)) return $el.every((element: Element) => hasAttr(element, name));
   if ('$host' in $el) $el = $el.$host;
   return $el.hasAttribute(name);
 }
 
 /** @returns attribute or passed fallback value. Identical to getAttribute by default */
-export function getAttr($el: Element | AttributeTarget | undefined | null, name: string): string | null;
-export function getAttr<T>($el: Element | AttributeTarget | undefined | null, name: string, fallback: T): string | T;
-export function getAttr($el: Element | AttributeTarget | undefined | null, name: string, fallback: string | null = null): string | null {
+export function getAttr($el: ArgumentTarget, name: string): string | null;
+export function getAttr<T>($el: ArgumentTarget, name: string, fallback: T): string | T;
+export function getAttr($el: ArgumentTarget, name: string, fallback: string | null = null): string | null {
   if (!$el) return null;
+  if (Array.isArray($el)) return getAttr($el[0], name);
   if ('$host' in $el) $el = $el.$host;
   const value = $el.getAttribute(name);
   return value === null ? fallback : value;
 }
 
 /** Sets attribute */
-export function setAttr<T extends Element | AttributeTarget>(
-  $el: ArrayLike<T> | Element | AttributeTarget | undefined | null,
-  name: string,
-  value: undefined | null | boolean | string
-): void {
-  if (isArrayLike($el)) {
-    Array.from($el).forEach((element: T) => setAttr(element, name, value));
+export function setAttr($el: ArgumentTarget, name: string, value: undefined | null | boolean | string): void {
+  if (!$el) return;
+  if (Array.isArray($el)) {
+    $el.forEach((element: Element) => setAttr(element, name, value));
     return;
   }
-  if (!$el) return;
   if ('$host' in $el) $el = $el.$host;
   if (value === undefined || value === null || value === false) {
     $el.removeAttribute(name);
