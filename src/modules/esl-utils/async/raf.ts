3a938876791201e38919af33646d8fb28d248e45
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-05-18 19:11:38 +0200

    feat(esl-utils): add `promisifyNextRender` common utility

--- src/modules/esl-utils/async/raf.ts
+++ src/modules/esl-utils/async/raf.ts
@@ -26,3 +26,6 @@ export const rafDecorator = <T extends AnyToVoidFnSignature>(fn: T): T => {
     lastArgs = args;
   } as T;
 };
+
+/** @returns promise that will be resolved after next render */
+export const promisifyNextRender = (): Promise<void> => new Promise((resolve) => afterNextRender(resolve));
