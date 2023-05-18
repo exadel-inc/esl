19bd241ef58dd96c3787324ac0157ab138fe3f0e
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-05-18 19:21:09 +0200

    feat(esl-carousel): complete support of the navigation plugins for ESLCarousel

---
+++ src/modules/esl-carousel/plugin/nav/esl-carousel.nav.dots.ts
@@ -0,0 +1,111 @@
+// TODO: rework, simplify and refactor
+import {ExportNs} from '../../../esl-utils/environment/export-ns';
+import {attr, listen, memoize} from '../../../esl-utils/decorators';
+import {ARROW_LEFT, ARROW_RIGHT} from '../../../esl-utils/dom/keys';
+import {findNextLooped, findPrevLooped} from '../../../esl-utils/dom/traversing';
+import {ESLBaseElement} from '../../../esl-base-element/core';
+import {ESLTraversingQuery} from '../../../esl-traversing-query/core';
+
+import {indexToGroup} from '../../core/nav/esl-carousel.nav.utils';
+
+import type {ESLCarousel} from '../../core/esl-carousel';
+
+/**
+ * {@link ESLCarousel} Dots navigation element
+ * @author Julia Murashko
+ *
+ * Example:
+ * ```
+ * <esl-carousel-dots></esl-carousel-dots>
+ * ```
+ */
+@ExportNs('Carousel.Dots')
+export class ESLCarouselNavDots extends ESLBaseElement {
+  public static override is = 'esl-carousel-dots';
+
+  /** {@link ESLTraversingQuery} string to find {@link ESLCarousel} instance */
+  @attr({
+    defaultValue: '::parent([esl-carousel-container])::find(esl-carousel)'
+  })
+  public carousel: string;
+
+  /** @returns ESLCarousel instance; based on {@link carousel} attribute */
+  @memoize()
+  public get $carousel(): ESLCarousel | null {
+    return ESLTraversingQuery.first(this.carousel, this) as ESLCarousel;
+  }
+
+  public override async connectedCallback(): Promise<void> {
+    this.$$attr('disabled', true);
+    if (!this.$carousel) return;
+    await customElements.whenDefined(this.$carousel.tagName.toLowerCase());
+    super.connectedCallback();
+    this.rerender();
+  }
+
+  public override disconnectedCallback(): void {
+    super.disconnectedCallback();
+    memoize.clear(this, '$carousel');
+  }
+
+  /** Renders dots according to the carousel state. */
+  public rerender(): void {
+    if (!this.$carousel) return;
+    const {firstIndex, count, size} = this.$carousel;
+    this.$$attr('disabled', size < 2);
+    let html = '';
+    const activeDot = indexToGroup(firstIndex, count, size);
+    for (let i = 0; i < Math.ceil(size / count); ++i) {
+      html += this.buildDot(i, i === activeDot);
+    }
+    this.innerHTML = html;
+  }
+
+  /** Builds content of dots. */
+  public buildDot(index: number, isActive: boolean): string {
+    return `<button role="button"
+                data-nav-target="group:${index}"
+                class="carousel-dot ${isActive ? 'active-dot' : ''}"
+                aria-current="${isActive ? 'true' : 'false'}"></button>`;
+  }
+
+  @listen({
+    event: 'esl:slide:changed',
+    target: ($el: ESLCarouselNavDots) => $el.$carousel
+  })
+  protected _onSlideChange(e: Event): void {
+    if (this.$carousel !== e.target) return;
+    this.rerender();
+  }
+
+  @listen('click')
+  protected _onClick(event: PointerEvent): void {
+    if (!this.$carousel || typeof this.$carousel.goTo !== 'function') return;
+    const $target = (event.target as Element).closest('[data-nav-target]');
+    if (!$target) return;
+    this.$carousel.goTo($target.getAttribute('data-nav-target')!);
+  }
+
+  /** Handles `keydown` event. */
+  @listen('keydown')
+  protected _onKeydown(event: KeyboardEvent): void {
+    const $eventTarget: HTMLElement = event.target as HTMLElement;
+    if (ARROW_LEFT === event.key) {
+      const $nextDot = findPrevLooped($eventTarget, '.carousel-dot') as HTMLElement;
+      $nextDot.click();
+    }
+    if (ARROW_RIGHT === event.key) {
+      const $nextDot = findNextLooped($eventTarget, '.carousel-dot') as HTMLElement;
+      $nextDot.click();
+    }
+  }
+}
+
+declare global {
+  export interface ESLCarouselNS {
+    Dots: typeof ESLCarouselNavDots;
+  }
+  export interface HTMLElementTagNameMap {
+    'esl-carousel-dots': ESLCarouselNavDots;
+  }
+}
