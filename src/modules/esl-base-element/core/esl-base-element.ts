66266a9996d52245c3c094edfb8397066e1d6895
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-07-03 19:30:46 +0200

    fix(esl-base-element): more accurate check for element redeclaration (detects both: tag and class inconsistency)

--- src/modules/esl-base-element/core/esl-base-element.ts
+++ src/modules/esl-base-element/core/esl-base-element.ts
@@ -108,16 +108,16 @@ export abstract class ESLBaseElement extends HTMLElement implements ESLBaseCompo
    */
   public static register(this: typeof ESLBaseElement, tagName?: string): void {
     tagName = tagName || this.is;
-    if (!tagName) throw new Error('Can not define custom element');
+    if (!tagName) {
+      throw new DOMException('[ESL]: Incorrect tag name', 'NotSupportedError');
+    }
     const constructor: any = customElements.get(tagName);
-    if (constructor) {
-      if (constructor.is !== tagName) throw new Error('Element declaration tag inconsistency');
-      return;
+    if (constructor && (constructor !== this || constructor.is !== tagName)) {
+      throw new DOMException('[ESL]: Element tag already occupied or inconsistent', 'NotSupportedError');
     }
-    if (this.is !== tagName) {
-      this.is = tagName;
-    }
+    if (constructor) return;
     customElements.define(tagName, this as any as CustomElementConstructor);
+    Object.defineProperty(this, 'is', {value: tagName, writable: false});
   }
 
   /** Shortcut for `customElements.whenDefined(currentCustomElement)` */
