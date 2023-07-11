518eadd89684dae6110160d48a46e18dce95aba8
Feoktist Shovchko <fshovchko@exadel.com>
2023-07-11 12:33:50 +0300

    chore(esl-mixin): refactor tests to describe

--- src/modules/esl-mixin-element/test/mixin.test.ts
+++ src/modules/esl-mixin-element/test/mixin.test.ts
@@ -113,48 +113,76 @@ describe('ESLMixinElement', () => {
 
     beforeAll(() => document.body.appendChild($host));
 
-    test('ESLMixinElement $attr', () => {
-      const $el = CTestMixin.get($host) as CTestMixin;
-
+    describe('ESLMixinElement $attr', () => {
+      let $el: CTestMixin;
       const attrName = 'test-attr';
-      const val = $host.getAttribute(attrName);
-      expect($el.$$attr(attrName)).toBe(val);
-      expect($el.$$attr(attrName, 'test')).toBe(val);
-      expect($el.$$attr(attrName)).toBe('test');
+
+      beforeAll(() => $el = CTestMixin.get($host) as CTestMixin);
+
+      test('should return the attribute value', () => {
+        const val = $host.getAttribute(attrName);
+        expect($el.$$attr(attrName)).toBe(val);
+      });
+
+      test('should set the attribute value and return the new value', () => {
+        expect($el.$$attr(attrName, 'test')).toBe(null);
+        expect($el.$$attr(attrName)).toBe('test');
+      });
     });
 
-    test('ESLMixinElement $attr - boolean', () => {
-      const el = CTestMixin.get($host) as CTestMixin;
-
+    describe('ESLMixinElement $attr - boolean', () => {
+      let el: CTestMixin;
       const attrName = 'test-attr-bool';
-      const val = $host.getAttribute(attrName);
-      expect(el.$$attr(attrName)).toBe(val);
-      expect(el.$$attr(attrName, true)).toBe(val);
-      expect(el.$$attr(attrName)).toBe('');
-      expect(el.$$attr(attrName, false)).toBe('');
-      expect(el.$$attr(attrName)).toBe(null);
+
+      beforeAll(() => el = CTestMixin.get($host) as CTestMixin);
+
+      test('should return the initial attribute value', () => {
+        const val = $host.getAttribute(attrName);
+        expect(el.$$attr(attrName)).toBe(val);
+      });
+
+      test('should set and return an empty string for boolean true', () => {
+        expect(el.$$attr(attrName, true)).toBe(null);
+        expect(el.$$attr(attrName)).toBe('');
+      });
+
+      test('should set and return null for removed attribute', () => {
+        expect(el.$$attr(attrName, false)).toBe('');
+        expect(el.$$attr(attrName)).toBe(null);
+      });
     });
 
-    test('ESLMixinElement $cls - get', () => {
-      const $el = CTestMixin.get($host) as CTestMixin;
+    describe('ESLMixinElement $cls - get', () => {
+      let $el: CTestMixin;
 
-      $host.className = '';
-      expect($el.$$cls('a')).toBe(false);
-      $host.className = 'a b';
-      expect($el.$$cls('a')).toBe(true);
-      expect($el.$$cls('a b')).toBe(true);
+      beforeAll(() => $el = CTestMixin.get($host) as CTestMixin);
+
+      test('should return false when class does not exist', () => expect($el.$$cls('a')).toBe(false));
+
+      test('should return true when class exists', () => {
+        $host.className = 'a b';
+        expect($el.$$cls('a')).toBe(true);
+        expect($el.$$cls('a b')).toBe(true);
+      });
     });
 
-    test('ESLMixinElement $cls - set', () => {
-      const $el = CTestMixin.get($host) as CTestMixin;
+    describe('ESLMixinElement $cls - set', () => {
+      let $el: CTestMixin;
 
-      $host.className = '';
-      expect($el.$$cls('a', true)).toBe(true);
-      expect($host.className).toBe('a');
-      expect($el.$$cls('b', true)).toBe(true);
-      expect($host.className).toBe('a b');
-      expect($el.$$cls('a b', false)).toBe(false);
-      expect($host.className).toBe('');
+      beforeAll(() => $el = CTestMixin.get($host) as CTestMixin);
+
+      test('should add the class when setting to true', () => {
+        $host.className = '';
+        $el.$$cls('a', true);
+        $el.$$cls('b', true);
+        expect($host.className).toBe('a b');
+      });
+
+      test('should remove the class when setting to false', () => {
+        $host.className = 'a b';
+        expect($el.$$cls('a b', false)).toBe(false);
+        expect($host.className).toBe('');
+      });
     });
 
     test('ESLMixinElement $$fire', (done) => {
