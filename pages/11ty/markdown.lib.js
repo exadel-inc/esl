972d9ec1a42dc1346d2348a25851f6a361a45b38
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-07-13 03:44:07 +0200

    docs(site): add contribution required banner

--- pages/11ty/markdown.lib.js
+++ pages/11ty/markdown.lib.js
@@ -5,5 +5,6 @@ const markdown = MarkdownIt({html: true, highlight});
 
 module.exports = (config) => {
   config.setLibrary('md', markdown);
+  config.addPairedShortcode('markdown', (content) => markdown.render(content));
 };
 module.exports.markdown = markdown;
