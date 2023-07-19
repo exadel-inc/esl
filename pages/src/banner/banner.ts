c074d3c07f9502a6196979f9e48cf40f016fd086
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-07-19 02:33:48 +0200

    chore(site): contribute to documentation note final design

--- pages/src/banner/banner.ts
+++ pages/src/banner/banner.ts
@@ -1,5 +1,5 @@
 import {ESLToggleable} from '../../../src/modules/esl-toggleable/core/esl-toggleable';
-import {attr, prop} from '../../../src/modules/esl-utils/decorators';
+import {attr, memoize, prop} from '../../../src/modules/esl-utils/decorators';
 
 import type {ESLToggleableActionParams} from '../../../src/modules/esl-toggleable/core/esl-toggleable';
 
@@ -8,18 +8,25 @@ export class ESLDemoBanner extends ESLToggleable {
 
   @attr({parser: (str) => str && encodeURIComponent(str).trim()})
   public cookieName: string;
-  @attr({defaultValue: 7, parser: parseInt})
+  @attr({defaultValue: 14, parser: parseInt})
   public cookieTime: number;
 
-  @prop({initiator: 'initial', showDelay: 5000})
+  @prop({initiator: 'initial', showDelay: 8000})
   public override initialParams: ESLToggleableActionParams;
 
   @prop(true) public override closeOnEsc: boolean;
+
+  protected _$focusBefore: HTMLElement | null = null;
 
   /** Check if the coolie {@link cookieName} is active */
   public get hasCookie(): boolean {
     const {cookieName} = this;
     return !!cookieName && document.cookie.indexOf(`${cookieName}=true`) !== -1;
+  }
+
+  @memoize()
+  public get $focusable(): HTMLElement | undefined {
+    return this.querySelector('a, button') as HTMLElement | undefined;
   }
 
   /** Store cookie {@link cookieName} for {@link cookieTime} period */
@@ -37,10 +44,13 @@ export class ESLDemoBanner extends ESLToggleable {
 
   protected override onShow(params: ESLToggleableActionParams): void {
     super.onShow(params);
+    this._$focusBefore = document.activeElement as HTMLElement;
+    this.$focusable && this.$focusable.focus();
     if (params.initiator !== 'initial') this.registerCookie(-1);
   }
   protected override onHide(params: ESLToggleableActionParams): void {
     super.onHide(params);
+    this._$focusBefore && this._$focusBefore.focus();
     if (params.initiator !== 'initial') this.registerCookie();
   }
 }
