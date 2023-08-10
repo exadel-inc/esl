25b5b9139c36f9bfc78319ba39e7cf6cee464a46
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-08-10 03:39:47 +0200

    feat(esl-utils): `lockScroll` / `unlockScroll` methods reworked with no-scroll detection - `pseudo` scroll lock now uses padding hack instead of flexbox on the html level - new `background` type of lock repeats `pseudo` but with much more lower default `z-index` for scrollbar stub

--- src/modules/esl-utils/dom/scroll/utils.ts
+++ src/modules/esl-utils/dom/scroll/utils.ts
@@ -2,20 +2,18 @@ import {getScrollParent} from './parent';
 
 const $html = document.documentElement;
 
-const initiatorMap = new WeakMap<Element, any[]>();
-
 /** Checks if element is blocked from scrolling */
 export function isScrollLocked(target: Element): boolean {
   return target.hasAttribute('esl-scroll-lock');
 }
 
 /** Checks vertical scroll based on content height */
-export function hasVerticalScroll(target = $html): boolean {
+export function hasVerticalScroll(target: Element = $html): boolean {
   return target.scrollHeight > target.clientHeight;
 }
 
-/** Checks  horizontal scroll based on content height */
-export function hasHorizontalScroll(target = $html): boolean {
+/** Checks horizontal scroll based on content height */
+export function hasHorizontalScroll(target: Element = $html): boolean {
   return target.scrollWidth > target.clientWidth;
 }
 
@@ -24,57 +22,63 @@ export type ScrollLockOptions = {
    * Option to lock scroll:
    * - 'none' | null | undefined - totally locks scroll with `overflow: hidden` option
    * - 'native' (applicable for page only) - left page scroll visible but inactive
-   * - 'pseudo' (applicable for page only) - uses special flexbox hack on the page,
-   * to make page static on lock but with capability to overlap the scroll
+   * - 'pseudo' (applicable for page only) - uses padding offset to make page static on lock
+   * - 'background' (applicable for page only) - uses padding offset to make page static on lock and uses small pseudo-scrollbar z-index
    */
-  strategy?: 'none' | 'native' | 'pseudo' | null;
+  strategy?: 'none' | 'native' | 'pseudo' | 'background' | null;
   /** Locks all scrollable parents */
   recursive?: boolean;
   /** Initiator (requester) object to limit lock operations by the query */
   initiator?: any;
 };
 
+// Scroll lock mutex holder
+const initiatorMap = new WeakMap<Element, any[]>();
+/** Occupy inner mutex of scroll lock */
+const requestLock = (target: Element, initiator: any): void => {
+  if (!initiator) return;
+  const initiatorList = initiatorMap.get(target) || [];
+  const index = initiatorList.indexOf(initiator);
+  if (index === -1) initiatorList.push(initiator);
+  initiatorMap.set(target, initiatorList);
+};
+
+/** Free inner mutex of the scroll lock */
+const requestUnlock = (target: Element, initiator: any): boolean => {
+  const initiatorList = initiatorMap.get(target) || [];
+  const index = initiatorList.indexOf(initiator);
+  if (index >= 0) initiatorList.splice(index, 1);
+  if (!initiatorList.length || !initiator) initiatorMap.delete(target);
+  return !initiatorMap.has(target);
+};
+
 /**
- * Disables scroll on the element.
+ * Disables a scroll on the element.
  * @param target - scrollable element which will be blocked from scrolling
  * @param options - additional options to lock scroll
  * */
 export function lockScroll(target: Element = $html, options: ScrollLockOptions = {}): void {
-  const {initiator} = options;
-  if (initiator) {
-    const initiatorList = initiatorMap.get(target);
-    if (initiatorList) {
-      if (initiatorList.includes(initiator)) return;
-      initiatorList.push(initiator);
-      if (initiatorList.length > 1) return;
-    } else {
-      initiatorMap.set(target, [initiator]);
-    }
-  }
-
+  requestLock(target, options.initiator);
   const scrollable = target === $html ? target : getScrollParent(target);
+  const hasVScroll = hasVerticalScroll(scrollable);
+  if (scrollable === $html && hasVScroll) {
+    const scrollWidth = window.innerWidth - document.documentElement.clientWidth;
+    $html.style.setProperty('--s-lock-offset', `${scrollWidth}px`);
+  }
+  scrollable.toggleAttribute('esl-scroll-lock-passive', !hasVScroll);
   scrollable.setAttribute('esl-scroll-lock', options.strategy || '');
   if (options.recursive && scrollable.parentElement) lockScroll(scrollable.parentElement, options);
 }
 
 /**
- * Enables scroll on the target element in case it was requested with given initiator.
+ * Enables a scroll on the target element in case it was requested with given initiator.
  * @param target - scrollable element
  * @param options - additional options to unlock scroll
  */
 export function unlockScroll(target: Element = $html, options: ScrollLockOptions = {}): void {
-  const {initiator} = options;
-  if (initiator) {
-    const initiatorList = initiatorMap.get(target);
-    if (!initiatorList) return;
-    const index = initiatorList.indexOf(initiator);
-    if (index >= 0) initiatorList.splice(index, 1);
-    if (initiatorList.length > 0) return;
-  } else {
-    initiatorMap.delete(target);
-  }
-
+  if (!requestUnlock(target, options.initiator)) return;
   const scrollable = target === $html ? target : getScrollParent(target);
+  scrollable.removeAttribute('esl-scroll-lock-passive');
   scrollable.removeAttribute('esl-scroll-lock');
   if (options.recursive && scrollable.parentElement) unlockScroll(scrollable.parentElement, options);
 }
