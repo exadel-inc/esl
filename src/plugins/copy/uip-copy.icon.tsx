f96e9f063734ce67f25bf9e0c01ad894e1ebd213
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-10-17 10:17:32 +0200

    refactor(uip-copy): copy widget common attributive

---
+++ src/plugins/copy/uip-copy.icon.tsx
@@ -0,0 +1,8 @@
+import React from 'jsx-dom';
+
+export const CopyIcon = (): SVGElement => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#5f5f5f" stroke-width="1.25" viewBox="0 0 24 24">
+    <rect width="12" height="15" x="4" y="3" fill="#fff" rx="2"/>
+    <rect width="12" height="15" x="8" y="7" fill="#fff" rx="2"/>
+  </svg>
+) as SVGElement;
--- src/plugins/copy/uip-copy.shape.ts
+++ src/plugins/copy/uip-copy.shape.ts
@@ -1,10 +1,14 @@
 import type {ESLBaseElementShape} from '@exadel/esl/modules/esl-base-element/core';
 import type {UIPCopy} from './uip-copy';
 
+interface UIPCopyShape extends ESLBaseElementShape<UIPCopy> {
+  children?: any;
+}
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'uip-copy': ESLBaseElementShape<UIPCopy>;
+      'uip-copy': UIPCopyShape;
     }
   }
 }
