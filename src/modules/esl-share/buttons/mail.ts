c0d62ac774a9750e6d1f25f7d48c125a1d674066
Dmitry Shovchko <d.shovchko@gmail.com>
2023-10-20 14:38:08 +0300

    fix(esl-share): add a fill color to social network icons

--- src/modules/esl-share/buttons/mail.ts
+++ src/modules/esl-share/buttons/mail.ts
@@ -6,7 +6,7 @@ import type {ESLShareButtonConfig} from '../core/esl-share-config';
 
 export const mail: ESLShareButtonConfig = {
   action: 'external',
-  icon: '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" role="presentation" viewBox="0 0 28 28"><path d="M4.63 6.94v14.1h18.8V6.94H4.63zm2.44 1.78h13.9l-6.93 5.67-6.93-5.67zm14.6 1.72v8.82H6.47v-8.79l7.59 6.21 7.63-6.24z"/></svg>',
+  icon: '<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" role="presentation" viewBox="0 0 28 28"><path d="M4.63 6.94v14.1h18.8V6.94H4.63zm2.44 1.78h13.9l-6.93 5.67-6.93-5.67zm14.6 1.72v8.82H6.47v-8.79l7.59 6.21 7.63-6.24z" fill="#fff"/></svg>',
   iconBackground: '#ff1493',
   link: 'mailto:?subject={title}&body={url}',
   name: 'mail',
