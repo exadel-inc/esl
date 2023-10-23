58866c298c68c68186da6cca72e644f449fdd2d8
Dmitry Shovchko <d.shovchko@gmail.com>
2023-10-23 16:29:24 +0300

    style(esl-share): update shape of share trigger to support TSX

--- src/modules/esl-share/core/esl-share-popup-trigger.shape.ts
+++ src/modules/esl-share/core/esl-share-popup-trigger.shape.ts
@@ -6,6 +6,13 @@ import type {ESLSharePopupTrigger} from './esl-share-popup-trigger';
  * Used for TSX declaration
  */
 export interface ESLSharePopupTriggerTagShape extends ESLBaseElementShape<ESLSharePopupTrigger> {
+  /** Define the list of social networks or groups of them to display at popup (all by default) */
+  list?: string;
+  /** Define URL to share (current page URL by default) */
+  'share-url'?: string;
+  /** Define title to share (current document title by default) */
+  'share-title'?: string;
+
   /** Allowed children */
   children?: any;
 }
