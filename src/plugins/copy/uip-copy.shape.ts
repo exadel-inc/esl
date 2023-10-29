81b2f57e0c1ecacac730fa4748987c9f1c90495f
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-10-29 02:17:54 +0100

    refactor(uip-copy): rework and fix uip-copy widget based on button-plugin

--- src/plugins/copy/uip-copy.shape.ts
+++ src/plugins/copy/uip-copy.shape.ts
@@ -1,7 +1,7 @@
 import type {ESLBaseElementShape} from '@exadel/esl/modules/esl-base-element/core';
 import type {UIPCopy} from './uip-copy';
 
-interface UIPCopyShape extends ESLBaseElementShape<UIPCopy> {
+export interface UIPCopyShape extends ESLBaseElementShape<UIPCopy> {
   children?: any;
 }
 
