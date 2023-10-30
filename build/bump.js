006c1569205512b8cab050345b7dd870a3b65857
Feoktist Shovchko <fshovchko@exadel.com>
2023-10-30 14:14:01 +0200

    chore(build): simplify version bumper logic

--- build/bump.js
+++ build/bump.js
@@ -2,14 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 const color = require('kleur');
-const {exec} = require('child_process');
 
 const rootFolder = process.cwd();
 const config = yaml.load(fs.readFileSync(path.resolve(rootFolder, 'bump.config.yml')));
-const {version} = require(path.resolve(rootFolder, config.versionFile));
+const {version} = require(path.resolve(rootFolder, 'package.json'));
 
 config.files.forEach(processFile);
-doPostAction();
 
 function processFile(file) {
   try {
@@ -27,20 +25,4 @@ function processFile(file) {
   } catch (err) {
     console.error(color.red('Can`t bump version: '), err);
   }
-}
-
-function doPostAction() {
-  const {postAction} = config;
-  if (!config.postAction) return;
-  exec(postAction, (error, stdout, stderr) => {
-    if (error) {
-      console.error(color.red(`Error executing post action: ${error.message}`));
-      return;
-    }
-    if (stderr) {
-      console.error(color.red(`Error executing post action (stderr): ${stderr}`));
-      return;
-    }
-    console.log(color.green(`stdout ${postAction}: ${stdout}`));
-  });
 }
