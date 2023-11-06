87df3a5950490315a1ebd786ab8fbe864779604f
Feoktist Shovchko <fshovchko@exadel.com>
2023-11-06 21:01:24 +0200

    feat(esl-event-listener): inert wheel detector

---
+++ src/modules/esl-event-listener/core/targets/inert.target.event.ts
@@ -0,0 +1,43 @@
+import {overrideEvent} from '../../../esl-utils/dom/events/misc';
+
+/**
+ * Event names that could be triggered by {@link ESLWheelTarget}
+ */
+export type WheelEventName = 'longwheel';
+
+/**
+ * Describes scroll information provided with {@link ESLWheelTarget}
+ */
+export interface ESLWheelEventInfo {
+  /** Distance scrolled along the x-axis */
+  deltaX: number;
+  /** Distance scrolled along the y-axis */
+  deltaY: number;
+  /** First wheel event */
+  startEvent: WheelEvent;
+  /** Last wheel event */
+  endEvent: WheelEvent;
+}
+
+/**
+ * Wheel event dispatched by {@link ESLWheelTarget}
+ */
+export class ESLWheelEvent extends UIEvent implements ESLWheelEventInfo {
+  public override readonly target: Element;
+
+  public readonly deltaX: number;
+  public readonly deltaY: number;
+  public readonly endEvent: WheelEvent;
+  public readonly startEvent: WheelEvent;
+
+  protected constructor(eventName: WheelEventName, target: Element, wheelInfo: ESLWheelEventInfo) {
+    super(eventName, {bubbles: true, cancelable: true});
+    overrideEvent(this, 'target', target);
+    Object.assign(this, wheelInfo);
+  }
+
+  /** Creates {@link ESLWheelEvent} based on {@link ESLWheelEventInfo} */
+  public static fromConfig(eventName: WheelEventName, target: Element, wheelInfo: ESLWheelEventInfo): ESLWheelEvent {
+    return new ESLWheelEvent(eventName, target, wheelInfo);
+  }
+}
---
+++ src/modules/esl-event-listener/core/targets/inert.target.ts
@@ -0,0 +1,101 @@
+import {SyntheticEventTarget} from '../../../esl-utils/dom/events/target';
+import {resolveDomTarget} from '../../../esl-utils/abstract/dom-target';
+import {isElement} from '../../../esl-utils/dom/api';
+import {bind} from '../../../esl-utils/decorators/bind';
+import {resolveCSSSize} from '../../../esl-utils/dom/units';
+import {aggregate} from '../../../esl-utils/async/aggregate';
+import {ESLEventListener} from '../listener';
+
+import {ESLWheelEvent} from './inert.target.event';
+
+import type {ESLWheelEventInfo} from './inert.target.event';
+import type {CSSSize} from '../../../esl-utils/dom/units';
+import type {ESLDomElementTarget} from '../../../esl-utils/abstract/dom-target';
+
+export {ESLWheelEvent};
+
+/**
+ * Describes settings object that could be passed to {@link ESLWheelTarget.for} as optional parameter
+ */
+export interface ESLWheelSetting {
+  /** The minimum distance to accept an inertial wheel (supports `px`, `vw`, and `vh` units) */
+  threshold?: CSSSize;
+  /** The maximum duration of the wheel events to consider it inertial */
+  timeout?: number;
+}
+
+/**
+ * Implementation of EventTarget to observe wheel events for inertial scrolling
+ */
+export class ESLWheelTarget extends SyntheticEventTarget {
+  protected static defaultConfig: Required<ESLWheelSetting> = {
+    threshold: '400px',
+    timeout: 100
+  };
+
+  protected readonly config: Required<ESLWheelSetting>;
+  protected lastWheelEvent: WheelEvent;
+  protected accumulatedDelta: number = 0;
+  protected wheelTimer: number | null = null;
+
+  public static for(target: ESLDomElementTarget, settings?: ESLWheelSetting): ESLWheelTarget | null {
+    const $target = resolveDomTarget(target);
+    if (isElement($target)) return new ESLWheelTarget($target, settings);
+
+    console.warn('[Test]: TestInertWheelTarget cannot observe %o', target);
+    return null;
+  }
+
+  protected constructor(
+    protected readonly target: Element,
+    settings?: ESLWheelSetting
+  ) {
+    super();
+    this.config = Object.assign({}, ESLWheelTarget.defaultConfig, settings);
+  }
+
+  protected resolveEventDetails(events: WheelEvent[]): ESLWheelEventInfo {
+    const delta = events.reduce((agg, e) => ({
+      x: agg.x + e.deltaX,
+      y: agg.y + e.deltaY
+    }), {x: 0, y: 0});
+
+    const startEvent = events[0];
+    const endEvent = events[events.length - 1];
+    return {startEvent, endEvent, deltaX: delta.x, deltaY: delta.y};
+  }
+
+  @bind
+  protected handleWheel(event: WheelEvent): void {
+    aggregate(this._onWheelAggregated, this.config.timeout)(event);
+  }
+
+  @bind
+  protected _onWheelAggregated(events: WheelEvent[]): void {
+    const detail = this.resolveEventDetails(events);
+    if (!this.isLongScroll(detail)) return;
+    this.dispatchEvent(ESLWheelEvent.fromConfig('longwheel', this.target, detail));
+  }
+
+  private isLongScroll(dist: ESLWheelEventInfo): boolean {
+    const threshold = resolveCSSSize(this.config.threshold);
+    return !!threshold && (Math.abs(dist.deltaX) >= threshold || Math.abs(dist.deltaY) >= threshold);
+  }
+
+  public override addEventListener(callback: EventListener): void;
+  public override addEventListener(event: string, callback: EventListener): void;
+  public override addEventListener(event: any, callback: EventListener = event): void {
+    super.addEventListener(event, callback);
+
+    if (this.getEventListeners().length > 1) return;
+    ESLEventListener.subscribe(this, this.handleWheel, {event: 'wheel', capture: false, target: this.target});
+  }
+
+  public override removeEventListener(callback: EventListener): void;
+  public override removeEventListener(event: string, callback: EventListener): void;
+  public override removeEventListener(event: any, callback: EventListener = event): void {
+    super.removeEventListener(event, callback);
+
+    if (!this.hasEventListener(event)) ESLEventListener.unsubscribe(this);
+  }
+}
