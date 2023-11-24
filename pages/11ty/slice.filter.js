d40f2704848f745875587719bb4e979277237685
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-11-24 15:13:24 +0100

    style(site): fix spaces in `pages/11ty/slice.filter.js`
    Co-authored-by: Anastasiya Lesun <72765981+NastaLeo@users.noreply.github.com>

--- pages/11ty/slice.filter.js
+++ pages/11ty/slice.filter.js
@@ -1,5 +1,5 @@
 /** filter to limit collection */
-const limit = (collection, count, start= 0) => collection ? collection.slice(start, start + count) : [];
+const limit = (collection, count, start = 0) => collection ? collection.slice(start, start + count) : [];
 
 module.exports = (config) => {
   config.addFilter('limit', limit);
