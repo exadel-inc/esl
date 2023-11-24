b5ca542359e311d3da78b9c137fab6d05f47eef5
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-11-24 13:23:56 +0100

    chore(site): update `filter` / `exclude` filters, clarify JS Doc

--- pages/11ty/collections.filter.js
+++ pages/11ty/collections.filter.js
@@ -1,18 +1,28 @@
 /** get value from object by path */
 const get = (item, path) => path.split('.').reduce((acc, key) => acc && acc[key], item);
 
-/** filter to include only items that have a property with passed value */
-function filter(collection, prop, value) {
-  const matcher = arguments.length === 2 ?
-    (item) => !!get(item, prop) :
-    (item) => get(item, prop) === value;
+/**
+ * filter to include only items that have a property with passed values
+ * - if no values are passed, include items that have a truthy value for the property
+ * - if values are passed, include items that have a value that matches one of the passed values
+ */
+function filter(collection, prop, ...values) {
+  const matcher = values.length > 0 ?
+    (item) => values.includes(get(item, prop)) :
+    (item) => !!get(item, prop);
+
   return (collection || []).filter(matcher);
 }
-/** filter to exclude items that have a property with passed value */
-function exclude(collection, prop, value) {
-  const matcher = arguments.length === 2 ?
-    (item) => !get(item, prop) :
-    (item) => get(item, prop) !== value;
+/**
+ * filter to exclude items that have a property with passed values
+ * - if no values are passed, exclude items that have a truthy value for the property
+ * - if values are passed, exclude items that have a value that matches one of the passed values
+ */
+function exclude(collection, prop, ...values) {
+  const matcher = values.length > 0 ?
+    (item) => !values.includes(get(item, prop)) :
+    (item) => !get(item, prop);
+
   return (collection || []).filter(matcher);
 }
 
