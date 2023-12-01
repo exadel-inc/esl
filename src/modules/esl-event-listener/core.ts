f75c49928b6b5eac6ec2167b0b32c6bce98cad6e
Feoktist Shovchko <fshovchko@exadel.com>
2023-12-01 16:46:44 +0200

    refactor(esl-event-listener): add missing wheel import

--- src/modules/esl-event-listener/core.ts
+++ src/modules/esl-event-listener/core.ts
@@ -3,4 +3,5 @@ export * from './core/api';
 export * from './core/targets/resize.target';
 export * from './core/targets/decorated.target';
 export * from './core/targets/swipe.target';
+export * from './core/targets/wheel.target';
 export * from './core/targets/intersection.target';
