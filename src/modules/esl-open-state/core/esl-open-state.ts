4b7dbf16be52311b3428a36f580d5cf37325a3c8
Yuliya Adamska <yadamska@exadel.com>
2023-12-12 19:48:20 +0300

    test(esl-open-state): create tests for initial state and media query change

--- src/modules/esl-open-state/core/esl-open-state.ts
+++ src/modules/esl-open-state/core/esl-open-state.ts
@@ -2,7 +2,7 @@ import {ExportNs} from '../../esl-utils/environment/export-ns';
 import {ESLMixinElement} from '../../esl-mixin-element/core';
 import {listen, attr, ready} from '../../esl-utils/decorators';
 import {ESLMediaQuery} from '../../esl-media-query/core';
-import type {ESLToggleable} from '../../esl-toggleable/core/esl-toggleable';
+import {ESLToggleable} from '../../esl-toggleable/core/esl-toggleable';
 import type {ESLMediaChangeEvent} from '../../esl-media-query/core';
 
 /**
@@ -15,20 +15,22 @@ import type {ESLMediaChangeEvent} from '../../esl-media-query/core';
 export class ESLOpenState extends ESLMixinElement {
   static override is = 'esl-open-state';
 
+  public override $host: ESLToggleable;
+
   /** Open state {@link ESLMediaQuery} condition from query string */
   @attr({name: ESLOpenState.is, parser: ESLMediaQuery.for}) public media: ESLMediaQuery;
 
   @ready
   protected override connectedCallback(): void {
     super.connectedCallback();
-    customElements.whenDefined('esl-toggleable').then(() => this.onMediaChange());
+    ESLToggleable.registered.then(() => this.onMediaChange());
   }
 
   /** Handles query change and processes {@link ESLToggleable} toggle event in case query value is matched */
   @listen({event: 'change', target: ($this: ESLMediaChangeEvent) => $this.media})
   protected onMediaChange(): void {
-    (this.$host as ESLToggleable).toggle(this.media.matches, {
-      initiator: 'media',
+    this.$host.toggle(this.media.matches, {
+      initiator: (this.constructor as typeof ESLOpenState).is,
       activator: this.$host
     });
   }
--- src/modules/esl-open-state/test/esl-open-state.test.ts
+++ src/modules/esl-open-state/test/esl-open-state.test.ts
@@ -1,26 +1,77 @@
-import {ESLOpenState} from '../core/esl-open-state';
-import {ESLToggleable} from '../../esl-toggleable/core/esl-toggleable';
-import {ESLTestTemplate} from '../../esl-utils/test/template';
+import {ESLOpenState} from '../core';
+import {ESLToggleable} from '../../esl-toggleable/core';
+import {getMatchMediaMock} from '../../esl-utils/test/matchMedia.mock';
+
+// TODO: consider as default jest loader
+jest.mock('../../esl-utils/dom/ready', () => ({
+  onDocumentReady: (clb: any) => clb()
+}));
 
 describe('ESLOpenState (mixin): tests', () => {
+  const TEST_MQ = '(min-width: 500px)';
+  const twoTicks = () => Promise.resolve().then(() => Promise.resolve());
 
-  beforeAll(() => {
+  beforeAll(async () => {
     jest.useFakeTimers();
 
     ESLToggleable.register();
     ESLOpenState.register();
+
+    await ESLToggleable.registered;
   });
 
-  describe('ESLOpenState: general behaviour tests', () => {
-    const REFERENCES = {
-      origin: 'esl-toggleable#origin-tgbl'
-    };
-    const TEMPLATE = ESLTestTemplate.create(`
-      <esl-toggleable id="origin-tgbl"></esl-toggleable>
-    `, REFERENCES).bind('beforeeach');
+  beforeEach(() => document.body.innerHTML = '');
 
-    test('ESLOpenState with \'@+MD\' query condition', () => {
-      (TEMPLATE.$origin as ESLToggleable).setAttribute('esl-open-state', '@+MD');
-    });
+  test('ESLOpenState initialize correctly', async () => {
+    const $el = ESLToggleable.create();
+    $el.setAttribute(ESLOpenState.is, 'not all');
+    document.body.append($el);
+    await twoTicks();
+    expect(ESLOpenState.get($el)).toEqual(expect.any(ESLOpenState));
+  });
+
+  test('ESLOpenState set up initial closed state if query is falsy', async () => {
+    const $el = ESLToggleable.create();
+    const hideSpy = jest.spyOn($el, 'hide');
+    $el.setAttribute(ESLOpenState.is, 'not all');
+    document.body.append($el);
+    await twoTicks();
+    expect(hideSpy).toHaveBeenCalledWith(expect.objectContaining({initiator: ESLOpenState.is}));
+  });
+
+  test('ESLOpenState set up initial open state if query is active', async () => {
+    const $el = ESLToggleable.create();
+    const showSpy = jest.spyOn($el, 'show');
+    $el.setAttribute(ESLOpenState.is, 'all');
+    document.body.append($el);
+    await twoTicks();
+    expect(showSpy).toHaveBeenCalledWith(expect.objectContaining({initiator: ESLOpenState.is}));
+  });
+
+  test('ESLOpenState calls ESLTogleable\'s show on media query activation', async () => {
+    const $el = ESLToggleable.create();
+    const showSpy = jest.spyOn($el, 'show');
+    getMatchMediaMock(TEST_MQ).set(false);
+    $el.setAttribute(ESLOpenState.is, TEST_MQ);
+    document.body.append($el);
+    await twoTicks();
+    expect(showSpy).not.toHaveBeenCalledWith(expect.objectContaining({initiator: ESLOpenState.is}));
+
+    getMatchMediaMock(TEST_MQ).set(true);
+    expect(showSpy).toHaveBeenCalledWith(expect.objectContaining({initiator: ESLOpenState.is}));
+  });
+
+  test('ESLOpenState calls ESLTogleable\'s hide on media query deactivation', async () => {
+    const $el = ESLToggleable.create();
+    const hideSpy = jest.spyOn($el, 'hide');
+    getMatchMediaMock(TEST_MQ).set(true);
+    $el.setAttribute('open', '');
+    $el.setAttribute(ESLOpenState.is, TEST_MQ);
+    document.body.append($el);
+    await twoTicks();
+    expect(hideSpy).not.toHaveBeenCalledWith(expect.objectContaining({initiator: ESLOpenState.is}));
+
+    getMatchMediaMock(TEST_MQ).set(false);
+    expect(hideSpy).toHaveBeenCalledWith(expect.objectContaining({initiator: ESLOpenState.is}));
   });
 });
