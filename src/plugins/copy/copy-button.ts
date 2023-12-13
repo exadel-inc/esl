4e9a846bc301d70ef5f9cd5f730364eb2973d27c
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-12-13 17:26:25 +0100

    refactor(uip-copy): rework copy message styles and text

--- src/plugins/copy/copy-button.ts
+++ src/plugins/copy/copy-button.ts
@@ -10,18 +10,13 @@ export class UIPCopy extends UIPPluginButton {
   public static override defaultTitle = 'Copy to clipboard';
 
   public static msgConfig: AlertActionParams = {
-    text: 'Markup copied',
-    cls: 'uip-alert-info'
+    text: 'Playground content copied to clipboard',
+    cls: 'uip-copy-alert'
   };
 
   protected override connectedCallback(): void {
     if (!navigator.clipboard) this.hidden = true;
     super.connectedCallback();
-  }
-
-  protected override disconnectedCallback(): void {
-    super.disconnectedCallback();
-    this.innerHTML = '';
   }
 
   public override onAction(): void {
