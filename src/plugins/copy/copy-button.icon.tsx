c4eb87d7dc8a10ddafc8529ef328b2c624bbca1d
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2023-12-18 06:49:54 +0100

    fix: fix visual accessibility, rework copy button icon

--- src/plugins/copy/copy-button.icon.tsx
+++ src/plugins/copy/copy-button.icon.tsx
@@ -1,8 +1,7 @@
 import React from 'jsx-dom';
 
 export const CopyIcon = (): SVGElement => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#5f5f5f" stroke-width="1.25" viewBox="0 0 24 24">
-    <rect width="12" height="15" x="4" y="3" fill="#fff" rx="2"/>
-    <rect width="12" height="15" x="8" y="7" fill="#fff" rx="2"/>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M19.5 16.5v-12l-.75-.75H9l-.75.75v3h-3l-.75.75v12l.75.75H15l.75-.75v-3h3l.75-.75Zm-3.75-.75v-7.5L15 7.5H9.75V5.25H18v10.5h-2.25ZM6 9h8.25v10.5H6V9Z"/>
   </svg>
 ) as SVGElement;
