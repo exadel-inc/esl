60b8cc6df96695ab7bd36fd94544c617fb0c8b83
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-01-05 09:52:20 +0100

    docs: fix `polyfills.ts` doc syntax

--- site/src/polyfill.ts
+++ site/src/polyfill.ts
@@ -1,4 +1,4 @@
-/** Target: Safari <13.3 */
+/** Target: `Safari <13.3` */
 import {ResizeObserver} from '@juggle/resize-observer';
 window.ResizeObserver = window.ResizeObserver || ResizeObserver;
 
