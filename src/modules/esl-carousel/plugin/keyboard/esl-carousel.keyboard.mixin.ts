a421428c7822470223356b4e134e331da8068936
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-01-06 04:04:01 +0100

    refactor(esl-carousel): fix keyboard support mixin

--- src/modules/esl-carousel/plugin/keyboard/esl-carousel.keyboard.mixin.ts
+++ src/modules/esl-carousel/plugin/keyboard/esl-carousel.keyboard.mixin.ts
@@ -2,7 +2,7 @@ import {ExportNs} from '../../../esl-utils/environment/export-ns';
 
 import {ESLCarouselPlugin} from '../esl-carousel.plugin';
 import {attr, listen} from '../../../esl-utils/decorators';
-import {ARROW_LEFT, ARROW_RIGHT} from '../../../esl-utils/dom/keys';
+import {ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT, ARROW_UP} from '../../../esl-utils/dom/keys';
 
 /**
  * {@link ESLCarousel} Keyboard arrow support
@@ -14,16 +14,16 @@ export class ESLCarouselKeyboardMixin extends ESLCarouselPlugin {
   public static override is = 'esl-carousel-keyboard';
 
   /** Prefix to request next/prev navigation */
-  @attr({defaultValue: 'slide', name: ESLCarouselKeyboardMixin.is}) public type: 'slide' | 'group';
+  @attr({name: ESLCarouselKeyboardMixin.is}) public type: 'slide' | 'group';
 
   /** Handles `keydown` event */
   @listen('keydown')
   protected _onKeydown(event: KeyboardEvent): void {
-    if (ARROW_LEFT === event.key) {
-      this.$host.goTo(`${this.type}:prev`);
+    if ([ARROW_LEFT, ARROW_UP].includes(event.key)) {
+      this.$host.goTo(`${this.type || 'slide'}:prev`);
     }
-    if (ARROW_RIGHT === event.key) {
-      this.$host.goTo(`${this.type}:next`);
+    if ([ARROW_RIGHT, ARROW_DOWN].includes(event.key)) {
+      this.$host.goTo(`${this.type || 'slide'}:next`);
     }
   }
 }
