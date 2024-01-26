c3af68b2c4a9cd20c341516b13dcf1d13d7b1adc
nharshunova <nharshunova@exadel.com>
2024-01-26 11:26:33 +0100

    refactor(microtask): split test cases

--- src/modules/esl-utils/async/test/microtask.test.ts
+++ src/modules/esl-utils/async/test/microtask.test.ts
@@ -14,16 +14,23 @@ describe('sync/microtask', () => {
     await Promise.resolve();
     expect(fn).toBeCalledTimes(1);
   });
-  test('Decorated as microtask callback receives a correct list of call arguments', async () => {
+  test('Decorated as microtask callback receives a list of call arguments', async () => {
     const fn = jest.fn();
     const decorated = microtask(fn);
-    const params1 = [Symbol('Arg 1'), Symbol('Arg 2'), Symbol('Arg 3')];
+    const params = [Symbol('Arg 1'), Symbol('Arg 2'), Symbol('Arg 3')];
 
+    for (const param of params) decorated(param);
+    await Promise.resolve();
+    expect(fn).toBeCalledWith(expect.arrayContaining(params));
+  });
+  test('Decorated as microtask callback refreshes after decorated method call (leak protected)', async () => {
+    const fn = jest.fn();
+    const decorated = microtask(fn);
+    const params1 = [Symbol('Arg 1'), Symbol('Arg 2')];
     for (const param of params1) decorated(param);
     await Promise.resolve();
-    expect(fn).toBeCalledWith(params1);
 
-    const params2 = [Symbol('Arg 4'), Symbol('Arg 5')];
+    const params2 = [Symbol('Arg 3'), Symbol('Arg 4')];
     for (const param of params2) decorated(param);
     await Promise.resolve();
     expect(fn).lastCalledWith(params2);
