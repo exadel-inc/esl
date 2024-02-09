80b02aa6db5692d4c0a73c90e9193e899a653371
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-02-09 17:36:05 +0100

    chore(site): support for environment PORT override, add e2e run mode with font normalization

--- site/11ty/env.config.js
+++ site/11ty/env.config.js
@@ -1,12 +1,13 @@
 const env = process.argv.find(arg => arg.startsWith('--env='))?.split('=')[1];
-const isDev = env === 'development';
+const isE2E = env === 'e2e';
+const isDev = env === 'development' || isE2E;
 const date = new Date();
 
 const buildVersion = process.env['BUILD_VERSION'] || 'local';
 const packageVersion = process.env.npm_package_version;
 const version = `${packageVersion}-${buildVersion}`; // e.g. 1.0.0-123
 
-const context = {isDev, version, env, date, buildVersion, packageVersion};
+const context = {isDev, isE2E, version, env, date, buildVersion, packageVersion};
 
 module.exports = (config) => {
   config.addGlobalData('env', context);
