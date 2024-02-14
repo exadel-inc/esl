1cd60a345022d1c3cfe913fcd1bd1f51b9eef765
Yuliya Adamska <yadamska@exadel.com>
2024-02-14 18:31:38 +0300

    chore: updateFrom method refactoring for select setting

--- src/plugins/settings/select-setting/select-setting.ts
+++ src/plugins/settings/select-setting/select-setting.ts
@@ -99,8 +99,13 @@ export class UIPSelectSetting extends UIPSetting {
   updateFrom(model: UIPStateModel): void {
     super.updateFrom(model);
     this.resetFieldValues();
-    const attrValues = model.getAttribute(this.target, this.attribute);
-    this.mode === 'replace' ? this.replaceFrom(attrValues) : this.appendFrom(attrValues);
+    const values: (string | null)[] = model.getAttribute(this.target, this.attribute);
+    const specifiedValues: (string | null)[] = values.filter((value: string | null) => value !== null);
+    if (!specifiedValues.length) {
+      this.setInconsistency(this.NOT_VALUE_SPECIFIED_MSG);
+    } else if (specifiedValues.length && specifiedValues.length !== values.length) {
+      this.setInconsistency(this.MULTIPLE_VALUE_MSG);
+    } else this.mode === 'replace' ? this.replaceFrom(values) : this.appendFrom(values);
   }
 
   /** Updates setting's value for replace {@link mode} */
@@ -139,7 +144,7 @@ export class UIPSelectSetting extends UIPSetting {
 
   protected setValue(value: string): void {
     this.$$off(this._onChange);
-    if (value) value.split(' ').forEach((opt) => this.$field.setSelected(opt, true));
+    value.split(' ').forEach((opt) => this.$field.setSelected(opt, true));
     this.$$on(this._onChange);
   }
 
