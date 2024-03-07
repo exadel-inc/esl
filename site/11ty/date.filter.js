91896c731c96cc68fbd6facbb3d703b95e0e6730
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-03-07 20:53:00 +0100

    chore(site): add freshness marker for newsline component

--- site/11ty/date.filter.js
+++ site/11ty/date.filter.js
@@ -1,6 +1,13 @@
 const date = (timestamp) =>
   new Intl.DateTimeFormat('en-GB', { timeZone: 'UTC', month: 'long', day: '2-digit', year: 'numeric' }).format(new Date(timestamp));
 
+const isFresh = (timestamp, days = 30) => {
+  if (!timestamp) return false;
+  const timeout = days * 24 * 60 * 60 * 1000;
+  return Date.now() - timestamp < timeout;
+};
+
 module.exports = (config) => {
   config.addFilter('date', date);
+  config.addFilter('fresh', isFresh);
 };
