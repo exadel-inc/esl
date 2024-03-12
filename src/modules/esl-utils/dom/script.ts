5949d0c24a7de299c43f034c9749d49e32e55eab
Dmitry Shovchko <d.shovchko@gmail.com>
2024-03-12 18:51:10 +0200

    feat(esl-utils): add ability to use additional attributes in loadScript utility

--- src/modules/esl-utils/dom/script.ts
+++ src/modules/esl-utils/dom/script.ts
@@ -1,11 +1,9 @@
 /**
- * Creates new async script tag by id and src
+ * Creates new script element with specified attributes
  */
-const createAsyncScript = (id: string, src: string): HTMLScriptElement => {
+const createScript = (attrs: Record<string, string | boolean>): HTMLScriptElement => {
   const script = document.createElement('script');
-  script.id = id;
-  script.async = true;
-  script.src = src;
+  Object.entries(attrs).forEach(([name, value]) => value && script.setAttribute(name, value === true ? '' : value));
   return script;
 };
 
@@ -13,11 +11,12 @@ const createAsyncScript = (id: string, src: string): HTMLScriptElement => {
  * Common function that loads script async
  * @param id - unique script id that is used as a marker to prevent future load
  * @param src - script src (url) to load
+ * @param options - additional attributes to set on the script tag
  */
-export function loadScript(id: string, src: string): Promise<Event> {
+export function loadScript(id: string, src: string, options: Record<string, string | boolean> = {}): Promise<Event> {
   return new Promise((resolve, reject) => {
     const script: HTMLScriptElement =
-      (document.getElementById(id) || createAsyncScript(id, src)) as HTMLScriptElement;
+      (document.getElementById(id) || createScript({async: true, ...options, src, id})) as HTMLScriptElement;
     const state = script.getAttribute('state');
 
     switch (state) {
