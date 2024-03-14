15e5506edd4287f1f4b067187b7e48cd9e1995fa
Feoktist Shovchko <fshovchko@exadel.com>
2024-03-14 18:45:26 +0200

    test(e2e): element screenshot

--- e2e/setup/scenarios.screenshot.ts
+++ e2e/setup/scenarios.screenshot.ts
@@ -14,6 +14,13 @@ cucumber.defineRule('take a screenshot of a full page', async (world: TestEnv) =
   world.screenshots.push(await page.screenshot({fullPage: true}));
 });
 
+cucumber.defineRule('take a screenshot of the element', async (world: TestEnv) => {
+  const element = world.elements[0];
+  const clip = await element?.boundingBox();
+  if (!clip) return;
+  world.screenshots.push(await page.screenshot({clip}));
+});
+
 cucumber.defineRule('check if the screenshot is exactly equal to the snapshoted version', (world: TestEnv) => {
   if (!world.screenshots.length) throw new Error('E2E: there is no any screenshot, make sure you have "Take a screenshot" before');
   const image = world.screenshots.pop();
