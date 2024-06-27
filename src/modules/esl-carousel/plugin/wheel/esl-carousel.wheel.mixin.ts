376f30328f79c9533a2f1e4bb4cabee427d1de91
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-06-27 19:31:31 +0200

    feat(esl-carousel): make ESLCarouselWheelMixin respect direction & able to prevent default wheel action

--- src/modules/esl-carousel/plugin/wheel/esl-carousel.wheel.mixin.ts
+++ src/modules/esl-carousel/plugin/wheel/esl-carousel.wheel.mixin.ts
@@ -1,5 +1,6 @@
 import {ExportNs} from '../../../esl-utils/environment/export-ns';
 import {throttle} from '../../../esl-utils/async/throttle';
+import {parseBoolean} from '../../../esl-utils/misc/format';
 import {attr, bind, decorate, listen} from '../../../esl-utils/decorators';
 import {ESLWheelEvent, ESLWheelTarget} from '../../../esl-event-listener/core';
 
@@ -25,8 +26,33 @@ export class ESLCarouselWheelMixin extends ESLCarouselPlugin {
   })
   public ignore: string;
 
+  /**
+   * Restricts wheel direction.
+   * Values:
+   * - 'auto' - depends on the carousel orientation (default)
+   * - 'x' - horizontal only
+   * - 'y' - vertical only
+   */
+  @attr({name: ESLCarouselWheelMixin.is + '-direction'}) public direction: string;
+
+  /** Prevent default action for wheel event */
+  @attr({
+    name: ESLCarouselWheelMixin.is + '-prevent-default',
+    defaultValue: true,
+    parser: parseBoolean
+  }) public preventDefault: boolean;
+
+  /** @returns true if the plugin should track vertical wheel */
+  protected get isVertical(): boolean {
+    if (this.direction === 'x') return false;
+    if (this.direction === 'y') return true;
+    return this.$host.state.vertical;
+  }
+
+  /** @returns true if the plugin should track passed event */
   @bind
   protected isEventIgnored(e: WheelEvent & {target: Element}): boolean {
+    if (e.shiftKey === this.isVertical) return true;
     return !!this.ignore && !!e.target.closest(this.ignore);
   }
 
@@ -34,13 +60,16 @@ export class ESLCarouselWheelMixin extends ESLCarouselPlugin {
   @listen({
     event: ESLWheelEvent.TYPE,
     target: (plugin: ESLCarouselWheelMixin) => ESLWheelTarget.for(plugin.$host, {
+      distance: 10,
+      preventDefault: plugin.preventDefault,
       ignore: plugin.isEventIgnored
     })
   })
   @decorate(throttle, 400)
   protected _onWheel(e: ESLWheelEvent): void {
     if (!this.$host || this.$host.animating) return;
-    const direction = e.deltaY > 0 ? 'next' : 'prev';
+    const delta = this.isVertical ? e.deltaY : e.deltaX;
+    const direction = delta > 0 ? 'next' : 'prev';
     this.$host?.goTo(`${this.type || 'slide'}:${direction}`);
   }
 }
