d2ea5e7731f1ee20167985c03c87c9d1934284ac
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-07-10 23:57:43 +0200

    chore(site): fix site sorters, make them generalized

--- site/11ty/sort.filter.js
+++ site/11ty/sort.filter.js
@@ -1,31 +1,47 @@
 module.exports = (config) => {
-  /** Generic sort njk filter */
-  const createSortFilter = (comparer) => (values) => {
+  /** Resolve date from item */
+  const resolveDate = (item) => new Date(item.date).getTime();
+  /** Resolve date from item without fileUpdateDate fallback */
+  const resolveMetaDate = (item) => item.data.date ? new Date(item.date).getTime() : Number.POSITIVE_INFINITY;
+
+  /** Date metadata comparer */
+  const dateComparer = (a, b) => resolveDate(a) - resolveDate(b);
+  /** Date metadata comparer (will not use file creation date) */
+  const metaDateComparer = (a, b) => resolveMetaDate(a) - resolveMetaDate(b);
+
+  /** Order metadata comparer */
+  const orderComparer = (a, b) => (a.data.order ?? 0) - (b.data.order ?? 0);
+
+  /** Abstract string comparer */
+  const stringComparer = (field) => (a, b) => {
+    const aField = a.data[field] || '';
+    const bField = b.data[field] || '';
+    return aField.localeCompare(bField, 'en');
+  };
+
+  /** Build comparer for field */
+  const buildComparer = (field) => {
+    switch (field) {
+      case 'date':
+        return dateComparer;
+      case 'meta-date':
+        return metaDateComparer;
+      case 'order':
+        return orderComparer;
+      default:
+        return stringComparer(field);
+    }
+  };
+
+  /** Comparer composer */
+  const compose = (...comparers) => (a, b) => comparers.reduce((res, cmp) => res || cmp(a, b), 0);
+
+  config.addFilter('sortBy', (values, ...fields) => {
     if (!Array.isArray(values)) {
       console.error(`Unexpected values for sort filter: ${values}`);
       return values;
     }
-    return [...values].sort(comparer);
-  };
-
-  // Utils
-  const resolveDate = (item) => new Date(item.date).getTime();
-  const resolveMetaDate = (item) => item.data.date ? new Date(item.date).getTime() : Number.POSITIVE_INFINITY;
-
-  /** Comparer composer */
-  const compose = (...cmps) => (a, b) => cmps.reduce((res, cmp) => res || cmp(a, b), 0);
-
-  /** Name metadata comparer */
-  const nameComparer = (a, b) => a.data.name.localeCompare(b.data.name);
-  /** Order metadata comparer */
-  const orderComparer = (a, b) => (a.data.order || 0) - (b.data.order || 0);
-  /** Date metadata comparer */
-  const dateComparer = (a, b) => resolveDate(a) - resolveDate(b);
-  /** Date metadata comparer (will not use file creation date) */
-  const dateComparerStrict = (a, b) => resolveMetaDate(a) - resolveMetaDate(b);
-
-  config.addFilter('sortByName', createSortFilter(nameComparer));
-  config.addFilter('sortByNameAndOrder', createSortFilter(compose(orderComparer, nameComparer)));
-  config.addFilter('sortByDate', createSortFilter(dateComparer));
-  config.addFilter('sortByDateStrict', createSortFilter(dateComparerStrict));
+    const comparers = fields.map(buildComparer);
+    return [...values].sort(compose(...comparers));
+  });
 };
