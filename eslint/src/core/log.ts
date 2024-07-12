c85bd5a702cc573528fea4a67506ce0c688dd664
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-07-12 19:44:15 +0200

    refactor(lint): check version common utility revisited, logger utility introduced

---
+++ eslint/src/core/log.ts
@@ -0,0 +1,29 @@
+import color from 'kleur';
+import type {Rule} from 'eslint';
+
+const LOGGERS = {
+  error: console.error,
+  warn: console.warn,
+  off: console.info,
+};
+const HEADERS = {
+  error: color.red('[ESL Lint Plugin] ❌ Error:'),
+  warn: color.yellow('[ESL Lint Plugin] ⚠️ Warning:'),
+  off: color.green('[ESL Lint Plugin] ℹ️ Info:'),
+};
+
+export function log(msg: string, severity: 'error' | 'warn' | 'off' = 'off'): void {
+  LOGGERS[severity](`\n${HEADERS[severity]}\n${msg}`);
+}
+
+export function buildLoggingRule(msg: string, severity: 'error' | 'warn' | 'off' = 'off'): Rule.RuleModule {
+  log(msg, severity);
+  return {
+    meta: {
+      docs: {
+        description: msg,
+      }
+    },
+    create: (): Rule.RuleListener => ({})
+  };
+}
