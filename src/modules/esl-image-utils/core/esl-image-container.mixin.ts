602afc7eb4e208c11d89cfe9bce8198d4c0b664e
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2024-08-12 23:05:33 +0200

    refactor(esl-image-utils): update implementation to use json attr and new compact representation
    BREAKING-CHANGE: legacy implementation of `esl-image` no longer distributes aspect-ratio styles

--- src/modules/esl-image-utils/core/esl-image-container.mixin.ts
+++ src/modules/esl-image-utils/core/esl-image-container.mixin.ts
@@ -1,59 +1,88 @@
 import {ESLMixinElement} from '../../esl-mixin-element/core';
-import {ESLTraversingQuery} from '../../esl-traversing-query/core';
-import {attr, listen} from '../../esl-utils/decorators';
-import {CSSClassUtils} from '../../esl-utils/dom';
+import {jsonAttr, listen, memoize} from '../../esl-utils/decorators';
 import {ExportNs} from '../../esl-utils/environment/export-ns';
+import {findAll} from '../../esl-utils/dom/traversing';
 
 /**
- * ESLImgContainerMixin - mixin to provide image container functionality
+ * ESLImageContainerConfig - interface for ESLImageContainerMixin config
+ */
+export interface ESLImageContainerConfig {
+  /** Class to add to the target element when the image is loaded (successfully) */
+  readyCls: string;
+  /** Class to add to the target element when the image is not loaded (error) */
+  errorCls: string;
+  /** Image element selector */
+  selector: string;
+}
+
+/**
+ * ESLImageContainerMixin - mixin to provide image loading state class functionality
  * @author Anna Barmina, Alexey Stsefanovich (ala'n)
  *
  * Use example:
  * ```
- * <picture class="img-container img-container-16-9">
- *   <img esl-img-container loading="lazy" alt="img" src="img.png"/>
+ * <picture class="img-container img-container-16-9" esl-image-container>
+ *   <img loading="lazy" alt="img" src="img.png"/>
  * </picture>
  * ```
  *
- * This mixin is used to enhance an image element by adding specific classes to a target when the image has completely loaded or not
+ * This mixin is used to enhance native image developer experience by adding specific classes when the image has completely loaded or not
  */
+@ExportNs('ImageContainer')
+export class ESLImageContainerMixin extends ESLMixinElement {
+  public static override is = 'esl-image-container';
 
-@ExportNs('ImgContainer')
-export class ESLImgContainerMixin extends ESLMixinElement {
-  static override is = 'esl-img-container';
+  /** Default configuration object */
+  public static DEFAULT_CONFIG: ESLImageContainerConfig = {
+    readyCls: 'img-container-loaded',
+    errorCls: '',
+    selector: 'img',
+  };
 
-  /** Target element selector ('::parent' by default) */
-  @attr({name: ESLImgContainerMixin.is}) public target: string;
-  /** Class to add to the target element when the image is loaded */
-  @attr({name: ESLImgContainerMixin.is + '-cls', defaultValue: 'img-container-loaded'}) public targetCls: string;
-  /** Class to add to the target element when the image is not loaded */
-  @attr({name: ESLImgContainerMixin.is + '-error-cls'}) public targetErrorCls: string;
+  /** Configuration object */
+  @jsonAttr({name: ESLImageContainerMixin.is}) public rawConfig: Partial<ESLImageContainerConfig>;
 
-  public override $host!: HTMLImageElement;
+  /** Merged configuration object */
+  @memoize()
+  public get config(): ESLImageContainerConfig {
+    return {...ESLImageContainerMixin.DEFAULT_CONFIG, ...this.rawConfig};
+  }
 
-  get $target(): Element | null {
-    return ESLTraversingQuery.first(this.target || '::parent', this.$host);
+  /** Image element */
+  @memoize()
+  protected get $images(): HTMLImageElement[] {
+    if (this.$host.tagName === 'IMG') return [this.$host as HTMLImageElement];
+    return findAll(this.$host, this.config.selector) as HTMLImageElement[];
+  }
+
+  /** Check if all images are loaded */
+  public get complete(): boolean {
+    return this.$images.every(img => img.complete);
+  }
+  /** Check if any image has loading error */
+  public get hasError(): boolean {
+    return this.$images.some(img => !img.naturalHeight && !img.naturalWidth);
   }
 
   protected override connectedCallback(): void {
-    if (this.$host.tagName !== 'IMG') return;
     super.connectedCallback();
-    if (this.$host.complete) {
-      const eventType = this.$host.naturalHeight && this.$host.naturalWidth ? 'load' : 'error';
-      this._onReady(new Event(eventType));
-    }
-    else this.$$on(this._onReady);
+    this._onReady();
+  }
+
+  protected override attributeChangedCallback(name: string): void {
+    if (name !== ESLImageContainerMixin.is) return;
+    memoize.clear(this, ['config', '$images']);
+    this._onReady();
   }
 
   @listen({
     event: 'load error',
-    auto: false,
-    once: true
+    target: (that: ESLImageContainerMixin) => that.$images
   })
-  protected _onReady(event: Event): void {
-    const {$target} = this;
-    if (!$target) return;
-    CSSClassUtils.add($target, this.targetCls);
-    if (event.type === 'error') CSSClassUtils.add($target, this.targetErrorCls);
+  protected _onReady(): void {
+    if (!this.complete) return;
+    this.$$off();
+    this.$$cls(this.config.readyCls, true);
+    this.$$cls(this.config.errorCls, this.hasError);
   }
 }
