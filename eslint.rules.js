bdf34371f94293f5bce80872cd8ec7a339485499
alesun <alesun@exadel.com>
2024-08-26 17:02:57 +0300

    style(deps): turn off import.eslintrc.yml

--- eslint.rules.js
+++ eslint.rules.js
@@ -2,7 +2,7 @@ const yaml = require('js-yaml');
 const path = require('path')
 const fs = require('fs');
 
-const LINT_PATHS = ['deprecated', 'codestyle', 'coderules', 'import', 'sonarjs'];
+const LINT_PATHS = ['deprecated', 'codestyle', 'coderules', 'sonarjs'];
 
 const LINT_RULES = LINT_PATHS.reduce((acc,name) => {
   const content = fs.readFileSync(path.resolve(__dirname, `./linting/${name}.eslintrc.yml`), 'utf8');
