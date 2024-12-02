b6d1d6a3669d62996c3e824e929050b499be9095
alesun <alesun@exadel.com>
2024-12-02 12:40:05 +0300

    style(site): move treepath filter

--- site/11ty/tree.filter.js
+++ site/11ty/tree.filter.js
@@ -21,6 +21,25 @@ function treeBuilder(items) {
   return root;
 }
 
+function treePath(items) {
+  const possibleTags = ['introduction', 'core', 'components', 'examples', 'blogs', 'dev'];
+  const result = [];
+  if (!Array.isArray(items)) return;
+  items.forEach((item) => {
+    const {tags} = item.data;
+    if (!possibleTags.includes(tags[0])) return;
+    const {title, parent} = item.data;
+    const parents = [{'title': tags[0].replace(/\b\w/g, c => c.toUpperCase()), 'url': `/${tags[0]}/`}];
+    if (parent) {
+      const nextParent = findParent(items, parent);
+      parents.push({'title': parent, 'url': nextParent.data.page.url});
+    }
+    result.push({title, 'parents': parents});
+  })
+  return result;
+}
+
 module.exports = (config) => {
   config.addFilter('tree', treeBuilder);
+  config.addFilter('treepath', treePath);
 };
