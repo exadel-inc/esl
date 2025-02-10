d8da9518ad088e78e1983fa623884f414219a6bc
alesun <alesun@exadel.com>
2025-02-10 19:39:15 +0100

    fix(esl-event-listener): subscribe descriptors if the host is function/class

--- src/modules/esl-event-listener/test/descriptor.test.ts
+++ src/modules/esl-event-listener/test/descriptor.test.ts
@@ -1,4 +1,5 @@
 import {ESLEventUtils} from '../core';
+import {listen} from '../../esl-utils/decorators/listen';
 
 describe('dom/events: ESLEventUtils: ESLListenerDescriptor Utils', () => {
   describe('ESLEventUtils.isEventDescriptor', () => {
@@ -182,6 +183,20 @@ describe('dom/events: ESLEventUtils: ESLListenerDescriptor Utils', () => {
       const obj = {onEvent: () => void 0};
       ESLEventUtils.initDescriptor(obj, 'onEvent', {event: 'event'});
       expect(ESLEventUtils.descriptors(obj)).toEqual([obj.onEvent]);
+    });
+
+    test('ESLEventListener.descriptors: are collected and subscribed in case of any object-like host', () => {
+      class HostClass {
+        @listen({event: 'event-one'})
+        onEventOne() {}
+        @listen({event: 'event-two'})
+        onEventTwo() {}
+      }
+      const $host = new HostClass();
+      ESLEventUtils.subscribe($host);
+      expect(ESLEventUtils.descriptors($host)).toContain(HostClass.prototype.onEventOne);
+      expect(ESLEventUtils.descriptors($host)).toContain(HostClass.prototype.onEventTwo);
+      ESLEventUtils.unsubscribe($host);
     });
 
     describe('ESLEventUtils.descriptors: filters by criteria', () => {
