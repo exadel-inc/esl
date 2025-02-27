db466d904f8b8f6f07eecf1b683ed5f04b87fa72
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2025-02-27 17:28:40 +0100

    feat(stylelint-config): introducing `@exadel/stylelint-config-esl` instead of prettier

---
+++ stylelint-config/index.js
@@ -0,0 +1,13 @@
+/** @type {import('stylelint').Config} */
+export default {
+  plugins: [`@stylistic/stylelint-plugin`],
+  extends: [`@stylistic/stylelint-config`],
+  rules: {
+    '@stylistic/string-quotes': 'single',
+    '@stylistic/declaration-colon-newline-after': null,
+    '@stylistic/max-line-length': null,
+    '@stylistic/indentation': [2, {
+      ignore: ['value', 'inside-parens']
+    }]
+  }
+};
