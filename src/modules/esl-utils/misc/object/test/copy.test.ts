503ef52e23b4f5d6fc05700abc8fe68ce39cff3d
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2025-03-19 00:08:14 +0100

    feat(esl-utils): add `pick` method implementation to `esl-utils/misc/object`

--- src/modules/esl-utils/misc/object/test/copy.test.ts
+++ src/modules/esl-utils/misc/object/test/copy.test.ts
@@ -1,4 +1,4 @@
-import {copy, copyDefinedKeys, omit} from '../copy';
+import {copy, copyDefinedKeys, omit, pick} from '../copy';
 
 describe('misc/object: copy', () => {
   describe('copy', () => {
@@ -9,7 +9,7 @@ describe('misc/object: copy', () => {
       [[1, 2], {0: 1, 1: 2}],
       [{a: 1, b: {c: 2}}, {a: 1, b: {c: 2}}],
     ])('full copy of %p', (inp, out) => {
-      expect(copy(inp)).toEqual(out);
+      expect(copy(inp as any)).toEqual(out);
     });
 
     const predicate = (key: string) => !key.startsWith('_');
@@ -22,13 +22,13 @@ describe('misc/object: copy', () => {
       [{_a: 1, b: 1}, {b: 1}],
       [{a: 1, b: {c: 2}}, {a: 1, b: {c: 2}}],
     ])('copy %p with predicate', (inp, out) => {
-      expect(copy(inp, predicate)).toEqual(out);
+      expect(copy(inp as any, predicate)).toEqual(out);
     });
 
     test('special cases', () => {
       const obj = {_a: 1, b: 2};
       Object.setPrototypeOf(obj, {c: 3, _d: 4});
-      expect(copy(obj, predicate)).toEqual({b: 2});
+      expect(copy(obj as any, predicate)).toEqual({b: 2});
     });
   });
 
@@ -42,7 +42,7 @@ describe('misc/object: copy', () => {
       [{a: 1, b: 2}, {a: 1, b: 2}],
       [{a: 1, b: {}}, {a: 1, b: {}}]
     ])('%p to %p', (inp, out) => {
-      expect(copyDefinedKeys(inp)).toEqual(out);
+      expect(copyDefinedKeys(inp as any)).toEqual(out);
     });
   });
 
@@ -58,7 +58,7 @@ describe('misc/object: copy', () => {
       [{a: 1, b: 1}, ['c'], {a: 1, b: 1}],
       [{a: 1, b: {}}, [], {a: 1, b: {}}],
     ])('omit from %p properties %p', (inp, keys, out) => {
-      expect(omit(inp, keys)).toEqual(out);
+      expect(omit(inp as any, keys)).toEqual(out);
     });
   });
 
@@ -80,5 +80,21 @@ describe('misc/object: copy', () => {
     expect(({} as any).polluted).not.toBe(true); // safe Plain Old Javascript Object
     expect(a.polluted).not.toBe(true); // safe a input
     expect(copied.constructor.prototype.polluted).toBe(true);
+  });
+
+  describe('pick', () => {
+    test.each([
+      [undefined, ['prop'], {}],
+      [null, ['prop'], {}],
+      [{}, ['prop'], {}],
+      [[1, 2], ['1'], {1: 2}],
+      [[1, 2], ['0', '1'], {0: 1, 1: 2}],
+      [{a: 1, b: 1}, ['a', 'b'], {a: 1, b: 1}],
+      [{a: 1, b: 1}, ['b'], {b: 1}],
+      [{a: 1, b: 1}, ['c'], {}],
+      [{a: 1, b: {}}, ['a', 'b'], {a: 1, b: {}}]
+    ])('omit from %p properties %p', (inp, keys, out) => {
+      expect(pick(inp as any, keys)).toEqual(out);
+    });
   });
 });
