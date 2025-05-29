e32fce8b6f0315fdaa6804e3238db010906f4fa2
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2025-05-29 03:04:33 +0200

    feat(esl-carousel): `esl-carousel-autoplay` plugin full support
    Here is the full list of what new capabilities include:
     - Ability to pass multiple configuration.
     - Ability to control duration using time format.
     - Ability to pass selector for controls to toggle plugin state.
     - Ability to pass state marker classes to add to controls or/and container.
     - Dispatching of the state events.
     - Optional mixin/example - esl-carousel-autoplay-progress - to make simple autoplay progress UI
       (you can do progress controls based on CSS animation/transition out of the box without JS or use mixin source code as an example to crete custom control)

--- packages/esl/src/esl-carousel/plugin/autoplay/esl-carousel.autoplay.mixin.ts
+++ packages/esl/src/esl-carousel/plugin/autoplay/esl-carousel.autoplay.mixin.ts
@@ -1,15 +1,24 @@
 import {ExportNs} from '../../../esl-utils/environment/export-ns';
-import {bind, listen, memoize, ready} from '../../../esl-utils/decorators';
+import {listen, memoize, ready} from '../../../esl-utils/decorators';
+import {parseTime} from '../../../esl-utils/misc/format';
+import {CSSClassUtils} from '../../../esl-utils/dom/class';
+import {ESLTraversingQuery} from '../../../esl-traversing-query/core';
 
 import {ESLCarouselPlugin} from '../esl-carousel.plugin';
 import {ESLCarouselSlideEvent} from '../../core/esl-carousel.events';
-import {parseTime} from '../../../esl-utils/misc/format';
+import {ESLCarouselAutoplayEvent} from './esl-carousel.autoplay.event';
 
 export interface ESLCarouselAutoplayConfig {
   /** Timeout to send next command to the host carousel */
-  timeout: string | number;
+  duration: string | number;
   /** Navigation command to send to the host carousel. Default: 'slide:next' */
   command: string;
+  /** Selector for control to toggle plugin state */
+  control?: string;
+  /** Class to toggle on control element, when autoplay is active */
+  controlCls?: string;
+  /** Class to toggle on container element, when autoplay is active */
+  containerCls?: string;
 }
 
 /**
@@ -22,25 +31,46 @@ export interface ESLCarouselAutoplayConfig {
 export class ESLCarouselAutoplayMixin extends ESLCarouselPlugin<ESLCarouselAutoplayConfig> {
   public static override is = 'esl-carousel-autoplay';
   public static override DEFAULT_CONFIG: ESLCarouselAutoplayConfig = {
-    timeout: 5000,
+    duration: 5000,
     command: 'slide:next'
   };
-  public static override DEFAULT_CONFIG_KEY = 'timeout';
+  public static override DEFAULT_CONFIG_KEY: keyof ESLCarouselAutoplayConfig = 'duration';
 
-  private _timeout: number | null = null;
+  private _enabled: boolean = true;
+  private _duration: number | null = null;
 
+  /** True if the autoplay timer is currently active */
   public get active(): boolean {
-    return !!this._timeout;
+    return !!this._duration;
   }
-  public get timeout(): number {
-    return parseTime(this.config.timeout);
+
+  /** True if the autoplay plugin is enabled */
+  public get enabled(): boolean {
+    return this._enabled;
+  }
+  protected set enabled(value: boolean) {
+    this._enabled = value;
+    CSSClassUtils.toggle(this.$controls, this.config.controlCls, this._enabled);
+    const {$container} = this.$host;
+    $container && CSSClassUtils.toggle($container, this.config.containerCls, this._enabled);
+  }
+
+  /** The duration of the autoplay timer in milliseconds */
+  public get duration(): number {
+    return parseTime(this.config.duration);
+  }
+
+  /** A list of control elements to toggle plugin state */
+  @memoize()
+  public get $controls(): HTMLElement[] {
+    const sel = this.config.control;
+    return sel ? ESLTraversingQuery.all(sel, this.$host) as HTMLElement[] : [];
   }
 
   @ready
   protected override connectedCallback(): void {
-    if (super.connectedCallback()) {
-      this.start();
-    }
+    super.connectedCallback();
+    this.start();
   }
 
   protected override disconnectedCallback(): void {
@@ -48,41 +78,74 @@ export class ESLCarouselAutoplayMixin extends ESLCarouselPlugin<ESLCarouselAutop
     this.stop();
   }
 
+  @listen({inherit: true})
   protected override onConfigChange(): void {
+    super.onConfigChange();
+    memoize.clear(this, ['$controls', 'duration']);
+    // Full restart during config change
+    this.stop();
     this.start();
   }
 
-  /** Activates the timer to send commands */
+  /** Activates and restarts the autoplay carousel timer */
   public start(): void {
-    this.stop();
-    const {timeout} = this;
-    if (timeout <= 0 || isNaN(+timeout)) return;
-    this._timeout = window.setTimeout(this._onInterval, timeout);
+    const {duration} = this;
+    this.enabled = +duration > 0;
+    if (this.enabled) this._onCycle();
   }
 
-  /** Deactivates the timer to send commands */
-  public stop(): void {
-    this._timeout && window.clearTimeout(this._timeout);
-    this._timeout = null;
+  /**
+   * Deactivates the autoplay carousel timer.
+   * @param system - If true, the plugin will be suspended but not disabled.
+   */
+  public stop(system = false): void {
+    if (!this.active && !this.enabled) return;
+    if (!system) this.enabled = false;
+    this._duration && window.clearTimeout(this._duration);
+    this._duration = null;
+    ESLCarouselAutoplayEvent.dispatch(this);
   }
 
-  /** Handles next timer interval */
-  @bind
-  protected _onInterval(): void {
-    this.$host?.goTo(this.config.command);
-    this._timeout = window.setTimeout(this._onInterval, this.timeout);
+  /**
+   * Starts a new autoplay cycle.
+   * Produces cycle self call after a timeout with enabled command execution.
+   */
+  protected async _onCycle(exec?: boolean): Promise<void> {
+    this._duration && window.clearTimeout(this._duration);
+    this._duration = null;
+    if (exec) await this.$host?.goTo(this.config.command);
+    if (!this.enabled || this.active) return;
+    const {duration} = this;
+    this._duration = window.setTimeout(() => this._onCycle(true), duration);
+    ESLCarouselAutoplayEvent.dispatch(this);
   }
 
-  /** Handles auxiliary events to pause/resume timer */
-  @listen(`mouseout mouseover focusin focusout ${ESLCarouselSlideEvent.AFTER}`)
+  /** Handles click on control element to toggle plugin state */
+  @listen({
+    event: 'click',
+    target: ($this: ESLCarouselAutoplayMixin)=> $this.$controls,
+    condition: ($this: ESLCarouselAutoplayMixin)=> !!$this.$controls.length
+  })
+  protected _onToggle(e: Event): void {
+    this.enabled ? this.stop() : this.start();
+    e.preventDefault();
+  }
+
+  /** Handles auxiliary events that represent user interaction to pause/resume timer */
+  @listen('mouseleave mouseenter focusin focusout')
   protected _onInteract(e: Event): void {
-    // Slide change can only delay the timer, but not start it
-    if (e.type === ESLCarouselSlideEvent.AFTER && !this.active) return;
-    if (['mouseover', 'focusin'].includes(e.type)) {
-      this.stop();
+    if (!this.enabled) return;
+    if (['mouseenter', 'focusin'].includes(e.type)) {
+      this.stop(true);
     } else {
       this.start();
     }
+  }
+
+  /** Handles carousel slide change event to restart the timer */
+  @listen(ESLCarouselSlideEvent.AFTER)
+  protected _onSlideChange(): void {
+    if (this.active) this.start();
   }
 }
 
