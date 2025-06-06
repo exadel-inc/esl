34a70fab80f92d1f8572bbf219d5546d7164ceb8
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2025-06-06 04:30:04 +0200

    fix(esl-carousel): fix incorrect `canNavigate` behaviour for undefined direction navigation (equal distance for both direction to reach the target slide)

--- packages/esl/src/esl-carousel/test/core/esl-carousel.utils.base.test.ts
+++ packages/esl/src/esl-carousel/test/core/esl-carousel.utils.base.test.ts
@@ -1,15 +1,8 @@
-import {
-  normalize,
-  groupToIndex,
-  indexToGroup,
-  indexToDirection,
-  toIndex, canNavigate
-} from '../../core/esl-carousel.utils';
+import {normalize, groupToIndex, indexToGroup, indexToDirection, toIndex} from '../../core/esl-carousel.utils';
 import {ESLCarouselDirection} from '../../core/esl-carousel.types';
-
 import type {ESLCarouselSlideTarget, ESLCarouselState} from '../../core/esl-carousel.types';
 
-describe('ESLCarousel: Nav Utils', () => {
+describe('ESLCarousel: Nav Utils (Core)', () => {
   describe('normalize', () => {
     test.each([
       // [index, size, result]
@@ -284,57 +277,5 @@ describe('ESLCarousel: Nav Utils', () => {
         (target: ESLCarouselSlideTarget, cfg: ESLCarouselState, result: number) => expect(toIndex(target, cfg).index).toBe(result)
       );
     });
-  });
-
-  describe('canNavigate',  () => {
-    test.each([
-      // Loop for complete
-      ['next', {activeIndex: 0, size: 5, count: 1, loop: true}],
-      ['prev', {activeIndex: 0, size: 5, count: 1, loop: true}],
-      ['next', {activeIndex: 4, size: 5, count: 1, loop: true}],
-      // Non loop case with free space
-      ['next', {activeIndex: 0, size: 5, count: 1, loop: false}],
-      ['prev', {activeIndex: 4, size: 5, count: 1, loop: false}],
-      ['next', {activeIndex: 2, size: 5, count: 1, loop: false}],
-      ['prev', {activeIndex: 2, size: 5, count: 1, loop: false}],
-      // Group cases
-      ['group: next', {activeIndex: 0, size: 5, count: 2, loop: true}],
-      ['group: prev', {activeIndex: 0, size: 5, count: 2, loop: true}],
-      ['group: next', {activeIndex: 4, size: 5, count: 2, loop: true}],
-      ['group: prev', {activeIndex: 4, size: 5, count: 2, loop: true}],
-      // Non loop case with free space
-      ['group: next', {activeIndex: 0, size: 5, count: 2, loop: false}],
-      ['group: prev', {activeIndex: 2, size: 5, count: 2, loop: false}],
-      ['group: prev', {activeIndex: 4, size: 5, count: 2, loop: false}]
-    ])(
-      'Target %s is allowed for %p configuration',
-      (target: ESLCarouselSlideTarget, cfg: ESLCarouselState) => expect(canNavigate(target, cfg)).toBe(true)
-    );
-
-    test.each([
-      // Non loop case with no free space
-      ['next', {activeIndex: 4, size: 5, count: 1, loop: false}],
-      ['prev', {activeIndex: 0, size: 5, count: 1, loop: false}],
-      // Incomplete cases
-      ['next', {activeIndex: 0, size: 1, count: 1, loop: false}],
-      ['prev', {activeIndex: 0, size: 1, count: 1, loop: false}],
-      ['next', {activeIndex: 0, size: 1, count: 1, loop: true}],
-      ['prev', {activeIndex: 0, size: 1, count: 1, loop: true}],
-      ['next', {activeIndex: 0, size: 2, count: 2, loop: true}],
-      ['prev', {activeIndex: 0, size: 2, count: 2, loop: true}],
-      ['next', {activeIndex: 0, size: 1, count: 2, loop: true}],
-      ['prev', {activeIndex: 0, size: 1, count: 2, loop: true}],
-      // Same slide
-      ['0', {activeIndex: 0, size: 5, count: 1, loop: false}],
-      ['0', {activeIndex: 0, size: 5, count: 1, loop: true}],
-      ['1', {activeIndex: 1, size: 5, count: 2, loop: false}],
-      ['1', {activeIndex: 1, size: 5, count: 2, loop: true}],
-      // Group cases with no loop
-      ['group: next', {activeIndex: 4, size: 5, count: 2, loop: false}],
-      ['group: prev', {activeIndex: 0, size: 5, count: 2, loop: false}]
-    ])(
-      'Target %s is not allowed for %p configuration',
-      (target: ESLCarouselSlideTarget, cfg: ESLCarouselState) => expect(canNavigate(target, cfg)).toBe(false)
-    );
   });
 });
