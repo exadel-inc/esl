cdd46db033b9ee778815868229d04eb57b90d51a
Dmitry Shovchko <d.shovchko@gmail.com>
2025-08-26 15:57:23 +0300

    feat(esl-line-clamp): add auto mode for esl-line-clamp mixin

--- packages/esl/src/esl-line-clamp/core/esl-line-clamp.ts
+++ packages/esl/src/esl-line-clamp/core/esl-line-clamp.ts
@@ -1,9 +1,13 @@
 import {ExportNs} from '../../esl-utils/environment/export-ns';
 import {ESLMixinElement} from '../../esl-mixin-element/core';
 import {attr, boolAttr, listen, memoize} from '../../esl-utils/decorators';
-import {isElement} from '../../esl-utils/dom';
+import {isElement, isRelativeNode} from '../../esl-utils/dom';
 import {ESLResizeObserverTarget} from '../../esl-event-listener/core';
 import {ESLMediaRuleList} from '../../esl-media-query/core';
+
+function roundLineHeight(value: number, precision = 0.1): number {
+  return Math.round(value / precision) * precision;
+}
 
 /**
  * ESLLineClamp mixin element
@@ -17,6 +21,8 @@ import {ESLMediaRuleList} from '../../esl-media-query/core';
 export class ESLLineClamp extends ESLMixinElement {
   static override is = 'esl-line-clamp';
 
+  static mask: string = '@XS|@SM|@MD|@LG|@XL';
+
   /** Indicates whether the line clamping is active */
   @boolAttr({name: 'clamped', readonly: true}) public clamped: boolean;
 
@@ -29,7 +35,24 @@ export class ESLLineClamp extends ESLMixinElement {
    */
   @memoize()
   public get linesQuery(): ESLMediaRuleList<string> {
-    return ESLMediaRuleList.parse(this.lines, String);
+    return ESLMediaRuleList.parse(this.lines, ESLLineClamp.mask, String);
+  }
+
+  protected get linesExpected(): string | null {
+    const lines = this.linesQuery.value;
+    if (lines?.length === 0) return null;
+    if (lines === 'auto') {
+      const {lineHeight, maxHeight} = getComputedStyle(this.$host);
+
+      // Need to round line height after fractional conversion from em to px
+      const parsedLineHeight = roundLineHeight(parseFloat(lineHeight));
+      const parsedMaxHeight = parseFloat(maxHeight);
+      const autoLines = Math.floor(parsedMaxHeight / parsedLineHeight);
+
+      if (!Number.isFinite(autoLines)) return null;
+      return autoLines > 0 ? String(autoLines) : null;
+    }
+    return lines ?? null;
   }
 
   protected override connectedCallback(): void {
@@ -47,30 +70,49 @@ export class ESLLineClamp extends ESLMixinElement {
     this.onQueryChange();
   }
 
-  /** Handles query change and sets the CSS custom property for line clamping */
-  @listen({event: 'change', target: ($this: ESLLineClamp) => $this.linesQuery})
-  protected onQueryChange(): void {
-    const len = this.linesQuery.value;
-    if (len) {
-      this.$host.style.setProperty('--esl-line-clamp', len);
+  protected updateLines(): void {
+    const lines = this.linesExpected;
+    if (lines) {
+      this.$host.style.setProperty('--esl-line-clamp', lines);
     } else {
       this.$host.style.removeProperty('--esl-line-clamp');
     }
   }
 
+  /** Handles query change and sets the CSS custom property for line clamping */
+  @listen({event: 'change', target: ($this: ESLLineClamp) => $this.linesQuery})
+  protected onQueryChange(): void {
+    this.updateLines();
+  }
+
   /** Handles focus out event and resets scroll position */
   @listen('focusout')
   protected onFocusOut(e: FocusEvent): void {
-    const $target = e.relatedTarget as HTMLElement;
-    if (!isElement($target) || this.$host.contains($target)) return;
+    const $relatedTarget = e.relatedTarget as HTMLElement;
+    if (!isElement($relatedTarget) || this.$host.contains($relatedTarget)) return;
+    const $target = e.target as any;
+    if (isElement($target.$target) && $target.$target.contains($relatedTarget)) return;
     this.$host.scrollTo(0, 0);
   }
 
+  protected lastHeight = 0;
   /** Handles resize events to update clamped state */
   @listen({event: 'resize', target: ESLResizeObserverTarget.for})
   protected onResize(): void {
+    const {height} = this.$host.getBoundingClientRect();
+    if (this.lastHeight !== height && this.linesQuery.value === 'auto') {
+      this.lastHeight = height;
+      this.updateLines();
+    }
     const {clientHeight, clientWidth, scrollHeight, scrollWidth} = this.$host;
     this.$$attr('clamped', clientHeight < scrollHeight || clientWidth < scrollWidth);
+  }
+
+  /** Observes custom broadcast 'esl:refresh' event to force refresh */
+  @listen({auto: false, event: 'esl:refresh', target: window})
+  protected onRefresh({target}: Event): void {
+    if (isElement(target) && !isRelativeNode(target, this.$host)) return;
+    this.updateLines();
   }
 
   /** Handles show request events to scroll target into view */
