dbeec9bcef21c7b78bc5f7303577b0e999deff6e
alesun <alesun@exadel.com>
2025-09-24 12:12:44 +0200

    test(site): make error to test

--- packages/snapshot-tests/src/reporters/printers.js
+++ packages/snapshot-tests/src/reporters/printers.js
@@ -1,20 +1,18 @@
 import nunjucks from 'nunjucks';
+const env = new nunjucks.Environment();
 
-const resolveURL = (basePath, snapshot) => {
+env.addFilter('resolveURL', function resolveURL(basePath, snapshot) {
   if (!basePath) return snapshot;
   let path = basePath + (basePath.endsWith('/') ? '' : '/') + snapshot;
   if (basePath.includes('github')) path += '?raw=true';
   return path.replace(/\\/g, '/');
-};
+});
 
 const printSummary = (stats, template) => nunjucks.render(template, {stats});
 
 const printFiles = (files, basePath, template) => nunjucks.render(template, {files, basePath});
 
 export function print({stats, files, basePath, templates}) {
-  const env = new nunjucks.Environment();
-  env.addFilter('resolveURL', resolveURL);
-
   return `
   ${printSummary(stats, templates[0])}
   ${printFiles(files, basePath, templates[1])}
