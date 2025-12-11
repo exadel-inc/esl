fdc17b98e49526bf82b32044d27a51fa4287a2e1
Feoktist Shovchko <fshovchko@exadel.com>
2025-12-11 17:57:48 +0200

    chore(esl-line-clamp): add media query tests

--- packages/esl/src/esl-line-clamp/test/esl-line-clamp-alt.test.ts
+++ packages/esl/src/esl-line-clamp/test/esl-line-clamp-alt.test.ts
@@ -1,8 +1,14 @@
 import {ESLLineClampAlt} from '../core/esl-line-clamp-alt';
+import {ESLScreenBreakpoints} from '../../esl-media-query/core/common/screen-breakpoint';
+import {getMatchMediaMock} from '../../esl-utils/test/matchMedia.mock';
+import {ESLLineClamp} from '../core/esl-line-clamp';
 
 describe('ESLLineClampAlt (mixin): tests', () => {
   const microtaskQueue = () => Promise.resolve().then(() => Promise.resolve());
   let $host: HTMLElement;
+
+  const XLMediaMock = getMatchMediaMock(ESLScreenBreakpoints.get('xl')!.mediaQuery);
+  const SMMediaMock = getMatchMediaMock(ESLScreenBreakpoints.get('sm')!.mediaQuery);
 
   beforeAll(() => {
     ESLLineClampAlt.register();
@@ -24,9 +30,40 @@ describe('ESLLineClampAlt (mixin): tests', () => {
     expect(getComputedStyle($host).getPropertyValue('--esl-line-clamp-alt')).toBe('3');
   });
 
-  test('Media query attribute value reflected in the CSS props', async () => {
-    $host.setAttribute(ESLLineClampAlt.is, '@XS => 4 | 5 | @XL => 7');
+  test('should handle media queries', async () => {
+    SMMediaMock.matches = true;
+    XLMediaMock.matches = false;
+
+    $host.setAttribute(ESLLineClampAlt.is, '@SM => 5 | @XL => 7');
     await microtaskQueue();
     expect(getComputedStyle($host).getPropertyValue('--esl-line-clamp-alt')).toBe('5');
+
+    SMMediaMock.matches = false;
+    XLMediaMock.matches = true;
+
+    // Desn't work because onQueryChange callback is still subscribed to previous Mediarulelist `esl-line-clamp-alt="3"`
+    await microtaskQueue();
+    expect(getComputedStyle($host).getPropertyValue('--esl-line-clamp-alt')).toBe('7');
+
+    SMMediaMock.matches = false;
+    XLMediaMock.matches = false;
+  });
+
+  test('should handle media queries with mask', async () => {
+    SMMediaMock.matches = true;
+    XLMediaMock.matches = false;
+
+    $host.setAttribute(ESLLineClampAlt.is, '5 | 7');
+    $host.setAttribute(`${ESLLineClamp.is}-mask`, '@SM | @XL');
+    await microtaskQueue();
+
+    expect(getComputedStyle($host).getPropertyValue('--esl-line-clamp-alt')).toBe('5');
+
+    // Desn't work because onQueryChange callback is still subscribed to previous Mediarulelist `esl-line-clamp-alt="3"`
+    SMMediaMock.matches = false;
+    XLMediaMock.matches = true;
+
+    await microtaskQueue();
+    expect(getComputedStyle($host).getPropertyValue('--esl-line-clamp-alt')).toBe('7');
   });
 });
--- packages/esl/src/esl-line-clamp/test/esl-line-clamp.test.ts
+++ packages/esl/src/esl-line-clamp/test/esl-line-clamp.test.ts
@@ -1,9 +1,14 @@
 import {ESLLineClamp} from '../core/esl-line-clamp';
+import {ESLScreenBreakpoints} from '../../esl-media-query/core/common/screen-breakpoint';
+import {getMatchMediaMock} from '../../esl-utils/test/matchMedia.mock';
 
 describe('ESLLineClamp (mixin): tests', () => {
   const microtaskQueue = () => Promise.resolve().then(() => Promise.resolve());
   let $host: HTMLElement;
   let lineClamp: ESLLineClamp | null;
+
+  const XLMediaMock = getMatchMediaMock(ESLScreenBreakpoints.get('xl')!.mediaQuery);
+  const SMMediaMock = getMatchMediaMock(ESLScreenBreakpoints.get('sm')!.mediaQuery);
 
   beforeAll(() => {
     ESLLineClamp.register();
@@ -43,9 +48,40 @@ describe('ESLLineClamp (mixin): tests', () => {
   });
 
   test('should handle media queries', async () => {
-    $host.setAttribute(ESLLineClamp.is, '@XS => 4 | 5 | @XL => 7');
+    SMMediaMock.matches = true;
+    XLMediaMock.matches = false;
+
+    $host.setAttribute(ESLLineClamp.is, '@SM => 5 | @XL => 7');
     await microtaskQueue();
     expect($host.style.getPropertyValue('--esl-line-clamp')).toBe('5');
+
+    SMMediaMock.matches = false;
+    XLMediaMock.matches = true;
+
+    // Desn't work because onQueryChange callback is still subscribed to previous Mediarulelist `esl-line-clamp="3"`
+    await microtaskQueue();
+    expect($host.style.getPropertyValue('--esl-line-clamp')).toBe('7');
+
+    SMMediaMock.matches = false;
+    XLMediaMock.matches = false;
+  });
+
+  test('should handle media queries with mask', async () => {
+    SMMediaMock.matches = true;
+    XLMediaMock.matches = false;
+
+    $host.setAttribute(ESLLineClamp.is, '5 | 7');
+    $host.setAttribute(`${ESLLineClamp.is}-mask`, '@SM | @XL');
+    await microtaskQueue();
+
+    expect($host.style.getPropertyValue('--esl-line-clamp')).toBe('5');
+
+    // Desn't work because onQueryChange callback is still subscribed to previous Mediarulelist `esl-line-clamp="3"`
+    SMMediaMock.matches = false;
+    XLMediaMock.matches = true;
+
+    await microtaskQueue();
+    expect($host.style.getPropertyValue('--esl-line-clamp')).toBe('7');
   });
 
   describe('auto mode of clamping', () => {
