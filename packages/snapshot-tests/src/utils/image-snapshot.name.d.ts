58dbb5760e1fd5f311198c7bf0f7214fe7eafdef
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2026-01-30 19:24:29 +0100

    feat(e2e): legacy `snapshot-tests` module removed in flavor of new `esl-website-e2e` playwright based module

--- packages/snapshot-tests/src/utils/image-snapshot.name.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function buildSnapshotName(...snapshotParts: string[]): string;
-
-export declare function getSnapshotDir(testPath: string): string;
-
-export declare function getDiffDir(testPath: string): string;
-
-export declare function getDirName(testPath: string): string;
