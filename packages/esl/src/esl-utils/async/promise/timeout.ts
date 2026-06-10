1142a83102bbab153e7d3e9b60e78f621aa0e6dd
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2026-06-11 01:22:55 +0200

    refactor(esl-utils): update promisifyTimeout type to more accurate one

--- packages/esl/src/esl-utils/async/promise/timeout.ts
+++ packages/esl/src/esl-utils/async/promise/timeout.ts
@@ -1,9 +1,12 @@
+/** @returns Promise that will be resolved or rejected in `timeout` */
+export function promisifyTimeout(timeout: number): Promise<undefined>;
 /**
  * @returns Promise that will be resolved or rejected in `timeout` with an optional `payload`
  * If `isReject` is `true` the result promise will be rejected, otherwise (by default) the result promise will be resolved
  */
-export function promisifyTimeout<T>(timeout: number, payload?: T, isReject?: boolean): Promise<T> {
-  return new Promise<T>((resolve, reject) =>
+export function promisifyTimeout<T>(timeout: number, payload: T, isReject?: boolean): Promise<T>;
+export function promisifyTimeout<T>(timeout: number, payload?: T, isReject?: boolean): Promise<T | undefined> {
+  return new Promise<T | undefined>((resolve, reject) =>
     setTimeout((isReject ? reject : resolve).bind(null, payload), timeout)
   );
 }
