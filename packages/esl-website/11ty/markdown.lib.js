51f102c9d9180a3937b38d63d268d7c2bbaaf9ce
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2026-06-18 02:32:36 +0200

    chore(site): add frontmater ignore plugin for markdown-it

--- packages/esl-website/11ty/markdown.lib.js
+++ packages/esl-website/11ty/markdown.lib.js
@@ -1,13 +1,15 @@
 import MarkdownIt from 'markdown-it';
-import imglib from './markdown.img.lib.js';
+import imgPlugin from './markdown.img.lib.js';
 import tablePlugin from './markdown.table.lib.js';
+import frontMaterPlugin from './markdown.frontmater.lib.js';
 
 import {highlight} from './prismjs.lib.js';
 
 export const markdown = MarkdownIt({html: true, highlight});
 
-markdown.use(imglib.plugin);
+markdown.use(imgPlugin.plugin);
 markdown.use(tablePlugin.plugin);
+markdown.use(frontMaterPlugin.plugin);
 
 export default (config) => {
   config.setLibrary('md', markdown);
