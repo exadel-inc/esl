2cbf13d82dbedd0afea8c66476949fd69d5320b1
Dmytro Shovchko <d.shovchko@gmail.com>
2026-07-27 15:11:55 +0300

    refactor(esl-sticky-box): convert ESLStickyBox to ESLMixinElement mixin

--- packages/esl/src/esl-sticky-box/core.ts
+++ packages/esl/src/esl-sticky-box/core.ts
@@ -1,3 +1,1 @@
-export type {ESLStickyBoxTagShape} from './core/esl-sticky-box.shape';
-
 export * from './core/esl-sticky-box';
--- packages/esl/src/types/jsx.shape.ts
+++ packages/esl/src/types/jsx.shape.ts
@@ -14,7 +14,6 @@ import type {ESLPopupTagShape} from '../esl-popup/core';
 import type {ESLRandomTextTagShape} from '../esl-random-text/core';
 import type {ESLScrollbarTagShape} from '../esl-scrollbar/core';
 import type {ESLShareTagShape, ESLShareButtonTagShape, ESLShareListTagShape, ESLSharePopupTagShape} from '../esl-share/core';
-import type {ESLStickyBoxTagShape} from '../esl-sticky-box/core';
 import type {ESLTabTagShape, ESLTabsTagShape} from '../esl-tab/core';
 import type {ESLToggleableTagShape, ESLToggleableDispatcherTagShape} from '../esl-toggleable/core';
 import type {ESLTooltipTagShape} from '../esl-tooltip/core';
@@ -66,8 +65,6 @@ export interface ESLIntrinsicElements {
   'esl-share-list': ESLShareListTagShape;
   /** The {@link ESLSharePopup} tag declaration */
   'esl-share-popup': ESLSharePopupTagShape;
-  /** The {@link ESLStickyBox} tag declaration */
-  'esl-sticky-box': ESLStickyBoxTagShape;
   /** The {@link ESLTab} tag declaration */
   'esl-tab': ESLTabTagShape;
   /** The {@link ESLTabs} tag declaration */
