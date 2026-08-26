aa0bc6d317b08dbef6642c0ef5fdbec82af767ea
alesun <alesun@exadel.com>
2026-08-26 08:51:28 +0200

    chore(eslint-config): replace `eslint-plugin-import` with `eslint-plugin-perfectionist`
    BREAKING CHANGE: `@exadel/eslint-config-esl` no longer uses `eslint-plugin-import` rules

--- packages/eslint-config/rules/strict/index.js
+++ packages/eslint-config/rules/strict/index.js
@@ -1,13 +1,11 @@
 import ecmascript from './ecmascript.js';
 import typescript from './typescript.js';
-import imports from './imports.js';
 import sonarjs from './sonarjs.js';
 import tsdoc from './tsdoc.js';
 
 export default [
   ...ecmascript,
   ...typescript,
-  ...imports,
   ...sonarjs,
   ...tsdoc
 ];
